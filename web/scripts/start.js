55664865bc9bb668118f36912b7877df0ef5eb08
Kevin Jennison <kevin.jennison1@gmail.com>
2017-02-09 11:48:40 -0800

    Remove web app port selection logic because we always want to run on the same port with Docker.

--- web/scripts/start.js
+++ web/scripts/start.js
@@ -275,28 +275,4 @@ function run(port) {
   runDevServer(host, port, protocol);
 }
 
-// We attempt to use the default port but if it is busy, we offer the user to
-// run on a different port. `detect()` Promise resolves to the next free port.
-detect(DEFAULT_PORT).then(port => {
-  if (port === DEFAULT_PORT) {
-    run(port);
-    return;
-  }
-
-  if (isInteractive) {
-    clearConsole();
-    var existingProcess = getProcessForPort(DEFAULT_PORT);
-    var question =
-      chalk.yellow('Something is already running on port ' + DEFAULT_PORT + '.' +
-        ((existingProcess) ? ' Probably:\n  ' + existingProcess : '')) +
-        '\n\nWould you like to run the app on another port instead?';
-
-    prompt(question, true).then(shouldChangePort => {
-      if (shouldChangePort) {
-        run(port);
-      }
-    });
-  } else {
-    console.log(chalk.red('Something is already running on port ' + DEFAULT_PORT + '.'));
-  }
-});
+run(DEFAULT_PORT);
