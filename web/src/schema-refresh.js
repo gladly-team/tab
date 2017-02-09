a773d909e74687fcdcfd62bdde926c9cd9d56c99
Kevin Jennison <kevin.jennison1@gmail.com>
2017-02-09 15:44:16 -0800

    Remove unused component.

--- web/src/schema-refresh.js
+++
@@ -1,6 +0,0 @@
-// Last updated: Tue Feb  7 10:47:44 PST 2017
-// When the GraphQL server's schema changes, we copy it to the web app
-// for use by Relay. Then, we update the timestamp in this file to
-// cause a reload of the app.
-
-export default 1;
