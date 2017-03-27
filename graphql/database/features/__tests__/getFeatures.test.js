a344e8bb45d956146fea749b5f478106136db713
Raul Chall <raulchall@raul.lan>
2017-03-27 15:08:13 -0700

    Add unit testing setup for graphql.

---
+++ graphql/database/features/__tests__/getFeatures.test.js
@@ -0,0 +1,13 @@
+jest.mock('../../database');  
+
+import { getFeatures } from '../feature';
+
+test('fetch all the features', () => {
+	return getFeatures()
+    .then(data => {
+    	expect(data.length).toBe(3);
+    	expect(data[0].id).toBe('45bbefbf-63d1-4d36-931e-212fbe2bc3d9');
+    	expect(data[1].id).toBe('25cde869-7523-4605-81a6-3029651f2e10');
+    	expect(data[2].id).toBe('82718869-afea-4f49-9138-0b4e46df7dbf');
+    });
+});
