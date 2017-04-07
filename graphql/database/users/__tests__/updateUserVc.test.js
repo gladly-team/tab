1203a021cd6be2aee8123da1d9ed66a1c064d558
Raul Chall <raulchall@raul.lan>
2017-04-07 09:47:33 -0700

    Unittests now define their expected responses.

--- graphql/database/users/__tests__/updateUserVc.test.js
+++ graphql/database/users/__tests__/updateUserVc.test.js
@@ -1,8 +1,20 @@
-jest.mock('../../database');  
+import mockDatabase from '../../__mocks__/database';
+
+jest.mock('../../database', () => {
+	return mockDatabase;
+});
 
 import { updateUserVc, User } from '../user';
 
+function setup() {
+	mockDatabase.init();
+	return mockDatabase;
+}
+
 test('update user vc by x', () => {
+
+	const database = setup();
+
 	return updateUserVc('45bbefbf-63d1-4d36-931e-212fbe2bc3d9', 15)
     .then(data => {
         expect(data).not.toBe(null);
