9519d2c7bae0bd9aafefac5614925805bb4aa999
Raul Chall <raulchall@raul.local>
2017-04-14 21:26:41 -0700

    Include user background image on picker to ensure that info gets updated once the server respond.

--- web/src/js/components/BackgroundImage/BackgroundImagePickerContainer.js
+++ web/src/js/components/BackgroundImage/BackgroundImagePickerContainer.js
@@ -15,6 +15,11 @@ export default Relay.createContainer(BackgroundImagePicker, {
 	        }
 	      }
 	    }
+	    backgroundImage {
+  	      id
+  	      name
+  	      url
+  	    }
       }`
   }
 });
