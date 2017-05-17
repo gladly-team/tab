a60a2d4247e813f88724d261833cbca74067c2dd
Raul Chall <raulchall@raul.local>
2017-05-17 10:47:18 -0700

    Add a new base component for the routes to contain the auth routes. Start migrating all routes redirection to navigation.

--- web/src/js/routes/Route.js
+++ web/src/js/routes/Route.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { IndexRoute, Route, Redirect } from 'react-router';
 
+import BaseContainer from 'general/BaseContainer';
+
 import AppView from '../components/App/AppView';
 import DashboardView from '../components/Dashboard/DashboardView';
 import Authentication from '../components/Authentication/Authentication';
@@ -11,16 +13,23 @@ import SettingsView from '../components/Settings/SettingsView';
 import BackgroundSettingsView from '../components/Settings/Background/BackgroundSettingsView';
 import WidgetsSettingsView from '../components/Settings/Widgets/WidgetsSettingsView';
 
+import CreateUserView from '../components/User/CreateUserView';
+
 export default (
-  <Route path='/' component={AppView}>
-    <IndexRoute component={DashboardView}/>
+  <Route path='/' component={BaseContainer}>
+    <IndexRoute component={Authentication}/>
     <Route path='auth' component={Authentication}/>
-    <Route path='donate' component={DonateVcView}/>
-    <Route path='settings' component={SettingsView}>
-    	<IndexRoute component={WidgetsSettingsView}/>
-    	<Route path='widgets' component={WidgetsSettingsView}/>
-    	<Route path='background' component={BackgroundSettingsView}/>
+    <Route path='new-user' component={CreateUserView}/>
+    <Route path='app' component={AppView}>
+        <IndexRoute component={DashboardView}/>
+        <Route path='donate' component={DonateVcView}/>
+        <Route path='settings' component={SettingsView}>
+            <IndexRoute component={WidgetsSettingsView}/>
+            <Route path='widgets' component={WidgetsSettingsView}/>
+            <Route path='background' component={BackgroundSettingsView}/>
+        </Route>
     </Route>
     <Redirect from='*' to='/' />
   </Route>
+  
 );
