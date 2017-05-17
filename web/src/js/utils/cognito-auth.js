ca9f84f557a26b6a708e856c79edb0268c534425
Raul Chall <raulchall@raul.local>
2017-05-17 10:44:35 -0700

    Add wrapper for cognito api.

---
+++ web/src/js/utils/cognito-auth.js
@@ -0,0 +1,190 @@
+import AWS, {
+  Config,
+  CognitoIdentityCredentials,
+  CognitoIdentityServiceProvider
+} from 'aws-sdk';
+
+import {
+  CognitoUserPool,
+  CognitoUserAttribute
+} from 'amazon-cognito-identity-js';
+
+const appConfig = {
+  region: process.env.COGNITO_REGION,
+  IdentityPoolId: process.env.COGNITO_IDENTITYPOOLID,
+  UserPoolId: process.env.COGNITO_USERPOOLID,
+  ClientId: process.env.COGNITO_CLIENTID
+};
+
+import localStorageMgr from './local-storage-mgr';
+
+Config.region = appConfig.region;
+
+const userPool = new CognitoUserPool({
+  UserPoolId: appConfig.UserPoolId,
+  ClientId: appConfig.ClientId,
+});
+
+function checkUserExist(email, onSuccess, onFailure) {
+	login(email, '', () => {}, (err) => {
+		if(err.statusCode === 400) {
+			if(err.code === 'UserNotFoundException') {
+				onFailure(err);
+			} else if(err.code === 'NotAuthorizedException') {
+				onSuccess(err, true);
+			} else if(err.code === 'UserNotConfirmedException') {
+				onSuccess(err, false);
+			}
+		}
+	});
+}
+
+function login(email, password, onSuccessCallback, onFailureCallback) {
+
+	var authenticationData = {
+        Email: email,
+        Password: password,
+    };
+
+    var authenticationDetails = new CognitoIdentityServiceProvider.
+    							AuthenticationDetails(authenticationData);
+    
+    var userData = {
+      Username: email,
+      Pool: userPool
+    };
+
+    var cognitoUser = new CognitoIdentityServiceProvider.
+    									CognitoUser(userData);
+
+    cognitoUser.authenticateUser(authenticationDetails, {
+      onSuccess: function (result) {
+
+        const cognitoArn = `cognito-idp.${appConfig.region}.amazonaws.com/${appConfig.UserPoolId}`;
+        AWS.config.credentials = new CognitoIdentityCredentials({
+          IdentityPoolId : appConfig.IdentityPoolId,
+          Logins: {
+            [cognitoArn]: result.getIdToken().getJwtToken(),
+          },
+        });
+
+        onSuccessCallback(result);
+      },
+      onFailure: function(err) {
+          onFailureCallback(err);
+      },
+    });
+}
+
+function signup(email, password, onSuccessCallback, onFailureCallback) {
+
+	const attributeList = [
+      new CognitoUserAttribute({
+        Name: 'email',
+        Value: email,
+      })
+    ];
+
+    userPool.signUp(email, password, attributeList, null, (err, result) => {
+      if (err) {
+        onFailureCallback(err);
+        return;
+      }
+
+      onSuccessCallback(result);
+    });
+}
+
+function confirmRegistration(code, email, onSuccess, onFailure) {
+    
+    var userData = {
+      Username: email,
+      Pool: userPool
+    };
+
+    var cognitoUser = new CognitoIdentityServiceProvider.
+    									CognitoUser(userData);
+
+    cognitoUser.confirmRegistration(code, true, function(err, result) {
+        if (err) {
+        	console.error(err);
+        	onFailure(err);
+        	return;
+        }
+        console.log(result);
+        onSuccess(result);
+    });
+}
+
+function resendConfirmation(email, onSuccess, onFailure) {
+	var userData = {
+      Username: email,
+      Pool: userPool
+    };
+
+    var cognitoUser = new CognitoIdentityServiceProvider.
+    									CognitoUser(userData);
+
+    cognitoUser.resendConfirmationCode(function(err, result) {
+        if (err) {
+            console.error(err);
+        	onFailure(err);
+        	return;
+        }
+        onSuccess(result);
+    });
+}
+
+function getCurrentUser(callback) {
+	var cognitoUser = userPool.getCurrentUser();
+
+  if (cognitoUser != null) {
+    	cognitoUser.getSession(function(err, session) {
+        if (err) {
+          alert(err);
+          return;
+        }
+
+        // NOTE: getSession must be called to authenticate user before calling getUserAttributes
+        cognitoUser.getUserAttributes(function(err, attributes) {
+          if (err) {
+            console.error(err);
+            callback(null);
+          } else {
+            const user = {};
+            var attribute;
+            for(var index in attributes) {
+            	attribute = attributes[index];
+            	user[attribute.getName()] = attribute.getValue();
+            }
+            callback(user);
+          }
+        });
+    });
+  } else {
+    callback(null);
+  }
+}
+
+function logoutUser(callback) {
+	var cognitoUser = userPool.getCurrentUser();
+    if (cognitoUser != null) {
+    	
+      cognitoUser.signOut();
+      localStorageMgr.logoutUser();
+
+    	callback(true)
+    } else {
+      callback(false);
+    }
+} 
+
+export {
+	login,
+	signup,
+	confirmRegistration,
+	resendConfirmation,
+	getCurrentUser,
+	logoutUser,
+	checkUserExist
+}
