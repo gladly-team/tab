5b8c2dbc6fce8e209bd24c23a10d0093d24a6a70
Raul Chall <raulchall@raul.local>
2017-05-17 10:45:48 -0700

    Add create new user Relay mutation and create new user component.

---
+++ web/src/js/mutations/CreateNewUserMutation.js
@@ -0,0 +1,36 @@
+import {
+  commitMutation,
+  graphql,
+} from 'react-relay/compat';
+
+const mutation = graphql`
+  mutation CreateNewUserMutation($input: CreateNewUserInput!) {
+    createNewUser(input: $input) {
+      user {
+        id
+        email
+      }
+    }
+  }
+`;
+
+function commit(environment, userId, email, onCompleted, onError) {
+  return commitMutation(
+    environment,
+    {
+      mutation,
+      variables: {
+        input: { userId, email }
+      },
+      onCompleted: (response) => {
+        onCompleted(response);
+      },
+      onError: (err) => {
+        onError(err);
+      }
+    }
+  );
+}
+
+export default {commit};
+
