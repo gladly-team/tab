8528d36e2c565f1f54b817a42e011f785333f33d
Kevin Jennison <kevin.jennison1@gmail.com>
2017-06-30 15:28:52 -0700

    Add OpenX config and tag.

---
+++ web/src/js/ads/openxConfig.js
@@ -0,0 +1,6 @@
+
+window.OX_dfp_options = { prefetch: true }
+window.OX_dfp_ads = [
+  ['/43865596/HBTR', ['300x250'], 'div-gpt-ad-1464385742501-0'],
+  ['/43865596/HBTL', ['728x90'], 'div-gpt-ad-1464385677836-0']
+]
