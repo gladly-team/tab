f1e2c5fb288fa78564e3958281b194fcfa2c31eb
Kevin Jennison <kevin.jennison1@gmail.com>
2017-06-30 09:18:54 -0700

    Test ad component call to display an ad.

--- web/src/js/components/Ad/__tests__/Ad.test.js
+++ web/src/js/components/Ad/__tests__/Ad.test.js
@@ -1,8 +1,9 @@
 /* eslint-env jest */
 
 import React from 'react'
-import {shallow} from 'enzyme'
+import { shallow } from 'enzyme'
 import Ad from '../Ad'
+import AdClient from 'ads/AdClient'
 
 jest.mock('ads/AdClient')
 jest.mock('ads/activeAdClient')
@@ -17,5 +18,18 @@ describe('Ad component', function () {
         height={250} />
     )
     expect(wrapper.contains(<div id='abc123' />)).toBe(true)
+  })
+
+  it('calls to display ad on mount', function () {
+    const wrapper = shallow(
+      <Ad
+        adId='abc123'
+        adSlotId='def'
+        width={300}
+        height={250} />
+    )
+    expect(AdClient.displayAd).not.toHaveBeenCalled()
+    wrapper.instance().componentDidMount()
+    expect(AdClient.displayAd).toHaveBeenCalledTimes(1)
   })
 })
