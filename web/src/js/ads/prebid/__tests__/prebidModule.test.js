7e8d88580f9dc78b0c3cb833b0cc9bd463742f52
Kevin Jennison <kevin.jennison1@gmail.com>
2017-07-05 14:41:56 -0700

    Add prebid module test.

---
+++ web/src/js/ads/prebid/__tests__/prebidModule.test.js
@@ -0,0 +1,15 @@
+/* eslint-env jest */
+
+import prebidModule from '../prebidModule'
+
+beforeEach(() => {
+  delete window.pbjs
+})
+
+describe('prebid module', function () {
+  it('sets window.pbjs variable', () => {
+    expect(window.pbjs).not.toBeDefined()
+    prebidModule()
+    expect(window.pbjs).toBeDefined()
+  })
+})
