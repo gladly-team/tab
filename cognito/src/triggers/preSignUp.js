083e82708c42db3557cc91db0a10f32dda70a716
Raul Chall <raul@gladly.io>
2017-07-07 09:32:34 -0700

    Remove comments.

--- cognito/src/triggers/preSignUp.js
+++ cognito/src/triggers/preSignUp.js
@@ -1,55 +1,17 @@
 'use strict'
 
+/**
+ * Returns a flag to indicate if a user should be auto-confirmed.
+ * @param {Object} event - Cognito event.
+ * @param {Object} context - Cognito context.
+ * @param {Object} callback - Callback.
+ */
 function handler (event, context, callback) {
-  // This Lambda function returns a flag to indicate if a user should be auto-confirmed.
-  // Access your resource which contains the list of emails of users who were invited to sign up
-
   event.response.autoConfirmUser = true
   event.response.autoVerifyEmail = true
-
-  // Return result to Cognito
   context.done(null, event)
 }
 
 export {
     handler
 }
-
-// const handler = (event, context) => {
-//   return new Promise((resolve, reject) => {
-//     // This Lambda function returns a flag to indicate if a user should be auto-confirmed.
-//     // Access your resource which contains the list of emails of users who were invited to sign up
-
-//     event.response.autoConfirmUser = true
-//     event.response.autoVerifyEmail = true
-
-//     // Return result to Cognito
-//     context.done(null, event)
-//     resolve()
-//   });
-// }
-
-// const serverlessHandler = (event, context, callback) => {
-//   handler(event, context)
-//     .then(response => callback(null, event))
-// }
-
-// module.exports = {
-//   handler: handler,
-//   serverlessHandler: serverlessHandler
-// }
-
-// const serverlessHandler = (event, context, callback) => {
-//   // This Lambda function returns a flag to indicate if a user should be auto-confirmed.
-//   // Access your resource which contains the list of emails of users who were invited to sign up
-
-//   event.response.autoConfirmUser = true
-//   event.response.autoVerifyEmail = true
-
-//   // Return result to Cognito
-//   context.done(null, event)
-// }
-
-// module.exports = {
-//   serverlessHandler: serverlessHandler
-// }
