2009cc08bc06b942cabbde1569ca2dcba2f7a8d0
Kevin Jennison <kevin.jennison1@gmail.com>
2017-07-17 13:44:12 -0700

    Functions to create and log in user.

--- graphql/integration-tests/utils/auth-utils.js
+++ graphql/integration-tests/utils/auth-utils.js
@@ -12,13 +12,87 @@ AWS.config.update({
 const cognitoIDP = new AWS.CognitoIdentityServiceProvider()
 
 export const listUsers = async () => {
-  const limit = 10
-  var params = {
+  const params = {
     UserPoolId: config.COGNITO_USERPOOLID,
     AttributesToGet: [
       'email'
     ],
-    Limit: limit
+    Limit: 10
   }
   return cognitoIDP.listUsers(params).promise()
+}
+
+export const getUser = async (username) => {
+  const params = {
+    UserPoolId: config.COGNITO_USERPOOLID,
+    Username: username
+  }
+  return cognitoIDP.adminGetUser(params)
+    .promise()
+    .catch((err) => {
+      throw new Error('Could not get user.', err)
+    })
+}
+
+export const createUser = async (email, username, password) => {
+  const params = {
+    UserPoolId: config.COGNITO_USERPOOLID,
+    Username: username,
+    UserAttributes: [{
+      Name: 'email',
+      Value: email
+    }],
+    DesiredDeliveryMediums: [],
+    MessageAction: 'SUPPRESS',
+    TemporaryPassword: password
+  }
+  return cognitoIDP.adminCreateUser(params)
+    .promise()
+    .catch((err) => {
+      throw new Error('Could not create user.', err)
+    })
+}
+
+export const logIn = async (username, password) => {
+  // Admin auth. See:
+  // https://docs.aws.amazon.com/cognito/latest/developerguide/amazon-cognito-user-pools-authentication-flow.html#amazon-cognito-user-pools-admin-authentication-flow
+  const initiateAuthParams = {
+    AuthFlow: 'ADMIN_NO_SRP_AUTH',
+    ClientId: config.COGNITO_CLIENTID,
+    UserPoolId: config.COGNITO_USERPOOLID,
+    AuthParameters: {
+      'USERNAME': username,
+      'PASSWORD': password
+    }
+  }
+  const authInitResponse = await cognitoIDP
+    .adminInitiateAuth(initiateAuthParams)
+    .promise()
+    .catch((err) => {
+      throw new Error('Could not initiate auth.', err)
+    })
+  const userIdNonAlias = authInitResponse.ChallengeParameters.USER_ID_FOR_SRP
+  const session = authInitResponse.Session
+
+  // Log the user in.
+  const params = {
+    ChallengeName: 'NEW_PASSWORD_REQUIRED',
+    ClientId: config.COGNITO_CLIENTID,
+    UserPoolId: config.COGNITO_USERPOOLID,
+    ChallengeResponses: {
+      'USERNAME': userIdNonAlias,
+      'NEW_PASSWORD': password
+    },
+    Session: session
+  }
+  return cognitoIDP.adminRespondToAuthChallenge(params)
+    .promise()
+    .catch((err) => {
+      throw new Error('Could not authenticate user.', err)
+    })
+}
+
+export const createUserAndLogIn = async (email, username, password) => {
+  await createUser(email, username, password)
+  return logIn(username, password)
 }
