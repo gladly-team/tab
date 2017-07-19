a1e1be106d1f629027057a79b2931cbc490a57d4
Kevin Jennison <kevin.jennison1@gmail.com>
2017-07-19 13:39:58 -0700

    TODO notes for lambda authorization.

--- lambda/src/utils/database.js
+++ lambda/src/utils/database.js
@@ -4,6 +4,7 @@ import AWS from 'aws-sdk'
 AWS.config.update({
   region: process.env.AWS_REGION,
   endpoint: process.env.DYNAMODB_ENDPOINT,
+  // TODO: remove and rely on IAM role permissions.
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 })
