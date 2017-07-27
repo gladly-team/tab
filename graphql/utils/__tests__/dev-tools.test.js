7ed10bbf01cb1e69731207488dd39e22f93f125b
Kevin Jennison <kevin.jennison1@gmail.com>
2017-07-27 13:06:37 -0700

    Actual dev-tools tests.

--- graphql/utils/__tests__/dev-tools.test.js
+++ graphql/utils/__tests__/dev-tools.test.js
@@ -1,77 +1,22 @@
 /* eslint-env jest */
 
 import {
-  createGraphQLContext,
-  getUserClaimsFromLambdaEvent,
-  isUserAuthorized
-} from '../authorization-helpers'
+  getGraphQLContextFromRequest
+} from '../dev-tools'
 
-describe('authorization.js', () => {
-  it('correctly gets user claims from AWS Lambda event obj', () => {
-    const claims = {
-      sub: 'abcdef',
-      'cognito:username': 'my-name',
-      email_verified: 'true'
-    }
-    const minimalLambdaEventObj = {
-      requestContext: {
-        authorizer: {
-          claims: claims
-        }
-      }
-    }
-    const fetchedClaims = getUserClaimsFromLambdaEvent(minimalLambdaEventObj)
-    expect(fetchedClaims).toEqual(claims)
-  })
-
-  it('creates expected GraphQL context', () => {
-    const userClaims = {
-      sub: 'abcdef',
-      'cognito:username': 'my-name',
-      email_verified: 'true'
-    }
+describe('dev-tools', () => {
+  it('corrently forms GraphQL context from a request object', () => {
+    // Note: will have to update this when we remove hardcoded
+    // user claims.
+    const minimalRequestObject = {}
     const expectedContext = {
       user: {
-        id: 'abcdef',
-        'username': 'my-name',
-        'emailVerified': true
+        id: '45bbefbf-63d1-4d36-931e-212fbe2bc3d9',
+        emailVerified: true,
+        username: 'myUserName'
       }
     }
-    const context = createGraphQLContext(userClaims)
-    expect(expectedContext).toEqual(context)
-  })
-
-  it('does not authorize a user who does not have a verified email', () => {
-    const userClaims = {
-      sub: 'abcdef',
-      'cognito:username': 'my-name',
-      email_verified: 'false'
-    }
-    expect(isUserAuthorized(userClaims)).toBe(false)
-  })
-
-  it('does not authorize a user who does not have a username', () => {
-    const userClaims = {
-      sub: 'abcdef',
-      email_verified: 'true'
-    }
-    expect(isUserAuthorized(userClaims)).toBe(false)
-  })
-
-  it('does not authorize a user who does not have an ID', () => {
-    const userClaims = {
-      'cognito:username': 'my-name',
-      email_verified: 'true'
-    }
-    expect(isUserAuthorized(userClaims)).toBe(false)
-  })
-
-  it('authorizes a user who has appropriate claims', () => {
-    const userClaims = {
-      sub: 'abcdef',
-      'cognito:username': 'my-name',
-      email_verified: 'true'
-    }
-    expect(isUserAuthorized(userClaims)).toBe(true)
+    const context = getGraphQLContextFromRequest(minimalRequestObject)
+    expect(context).toEqual(expectedContext)
   })
 })
