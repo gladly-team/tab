16670aa74d9c864521fc559546e930e69879b87c
Kevin Jennison <kevin.jennison1@gmail.com>
2017-08-09 18:38:39 -0700

    Cleaner tests for setActiveWidget.

--- graphql/database/users/__tests__/setActiveWidget.test.js
+++ graphql/database/users/__tests__/setActiveWidget.test.js
@@ -1,22 +1,64 @@
 /* eslint-env jest */
 
+import moment from 'moment'
 import UserModel from '../UserModel'
 import setActiveWidget from '../setActiveWidget'
 import {
+  DatabaseOperation,
   getMockUserObj,
-  mockQueryMethods
+  mockDate,
+  setMockDBResponse
 } from '../../test-utils'
 
-const user = getMockUserObj()
-mockQueryMethods(UserModel)
+jest.mock('../../databaseClient')
+
+const userContext = getMockUserObj()
+
+beforeAll(() => {
+  mockDate.on()
+})
+
+afterAll(() => {
+  mockDate.off()
+})
 
 describe('setActiveWidget', () => {
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
   it('works as expected', async () => {
+    const updateMethod = jest.spyOn(UserModel, 'update')
     const widgetId = 'abcdefgh-12ab-12ab-12ab-123abc456def'
-    await setActiveWidget(user, user.id, widgetId)
-    expect(UserModel.update).toHaveBeenCalledWith(user, {
-      id: user.id,
-      activeWidget: widgetId
+    await setActiveWidget(userContext, userContext.id, widgetId)
+    expect(updateMethod).toHaveBeenCalledWith(userContext, {
+      id: userContext.id,
+      activeWidget: widgetId,
+      updated: moment.utc().toISOString()
     })
+  })
+
+  it('calls the database', async () => {
+    const widgetId = 'abcdefgh-12ab-12ab-12ab-123abc456def'
+
+    const userInfo = {
+      id: userContext.id,
+      username: userContext.username,
+      email: userContext.email
+    }
+    const mockUser = Object.assign({}, new UserModel(userInfo))
+    const expectedReturnedUser = Object.assign({}, mockUser, {
+      updated: moment.utc().toISOString()
+    })
+    const dbUpdateMock = setMockDBResponse(
+      DatabaseOperation.UPDATE,
+      {
+        Attributes: expectedReturnedUser
+      }
+    )
+    const response = await setActiveWidget(userContext,
+      userContext.id, widgetId)
+    expect(dbUpdateMock).toHaveBeenCalled()
+    expect(response).toEqual(expectedReturnedUser)
   })
 })
