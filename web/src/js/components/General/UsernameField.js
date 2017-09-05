6cac19175911d79b156c3bf94f470cee227760cb
Kevin Jennison <kevin.jennison1@gmail.com>
2017-09-05 14:51:59 -0700

    Add more helpful error messages for username length.

--- web/src/js/components/General/UsernameField.js
+++ web/src/js/components/General/UsernameField.js
@@ -45,7 +45,11 @@ class UsernameField extends React.Component {
       const username = this.username.input.value.trim()
       const isValid = validateUsername(username)
       if (!isValid) {
-        this.setErrorMessage('Username is invalid.')
+        if (username.length < 2) {
+          this.setErrorMessage('Must be at least two characters.')
+        } else {
+          this.setErrorMessage('Username is invalid.')
+        }
       } else {
         this.setErrorMessage(null)
       }
