b58149760e56dd6cfc07b00cc32c7d957155a70b
Kevin Jennison <kevin.jennison1@gmail.com>
2017-09-05 14:37:16 -0700

    Tests for email and password validation.

--- web/src/js/utils/__tests__/utils.test.js
+++ web/src/js/utils/__tests__/utils.test.js
@@ -32,3 +32,50 @@ describe('username validation', () => {
     expect(validateUsername('ab')).toEqual(true)
   })
 })
+
+describe('email validation', () => {
+  it('works for a typical email', () => {
+    expect(validateEmail('foo@bar.com')).toEqual(true)
+  })
+
+  it('allows special characters', () => {
+    expect(validateEmail('pequeño.pinguino@gmail.com')).toEqual(true)
+  })
+
+  it('allows plus sign', () => {
+    expect(validateEmail('foo+stuff@bar.com')).toEqual(true)
+  })
+
+  it('fails with invalid TLD', () => {
+    expect(validateEmail('foo@blah')).toEqual(false)
+  })
+
+  it('fails with invalid @@ signs', () => {
+    expect(validateEmail('foo@@blah.com')).toEqual(false)
+  })
+
+  it('fails with lack of @ sign', () => {
+    expect(validateEmail('fooblah.com')).toEqual(false)
+  })
+})
+
+const passwordConfig = {
+  lowercase: false,
+  uppercase: false,
+  numeric: false,
+  special: false,
+  minSize: 8
+}
+describe('password validation', () => {
+  it('works for a typical password', () => {
+    expect(validatePassword('abC123$@abv', passwordConfig).valid).toEqual(true)
+  })
+
+  it('does not require special characters or caps', () => {
+    expect(validatePassword('abcabcabc', passwordConfig).valid).toEqual(true)
+  })
+
+  it('fails when shorter than 8 characters', () => {
+    expect(validatePassword('1234567', passwordConfig).valid).toEqual(false)
+  })
+})
