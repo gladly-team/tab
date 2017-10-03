56815a803a5b52ed811b3ca368b3e9a049a0f716
Kevin Jennison <kevin.jennison1@gmail.com>
2017-10-03 13:30:31 -0700

    Disable fade animations for now.

--- web/src/js/components/General/FadeInAnimation.js
+++ web/src/js/components/General/FadeInAnimation.js
@@ -1,19 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { CSSTransitionGroup } from 'react-transition-group'
+// import { CSSTransitionGroup } from 'react-transition-group'
 
 class FadeInAnimation extends React.Component {
   render () {
     return (
-      <CSSTransitionGroup
-        transitionName='fade'
-        transitionAppear
-        transitionAppearTimeout={300}
-        transitionEnterTimeout={300}
-        transitionLeaveTimeout={300}>
+      <span>
         {this.props.children}
-      </CSSTransitionGroup>
+      </span>
     )
+    // return (
+    //   <CSSTransitionGroup
+    //     transitionName='fade'
+    //     transitionAppear
+    //     transitionAppearTimeout={300}
+    //     transitionEnterTimeout={300}
+    //     transitionLeaveTimeout={300}>
+    //     {this.props.children}
+    //   </CSSTransitionGroup>
+    // )
   }
 }
 
--- web/src/js/components/General/RandomAppearAnimation.js
+++ web/src/js/components/General/RandomAppearAnimation.js
@@ -1,55 +1,60 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { CSSTransitionGroup } from 'react-transition-group'
+// import { CSSTransitionGroup } from 'react-transition-group'
 
 class RandomAppearAnimation extends React.Component {
   constructor (props) {
     super(props)
 
     this.state = {
-      render: false
+      render: true
     }
     this.renderTimer = 0
   }
 
-  componentWillUnmount () {
-    clearTimeout(this.renderTimer)
-  }
+  // componentWillUnmount () {
+  //   clearTimeout(this.renderTimer)
+  // }
 
-  componentWillMount () {
-    if (this.props.delayRange) {
-      const delay = Math.floor(Math.random() * this.props.delayRange + 1)
-      this.renderTimer = setTimeout(() => {
-        this.setState({
-          render: true
-        })
-      }, delay)
-    } else {
-      this.setState({
-        render: true
-      })
-    }
-  }
+  // componentWillMount () {
+  //   if (this.props.delayRange) {
+  //     const delay = Math.floor(Math.random() * this.props.delayRange + 1)
+  //     this.renderTimer = setTimeout(() => {
+  //       this.setState({
+  //         render: true
+  //       })
+  //     }, delay)
+  //   } else {
+  //     this.setState({
+  //       render: true
+  //     })
+  //   }
+  // }
 
   render () {
-    if (!this.state.render) {
-      return (
-        <span style={{visibility: 'hidden'}}>
-          {this.props.children}
-        </span>
-      )
-    }
+    // if (!this.state.render) {
+    //   return (
+    //     <span style={{visibility: 'hidden'}}>
+    //       {this.props.children}
+    //     </span>
+    //   )
+    // }
 
     return (
-      <CSSTransitionGroup
-        transitionName='zoom-opacity'
-        transitionAppear
-        transitionAppearTimeout={300}
-        transitionEnterTimeout={300}
-        transitionLeaveTimeout={300}>
+      <span>
         {this.props.children}
-      </CSSTransitionGroup>
+      </span>
     )
+    // return (
+    //   <CSSTransitionGroup
+    //     transitionName='zoom-opacity'
+    //     transitionAppear
+    //     transitionAppearTimeout={300}
+    //     transitionEnterTimeout={300}
+    //     transitionLeaveTimeout={300}>
+    //     {this.props.children}
+    //   </CSSTransitionGroup>
+    // )
   }
 }
 
