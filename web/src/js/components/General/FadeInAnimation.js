d5aa7c43fd980a913c382c9cccb6478b3844b5aa
Kevin Jennison <kevin.jennison1@gmail.com>
2017-10-03 16:41:58 -0700

    Restore fade in animation.

--- web/src/js/components/General/FadeInAnimation.js
+++ web/src/js/components/General/FadeInAnimation.js
@@ -1,32 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-// import { CSSTransitionGroup } from 'react-transition-group'
+import { CSSTransitionGroup } from 'react-transition-group'
 
 class FadeInAnimation extends React.Component {
   render () {
     return (
-      <span>
+      <CSSTransitionGroup
+        transitionName='fade'
+        transitionAppear
+        transitionAppearTimeout={300}
+        transitionEnterTimeout={300}
+        transitionLeaveTimeout={300}>
         {this.props.children}
-      </span>
+      </CSSTransitionGroup>
     )
-    // return (
-    //   <CSSTransitionGroup
-    //     transitionName='fade'
-    //     transitionAppear
-    //     transitionAppearTimeout={300}
-    //     transitionEnterTimeout={300}
-    //     transitionLeaveTimeout={300}>
-    //     {this.props.children}
-    //   </CSSTransitionGroup>
-    // )
   }
 }
 
 FadeInAnimation.propTypes = {
   children: PropTypes.object.isRequired
-}
-
-FadeInAnimation.defaultProps = {
 }
 
 export default FadeInAnimation
