9687c15d7b93058b4d965c81b236dd07ba5381d2
Kevin Jennison <kevin.jennison1@gmail.com>
2017-11-01 15:18:49 -0700

    Remove unneeded proptypes.

--- web/src/js/components/Widget/ActiveWidgetAnimation.js
+++ web/src/js/components/Widget/ActiveWidgetAnimation.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import { CSSTransitionGroup } from 'react-transition-group'
 
 class ActiveWidgetAnimation extends React.Component {
@@ -15,10 +14,6 @@ class ActiveWidgetAnimation extends React.Component {
       </CSSTransitionGroup>
     )
   }
-}
-
-ActiveWidgetAnimation.propTypes = {
-  children: PropTypes.object.isRequired
 }
 
 export default ActiveWidgetAnimation
