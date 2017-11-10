3886b08653728914e584c7b2a12e1672933ec284
Kevin Jennison <kevin.jennison1@gmail.com>
2017-11-10 15:06:36 -0800

    Fix bug in firebaseui `credentialHelper` setting.

--- web/src/js/components/Authentication/Authentication.js
+++ web/src/js/components/Authentication/Authentication.js
@@ -30,10 +30,10 @@ const uiConfig = {
       requireDisplayName: false
     }
   ],
-  // TODO: This is not functional. Update it when fixed. See:
-  // https://github.com/firebase/firebaseui-web-react/issues/2
+  // Just using the constant rather than importing firebaseui
   // https://github.com/firebase/firebaseui-web#credential-helper
-  credentialHelper: 'firebaseui.auth.CredentialHelper.NONE',
+  // https://github.com/firebase/firebaseui-web/blob/bd710448caa34c4a47a2fd578d76be8506d392d8/javascript/widgets/config.js#L83
+  credentialHelper: 'none',
   tosUrl: 'https://tab.gladly.io/more/terms'
 }
 
