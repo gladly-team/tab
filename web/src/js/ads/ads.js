2ec95c9d47b8b9f277af190bb92f6e0561d135ec
Kevin Jennison <kevin.jennison1@gmail.com>
2017-11-30 16:34:57 -0800

    Add Amazon A9.

--- web/src/js/ads/ads.js
+++ web/src/js/ads/ads.js
@@ -5,12 +5,14 @@ import prebid from './prebid/prebidModule'
 import prebidConfig from './prebid/prebidConfig'
 import googleTagManager from './google/googleTagManager'
 import googleAdSlotDefinitions from './google/googleAdSlotDefinitions'
+import amazonBidder from './amazon/amazonBidder'
 import openxConfig from './openx/openxConfig'
 
 if (adsEnabled) {
   setUpGoogleTag()
   prebid()
   prebidConfig()
+  amazonBidder()
   openxConfig()
   googleTagManager()
   googleAdSlotDefinitions()
