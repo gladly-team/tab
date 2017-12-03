6f0ea83c9909670899749fbba356a26c79921b0b
Kevin Jennison <kevin.jennison1@gmail.com>
2017-12-03 13:08:32 -0800

    Skeleton for migrate widgets function.

---
+++ graphql/migration/migrateUserWidgets.js
@@ -0,0 +1,180 @@
+'use strict'
+
+import 'babel-polyfill' // For async/await support.
+import config from '../config'
+import logger from '../utils/logger'
+import updateUserWidgetData from '../database/widgets/userWidget/updateUserWidgetData'
+
+import {
+  getPermissionsOverride,
+  MIGRATION_OVERRIDE
+} from '../utils/permissions-overrides'
+const migrationOverride = getPermissionsOverride(MIGRATION_OVERRIDE)
+
+// Note: we need to use Bluebird until at least Node 8. Using
+// native promises breaks Sentry/Raven context:
+// https://github.com/getsentry/raven-node/issues/265
+global.Promise = require('bluebird')
+const Promise = require('bluebird')
+
+const createResponse = function (statusCode, body) {
+  return {
+    statusCode: statusCode,
+    headers: {
+      'Access-Control-Allow-Origin': '*' // Required for CORS
+    },
+    body: JSON.stringify(body)
+  }
+}
+
+// Legacy bookmark widget data structure
+/*
+ {'data': {u'color': u'#ff4141',
+           u'name': u'Pinterest',
+           u'textcolor': u'#781f1f',
+           u'url': u'www.pinterest.com'},
+  'deletable': 'true',
+  'height': 1,
+  'id': '1a0f725c2dfe4fcb89eb56f0a6a1fea6',
+  'modified_at': datetime.datetime(2015, 1, 15, 8, 0, tzinfo=<UTC>),
+  'movableBetweenPanes': 'true',
+  'pane': 0,
+  'permanent': False,
+  'resizable': 'true',
+  'settings': [{u'default': u'Google',
+                u'display_text': u'Name',
+                u'name': u'name',
+                u'type': u'input'},
+               {u'default': u'https://google.com',
+                u'display_text': u'URL',
+                u'name': u'url',
+                u'type': u'input'},
+               {u'default': u'#9C87BE',
+                u'display_text': u'Background color',
+                u'name': u'color',
+                u'type': u'color'},
+               {u'default': u'#2B2B2B',
+                u'display_text': u'Text color',
+                u'name': u'textColor',
+                u'type': u'color'}],
+  'widget_id': u'781eabcc1f2449d494ec994a327c330e',
+  'width': 1,
+  'x': 3,
+  'y': 2},
+*/
+
+// New bookmark widget data structure
+/*
+{
+  bookmarks: [
+    link: 'example.com',
+    name: 'Some Example'
+  ]
+}
+*/
+
+// Legacy note widget data structure
+/*
+ {'data': {u'color': u'#FAFAD2',
+           u'text': u'blah blah',
+           u'textColor': u'black'},
+  'deletable': 'true',
+  'height': 2,
+  'id': '5832b96bd5f54a69a43d90432a128c95',
+  'modified_at': datetime.datetime(2017, 12, 3, 18, 59, tzinfo=<UTC>),
+  'movableBetweenPanes': 'true',
+  'pane': 0,
+  'permanent': False,
+  'resizable': 'true',
+  'settings': [{u'default': u'black',
+                u'display_text': u'Text Color',
+                u'name': u'textcolor',
+                u'type': u'text-color-toggle'},
+               {u'default': u'LightGoldenRodYellow',
+                u'display_text': u'Background color',
+                u'name': u'color',
+                u'type': u'color'}],
+  'widget_id': u'7e8e8c158aea451fa6cd10d9dc905a29',
+  'width': 2,
+  'x': 3,
+  'y': 1}]
+*/
+
+// New note widget data structure
+/*
+{
+  notes: [
+    {
+      color: #FFF
+      content: 'Some note here'
+      created: 2017-11-07T22:56:32Z
+      id: 'icFC37'
+    }
+  ]
+*/
+
+export const getNewWidgetId = (legacyWidgetId) => {
+  // Bookmark
+  if (legacyWidgetId === '26a72807b8124f34b17dcf03250b9fa4') {
+    return 'a8cfd733-639b-49d4-a822-116cc7e5c2e2'
+  // Sticky note
+  } else if (legacyWidgetId === 'd84447e23fee40b98982241513e3005b') {
+    return '63859963-f691-42f6-bc80-ac83eddc4104'
+  // Oops
+  } else {
+    throw new Error('Not a valid legacy widget ID: ' + legacyWidgetId)
+  }
+}
+
+// Takes a legacy widget data object.
+// Returns a current data object.
+export const createDataFromLegacyWidgetData = (legacyData) => {
+  // TODO
+  return {}
+}
+
+export const migrateWidgets = async (userId, legacyWidgets) => {
+  return Promise.all(legacyWidgets.map(async (legacyWidget) => {
+    try {
+      // Reformat data.
+      const newWidgetId = getNewWidgetId(legacyWidget.widget_id)
+      const newWidgetData = createDataFromLegacyWidgetData(legacyWidget)
+
+      // Save widget.
+      await updateUserWidgetData(migrationOverride, userId, newWidgetId, newWidgetData)
+    } catch (err) {
+      throw err
+    }
+  }))
+}
+
+export const handler = function (event) {
+  var body
+  try {
+    body = JSON.parse(event.body)
+  } catch (e) {
+    logger.error(e)
+    return Promise.resolve(createResponse(500, e))
+  }
+
+  // Check admin authorization
+  const headers = event.headers
+  if (!headers || headers['Authorization'] !== config.SENTRY_PRIVATE_KEY) {
+    return Promise.resolve(createResponse(403, 'Not authorized.'))
+  }
+
+  const userId = body.userId
+  const legacyWidgets = body.widgets
+
+  return migrateWidgets(userId, legacyWidgets)
+    .then(response => createResponse(200, { status: 'success' }))
+    .catch(err => {
+      logger.error(err)
+      return createResponse(500, err)
+    })
+}
+
+export const serverlessHandler = function (event, context, callback) {
+  handler(event)
+    .then(response => callback(null, response))
+}
