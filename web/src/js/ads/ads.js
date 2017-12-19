adb8058f946ae55d192e67e1ce18e2bd4ecf4ad8
Kevin Jennison <kevin.jennison1@gmail.com>
2017-12-19 17:51:17 +0000

    Revert "Merge pull request #186 from gladly-team/kevin/amazon-bidder-changes"
    This reverts commit 5df39bac7dfc36fcd05bf153fe661edc6c20662b, reversing
    changes made to 7e20ae5b1241a0c8d5692c1bdad9ea8f5daed590.

--- web/src/js/ads/ads.js
+++ web/src/js/ads/ads.js
@@ -1,15 +1,16 @@
 
 import adsEnabled from './adsEnabledStatus'
-// import { setUpGoogleTag } from './google/googleTag'
+import { setUpGoogleTag } from './google/googleTag'
 import prebid from './prebid/prebidModule'
 import prebidConfig from './prebid/prebidConfig'
 import googleTagManager from './google/googleTagManager'
 import googleAdSlotDefinitions from './google/googleAdSlotDefinitions'
-// import amazonBidder from './amazon/amazonBidder'
+import amazonBidder from './amazon/amazonBidder'
 
 if (adsEnabled) {
-  // setUpGoogleTag()
-  // amazonBidder()
+  window.adStartTime = Date.now()
+  setUpGoogleTag()
+  amazonBidder()
   prebid()
   prebidConfig()
   googleTagManager()
