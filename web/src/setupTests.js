ba56612fe2f527e3697bbf9b5393128869585d43
Kevin Jennison <kevin.jennison1@gmail.com>
2018-02-21 16:09:36 -0800

    Add a mock localStorage to `global` in tests

--- web/src/setupTests.js
+++ web/src/setupTests.js
@@ -1,3 +1,4 @@
+/* eslint-env jest */
 
 // Used in package.json Jest configuration
 // and run before tests
@@ -6,4 +7,13 @@ import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-15'
 import 'babel-polyfill'
 
+// Initialize Enzyme
 configure({ adapter: new Adapter() })
+
+// Add a mock localStorage
+const localStorageMock = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  clear: jest.fn()
+}
+global.localStorage = localStorageMock
