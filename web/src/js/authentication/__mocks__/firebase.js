7b70c797dbf3fe14930f9f21ba42cfe1428294a0
Kevin Jennison <kevin.jennison1@gmail.com>
2018-02-21 20:43:19 -0800

    Add some notes

--- web/src/js/authentication/__mocks__/firebase.js
+++ web/src/js/authentication/__mocks__/firebase.js
@@ -8,3 +8,17 @@ const config = {
   projectId: 'xyz'
 }
 firebase.initializeApp(config)
+
+// In Jest tests when mounting firebaseui-web, getting error:
+// "The current environment does not support the specified persistence type"
+// https://github.com/firebase/firebase-js-sdk/blob/369b411b52de56ad38c82993c9dca4b6bd7b82a4/packages/auth/src/authstorage.js#L106
+// Should be able to set persistence to 'none' with:
+//   firebase.auth()
+//     .setPersistence(firebase.auth.Auth.Persistence.NONE)
+// https://firebase.google.com/docs/auth/web/auth-state-persistence#supported_types_of_auth_state_persistence
+
+// firebase.auth()
+//   .setPersistence(firebase.auth.Auth.Persistence.NONE)
+//   .then(() => {
+//     console.log('Set Firebase persistence!')
+//   })
