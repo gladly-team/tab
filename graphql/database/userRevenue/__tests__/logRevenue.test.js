c61347c7b4336499c4347fd3bc07c092a938865e
Kevin Jennison <kevin.jennison1@gmail.com>
2018-03-12 17:14:05 -0700

    Reenable revenue logging, using string type for `dfpAdvertiserId`

--- graphql/database/userRevenue/__tests__/logRevenue.test.js
+++ graphql/database/userRevenue/__tests__/logRevenue.test.js
@@ -1,17 +1,17 @@
 /* eslint-env jest */
 
-// import moment from 'moment'
-// import UserRevenueModel from '../UserRevenueModel'
-// import logRevenue from '../logRevenue'
+import moment from 'moment'
+import UserRevenueModel from '../UserRevenueModel'
+import logRevenue from '../logRevenue'
 import {
-  // addTimestampFieldsToItem,
-  // getMockUserContext,
+  addTimestampFieldsToItem,
+  getMockUserContext,
   mockDate
 } from '../../test-utils'
 
 jest.mock('../../databaseClient')
 
-// const userContext = getMockUserContext()
+const userContext = getMockUserContext()
 const mockCurrentTime = '2017-06-22T01:13:28.000Z'
 
 beforeAll(() => {
@@ -25,38 +25,34 @@ afterAll(() => {
 })
 
 describe('logRevenue', () => {
-  test('foo', () => {
-    expect(true).toBe(true)
+  test('calls DB to create item', async () => {
+    const userId = userContext.id
+    const userRevenueCreate = jest.spyOn(UserRevenueModel, 'create')
+    await logRevenue(userContext, userId, 0.0172, '2468')
+
+    expect(userRevenueCreate).toHaveBeenLastCalledWith(
+      userContext,
+      addTimestampFieldsToItem({
+        userId: userId,
+        timestamp: moment.utc().toISOString(),
+        revenue: 0.0172,
+        dfpAdvertiserId: '2468'
+      })
+    )
   })
 
-  // test('calls DB to create item', async () => {
-  //   const userId = userContext.id
-  //   const userRevenueCreate = jest.spyOn(UserRevenueModel, 'create')
-  //   await logRevenue(userContext, userId, 0.0172, 2468)
+  test('dfpAdvertiserId is optional', async () => {
+    const userId = userContext.id
+    const userRevenueCreate = jest.spyOn(UserRevenueModel, 'create')
+    await logRevenue(userContext, userId, 0.0172)
 
-  //   expect(userRevenueCreate).toHaveBeenLastCalledWith(
-  //     userContext,
-  //     addTimestampFieldsToItem({
-  //       userId: userId,
-  //       timestamp: moment.utc().toISOString(),
-  //       revenue: 0.0172,
-  //       dfpAdvertiserId: 2468
-  //     })
-  //   )
-  // })
-
-  // test('dfpAdvertiserId is optional', async () => {
-  //   const userId = userContext.id
-  //   const userRevenueCreate = jest.spyOn(UserRevenueModel, 'create')
-  //   await logRevenue(userContext, userId, 0.0172)
-
-  //   expect(userRevenueCreate).toHaveBeenLastCalledWith(
-  //     userContext,
-  //     addTimestampFieldsToItem({
-  //       userId: userId,
-  //       timestamp: moment.utc().toISOString(),
-  //       revenue: 0.0172
-  //     })
-  //   )
-  // })
+    expect(userRevenueCreate).toHaveBeenLastCalledWith(
+      userContext,
+      addTimestampFieldsToItem({
+        userId: userId,
+        timestamp: moment.utc().toISOString(),
+        revenue: 0.0172
+      })
+    )
+  })
 })
