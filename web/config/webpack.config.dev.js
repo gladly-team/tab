b40b64206a5d4597f880482b1993d70e5a15bc9d
Kevin Jennison <kevin.jennison1@gmail.com>
2018-03-23 10:33:05 -0700

    Use autodll-plugin in development

--- web/config/webpack.config.dev.js
+++ web/config/webpack.config.dev.js
@@ -1,6 +1,7 @@
 
 var path = require('path')
 var webpack = require('webpack')
+var AutoDllPlugin = require('autodll-webpack-plugin')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
@@ -20,22 +21,8 @@ var paths = require('./paths')
 const htmlTemplate = new HtmlWebpackPlugin({
   title: 'Tab for a Cause',
   template: paths.appHtml,
-  // https://github.com/jantimon/html-webpack-plugin/issues/481#issuecomment-262414169
-  chunks: ['vendor', 'ads', 'app'],
-  chunksSortMode: function (chunk1, chunk2) {
-    var orders = ['vendor', 'ads', 'app']
-    var order1 = orders.indexOf(chunk1.names[0])
-    var order2 = orders.indexOf(chunk2.names[0])
-    if (order1 > order2) {
-      return 1
-    } else if (order1 < order2) {
-      return -1
-    } else {
-      return 0
-    }
-  },
   mobile: true,
-  inject: false,
+  inject: true,
   tabAdsEnabled: process.env.ADS_ENABLED === 'true'
 })
 const favIcon = new FaviconsWebpackPlugin(paths.appLogo)
@@ -204,15 +191,6 @@ module.exports = {
     ]
   },
   plugins: [
-    // https://medium.com/@adamrackis/vendor-and-code-splitting-in-webpack-2-6376358f1923
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      filename: 'vendor.js',
-      minChunks (module, count) {
-        var context = module.context
-        return context && context.indexOf('node_modules') >= 0
-      }
-    }),
     // Helps understand what's getting included in our final bundle.
     // With this enabled, build the web app and view report.html in
     // the build directory.
@@ -228,6 +206,24 @@ module.exports = {
     // if (process.env.NODE_ENV === 'development') { ... }. See `./env.js`.
     new webpack.DefinePlugin(env),
     htmlTemplate,
+    new AutoDllPlugin({
+      // context: path.join(__dirname, '..'),
+      inject: true, // will inject the DLL bundle to index.html
+      debug: true,
+      filename: '[name].[hash].js',
+      path: './build/vendor',
+      entry: {
+        vendor: [
+          './config/vendor.js'
+        ]
+      },
+      plugins: [
+        new BundleAnalyzerPlugin({
+          // set to 'static' for analysis or 'disabled' for none
+          analyzerMode: 'disabled'
+        })
+      ]
+    }),
     favIcon
   ],
   // Some libraries import Node modules but don't use them in the browser.
