3f266fa8129b2df961633b05aa5f1efb663cd3a5
Kevin Jennison <kevin.jennison1@gmail.com>
2018-04-02 12:06:19 -0700

    Add Sentry logging to client JS

--- web/src/index.js
+++ web/src/index.js
@@ -1,32 +1,49 @@
 import 'babel-polyfill'
+import Raven from 'raven-js'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
 import Root from './root'
-
-// Init Firebase
 import { initializeFirebase } from 'authentication/firebaseConfig'
-initializeFirebase()
 
-require('./index.css')
+// Start Sentry logger
+// https://docs.sentry.io/clients/javascript/config/
+Raven.config(process.env.WEB_SENTRY_DSN, {
+  environment: process.env.STAGE,
+  debug: process.env.WEB_SENTRY_DEBUG === 'true'
+}).install()
 
-const rootNode = document.createElement('div')
-document.body.appendChild(rootNode)
+const initApp = () => {
+  // Init Firebase
+  initializeFirebase()
 
-const render = (Component) => {
-  ReactDOM.render(
-    <AppContainer >
-      <Component />
-    </AppContainer>,
-    rootNode
-  )
+  require('./index.css')
+
+  const rootNode = document.createElement('div')
+  document.body.appendChild(rootNode)
+
+  const render = (Component) => {
+    ReactDOM.render(
+      <AppContainer >
+        <Component />
+      </AppContainer>,
+      rootNode
+    )
+  }
+
+  render(Root)
+
+  // Hot Module Replacement API
+  if (module.hot) {
+    module.hot.accept('./root', () => {
+      render(root)
+    })
+  }
 }
 
-render(Root)
-
-// Hot Module Replacement API
-if (module.hot) {
-  module.hot.accept('./root', () => {
-    render(root)
-  })
-}
+Raven.context(() => {
+  if (process.env.WEB_SENTRY_DEBUG === 'true') {
+    console.log(`Initialized Raven for Sentry DSN ${process.env.WEB_SENTRY_DSN}`)
+  }
+  initApp()
+})
