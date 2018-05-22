eda5d15ddeef1c73fe7d8f6f5bc492d858da2b06
Kevin Jennison <kevin.jennison1@gmail.com>
2018-05-22 12:50:30 -0700

    Add some documentation

--- web/src/js/ads/__tests__/consentManagement.test.js
+++ web/src/js/ads/__tests__/consentManagement.test.js
@@ -2,7 +2,42 @@
 
 beforeEach(() => {
   // Mock CMP
-  window.__cmp = jest.fn()
+  window.__cmp = jest.fn((command, version, callback) => {
+    // Documenting available commands for Quantcast CMP.
+    // https://quantcast.zendesk.com/hc/en-us/articles/360003814853-Technical-Implementation-Guide
+    // IAB standard docs:
+    // https://github.com/InteractiveAdvertisingBureau/GDPR-Transparency-and-Consent-Framework/blob/master/CMP%20JS%20API%20v1.1%20Final.md#what-api-will-need-to-be-provided-by-the-cmp-
+    switch (command) {
+      case 'displayConsentUi':
+        break
+      case 'getConfig':
+        break
+      case 'getCurrentVendorConsents':
+        break
+      case 'getConsentData':
+        break
+      case 'getPublisherConsents':
+        break
+      case 'getCurrentPublisherConsents':
+        break
+      case 'getVendorConsents':
+        break
+      case 'getVendorList':
+        break
+      case 'init':
+        break
+      case 'initConfig':
+        break
+      case 'runConsentUiCallback':
+        break
+      case 'saveConsents':
+        break
+      case 'setConsentUiCallback':
+        break
+      default:
+        throw new Error('Invalid CMP command')
+    }
+  })
 })
 
 afterEach(() => {
