276f34adb0e692e102071aa2776dca625f834d70
Kevin Jennison <kevin.jennison1@gmail.com>
2018-05-22 18:24:55 -0700

    Add TODOs

--- web/src/js/components/Settings/SettingsChildWrapperComponent.js
+++ web/src/js/components/Settings/SettingsChildWrapperComponent.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import FadeInAnimation from 'general/FadeInAnimation'
 
+// TODO: consolidate progress loader logic here with a "dataLoaded"
+// component for all settings sub-views. Currently, each settings
+// view is handling its own loader.
+// TODO: style to fill remaining space so that the loader can be
+// centered without using vh or vw.
 class SettingsChildWrapper extends React.Component {
   render () {
     const container = {
--- web/src/js/components/Settings/SettingsPageComponent.js
+++ web/src/js/components/Settings/SettingsPageComponent.js
@@ -26,6 +26,12 @@ const styles = theme => ({
   }
 })
 
+// TODO:
+// - active style for menu item
+// - hover style for menu item
+// - fix "invite friends" text box color
+// - subheader for sections? possibly remove title from top-left?
+// - add "Account" page with email address and username
 class SettingsPage extends React.Component {
   constructor (props) {
     super(props)
