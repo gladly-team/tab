3892fbb61e4a43a9dbecd10f921f0764bb474ed4
Kevin Jennison <kevin.jennison1@gmail.com>
2018-06-01 10:21:18 -0700

    Revert "Remove Sonobi bid adapter"
    This reverts commit 24e55124a99385ae55101637ad1fc240cb7b91a0.

--- web/src/js/ads/prebid/prebidConfig.js
+++ web/src/js/ads/prebid/prebidConfig.js
@@ -27,6 +27,13 @@ export default function (isInEU) {
     },
     bids: [
       {
+        bidder: 'sonobi',
+        params: {
+          dom_id: 'div-gpt-ad-1464385742501-0',
+          ad_unit: '/43865596/HBTR'
+        }
+      },
+      {
         bidder: 'pulsepoint',
         params: {
           cf: '300X250',
@@ -80,6 +87,13 @@ export default function (isInEU) {
       }
     },
     bids: [
+      {
+        bidder: 'sonobi',
+        params: {
+          dom_id: 'div-gpt-ad-1464385677836-0',
+          ad_unit: '/43865596/HBTL'
+        }
+      },
       {
         bidder: 'pulsepoint',
         params: {
