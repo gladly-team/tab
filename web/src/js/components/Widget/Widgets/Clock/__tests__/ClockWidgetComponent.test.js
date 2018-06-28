37a785f18ac0236749adf6b4a26126727d7f9922
Kevin Jennison <kevin.jennison1@gmail.com>
2018-06-28 16:44:33 -0700

    Show two leading zeros when midnight in 24h format

--- web/src/js/components/Widget/Widgets/Clock/__tests__/ClockWidgetComponent.test.js
+++ web/src/js/components/Widget/Widgets/Clock/__tests__/ClockWidgetComponent.test.js
@@ -4,6 +4,18 @@ import React from 'react'
 import {
   shallow
 } from 'enzyme'
+import MockDate from 'mockdate'
+import moment from 'moment'
+
+const mockNow = '2018-05-15T14:31:43.130'
+
+beforeAll(() => {
+  MockDate.set(moment(mockNow))
+})
+
+afterAll(() => {
+  MockDate.reset()
+})
 
 const mockProps = {
   user: {
@@ -13,7 +25,10 @@ const mockProps = {
     id: 'widget-xyz',
     name: 'Clock',
     enabled: true,
-    config: JSON.stringify([
+    config: JSON.stringify({
+      format24: false
+    }),
+    settings: JSON.stringify([
       {
         'field': 'format24',
         'type': 'boolean',
@@ -21,9 +36,6 @@ const mockProps = {
         'defaultValue': false
       }
     ]),
-    settings: JSON.stringify({
-      format24: true
-    }),
     type: 'clock'
   }
 }
@@ -34,5 +46,67 @@ describe('Clock widget  component', () => {
     shallow(
       <ClockWidgetComponent {...mockProps} />
     )
+  })
+
+  it('shows the correct time', () => {
+    const ClockWidgetComponent = require('../ClockWidgetComponent').default
+    const mockNow = '2018-05-15T14:31:43.130'
+    MockDate.set(moment(mockNow))
+    const wrapper = shallow(
+      <ClockWidgetComponent {...mockProps} />
+    )
+    expect(wrapper.find('[data-test-id="clock-widget-time"]').text()).toEqual('2:31')
+  })
+
+  it('shows the correct date', () => {
+    const ClockWidgetComponent = require('../ClockWidgetComponent').default
+    const mockNow = '2018-05-15T14:31:43.130'
+    MockDate.set(moment(mockNow))
+    const wrapper = shallow(
+      <ClockWidgetComponent {...mockProps} />
+    )
+    expect(wrapper.find('[data-test-id="clock-widget-date"]').text()).toEqual('Tuesday, May 15')
+  })
+
+  it('shows the correct time in 24h format in the morning', () => {
+    const ClockWidgetComponent = require('../ClockWidgetComponent').default
+    const props = Object.assign({}, mockProps)
+    props.widget.config = JSON.stringify({
+      format24: true
+    })
+    const mockNow = '2018-05-15T08:58:22.130'
+    MockDate.set(moment(mockNow))
+    const wrapper = shallow(
+      <ClockWidgetComponent {...mockProps} />
+    )
+    expect(wrapper.find('[data-test-id="clock-widget-time"]').text()).toEqual('8:58')
+  })
+
+  it('shows the correct time in 24h format in the afternoon', () => {
+    const ClockWidgetComponent = require('../ClockWidgetComponent').default
+    const props = Object.assign({}, mockProps)
+    props.widget.config = JSON.stringify({
+      format24: true
+    })
+    const mockNow = '2018-05-15T14:31:43.130'
+    MockDate.set(moment(mockNow))
+    const wrapper = shallow(
+      <ClockWidgetComponent {...mockProps} />
+    )
+    expect(wrapper.find('[data-test-id="clock-widget-time"]').text()).toEqual('14:31')
+  })
+
+  it('midnight is correct in 24h format', () => {
+    const ClockWidgetComponent = require('../ClockWidgetComponent').default
+    const props = Object.assign({}, mockProps)
+    props.widget.config = JSON.stringify({
+      format24: true
+    })
+    const mockNow = '2018-05-15T00:16:02.130'
+    MockDate.set(moment(mockNow))
+    const wrapper = shallow(
+      <ClockWidgetComponent {...mockProps} />
+    )
+    expect(wrapper.find('[data-test-id="clock-widget-time"]').text()).toEqual('00:16')
   })
 })
