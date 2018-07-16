9a1eb28a897f0ad80cb7ef3f95f2a38f04aa914c
Kevin Jennison <kevin.jennison1@gmail.com>
2018-07-16 12:38:55 -0400

    Add another test for starting the Joyride tour

--- web/src/js/components/Dashboard/__tests__/NewUserTourComponent.test.js
+++ web/src/js/components/Dashboard/__tests__/NewUserTourComponent.test.js
@@ -6,6 +6,7 @@ import {
 } from 'enzyme'
 import { cloneDeep } from 'lodash/lang'
 import Joyride from 'react-joyride'
+import Button from '@material-ui/core/Button'
 import Dialog from 'material-ui/Dialog'
 import localStorageMgr from 'utils/localstorage-mgr'
 import { STORAGE_NEW_USER_HAS_COMPLETED_TOUR } from '../../../constants'
@@ -46,6 +47,23 @@ describe('New user tour component', () => {
     const introModal = wrapper.find(Dialog).first()
     expect(introModal.prop('open')).toBe(true)
     expect(introModal.prop('title')).toBe('Your tabs are changing the world!')
+  })
+
+  it('does not run the Joyride tour until clicking through the first modal', () => {
+    const NewUserTourComponent = require('../NewUserTourComponent').default
+    const wrapper = shallow(
+      <NewUserTourComponent {...mockProps} />
+    )
+    expect(wrapper.find(Joyride).first().prop('run')).toBe(false)
+
+    // Mock a button click on the intro modal
+    // It would be better to simulate a click on the button,
+    // but that's difficult with the old Material UI dialog.
+    // @material-ui-1-todo: find the dialog button and simulate a
+    //   click instead of calling the method directly.
+    wrapper.instance().introModalButtonClick()
+    wrapper.update()
+    expect(wrapper.find(Joyride).first().prop('run')).toBe(true)
   })
 
   it('does not initially show the final dialog', () => {
