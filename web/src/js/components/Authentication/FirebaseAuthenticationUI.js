2261e7d7e16df84f30833e9f0f19c4ba9381170f
Kevin Jennison <kevin.jennison1@gmail.com>
2018-07-30 11:42:23 -0700

    Change Firebase entry points

--- web/src/js/components/Authentication/FirebaseAuthenticationUI.js
+++ web/src/js/components/Authentication/FirebaseAuthenticationUI.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import * as firebase from 'firebase'
+import firebase from 'firebase/app'
+import 'firebase/auth'
 import { FirebaseAuth } from 'react-firebaseui'
 import {
   dashboardURL
