f30b33efc2f40d79c2b52cae8ea351df72072747
Kevin Jennison <kevin.jennison1@gmail.com>
2018-09-20 14:05:41 -0700

    Create widgets if they don’t exist when enabling or updating config

--- graphql/database/widgets/userWidget/__tests__/updateUserWidgetConfig.test.js
+++ graphql/database/widgets/userWidget/__tests__/updateUserWidgetConfig.test.js
@@ -28,6 +28,8 @@ afterEach(() => {
 
 describe('updateUserWidgetConfig', () => {
   it('works as expected', async () => {
+    expect.assertions(2)
+
     const userInfo = getMockUserInfo()
     const userWidget = new UserWidgetModel({
       userId: userInfo.id,
@@ -60,5 +62,132 @@ describe('updateUserWidgetConfig', () => {
         config: newConfig
       })
     expect(updatedWidget).toEqual(expectedWidget)
+  })
+
+  it('tries to create the object if it does not already exist', async () => {
+    expect.assertions(1)
+
+    const userInfo = getMockUserInfo()
+    const userWidgetCreate = jest.spyOn(UserWidgetModel, 'create')
+
+    // Set that the item doesn't exist when updating.
+    setMockDBResponse(
+      DatabaseOperation.UPDATE,
+      null,
+      { code: 'ConditionalCheckFailedException' }
+    )
+
+    // Set the return for the created object.
+    const userWidget = new UserWidgetModel({
+      userId: userInfo.id,
+      widgetId: 'ab5082cc-151a-4a9a-9289-06906670fd4e',
+      enabled: false,
+      config: {
+        foo: 'bar'
+      }
+    })
+    const newConfig = { foo: 'boop' }
+    const expectedWidget = Object.assign({}, userWidget, {
+      config: newConfig,
+      updated: moment.utc().toISOString()
+    })
+    setMockDBResponse(
+      DatabaseOperation.CREATE,
+      {
+        Attributes: expectedWidget
+      }
+    )
+
+    await updateUserWidgetConfig(userContext, userInfo.id,
+      userWidget.widgetId, newConfig)
+    expect(userWidgetCreate).toHaveBeenCalledWith(userContext, {
+      userId: userInfo.id,
+      widgetId: userWidget.widgetId,
+      created: moment.utc().toISOString(),
+      updated: moment.utc().toISOString(),
+      config: newConfig
+    })
+  })
+
+  it('returns the created object if it does not already exist', async () => {
+    expect.assertions(1)
+
+    const userInfo = getMockUserInfo()
+
+    // Set that the item doesn't exist when updating.
+    setMockDBResponse(
+      DatabaseOperation.UPDATE,
+      null,
+      { code: 'ConditionalCheckFailedException' }
+    )
+
+    // Set the return for the created object.
+    const userWidget = new UserWidgetModel({
+      userId: userInfo.id,
+      widgetId: 'ab5082cc-151a-4a9a-9289-06906670fd4e',
+      enabled: false,
+      config: {
+        foo: 'bar'
+      }
+    })
+    const newConfig = { foo: 'boop' }
+    const expectedWidget = Object.assign({}, userWidget, {
+      config: newConfig,
+      created: moment.utc().toISOString(),
+      updated: moment.utc().toISOString()
+    })
+    setMockDBResponse(
+      DatabaseOperation.CREATE,
+      {
+        Attributes: expectedWidget
+      }
+    )
+
+    const returnedUserWidget = await updateUserWidgetConfig(userContext,
+      userInfo.id, userWidget.widgetId, newConfig)
+    expect(returnedUserWidget).toEqual(expectedWidget)
+  })
+
+  it('throws when there is an update error other than failed conditional check', async () => {
+    expect.assertions(1)
+
+    const userInfo = getMockUserInfo()
+    const widgetId = 'ab5082cc-151a-4a9a-9289-06906670fd4e'
+
+    // Set some other update error.
+    setMockDBResponse(
+      DatabaseOperation.UPDATE,
+      null,
+      { code: 'SomethingElse' }
+    )
+
+    return expect(updateUserWidgetConfig(userContext,
+        userInfo.id, widgetId, { some: 'config' }))
+      .rejects.toThrow()
+  })
+
+  it('throws when there is a create error', async () => {
+    expect.assertions(1)
+
+    const userInfo = getMockUserInfo()
+    const widgetId = 'ab5082cc-151a-4a9a-9289-06906670fd4e'
+
+    // Set that the item doesn't exist when updating.
+    setMockDBResponse(
+      DatabaseOperation.UPDATE,
+      null,
+      { code: 'ConditionalCheckFailedException' }
+    )
+
+    // Set some error during creation.
+    setMockDBResponse(
+      DatabaseOperation.CREATE,
+      null,
+      { code: 'SomethingWentWrong' }
+    )
+
+    return expect(updateUserWidgetConfig(userContext,
+        userInfo.id, widgetId, { some: 'config' }))
+      .rejects.toThrow()
   })
 })
--- graphql/database/widgets/userWidget/__tests__/updateUserWidgetEnabled.test.js
+++ graphql/database/widgets/userWidget/__tests__/updateUserWidgetEnabled.test.js
@@ -57,4 +57,125 @@ describe('updateUserWidgetEnabled', () => {
       })
     expect(updatedWidget).toEqual(expectedWidget)
   })
+
+  it('tries to create the object if it does not already exist', async () => {
+    expect.assertions(1)
+
+    const userInfo = getMockUserInfo()
+    const userWidgetCreate = jest.spyOn(UserWidgetModel, 'create')
+
+    // Set that the item doesn't exist when updating.
+    setMockDBResponse(
+      DatabaseOperation.UPDATE,
+      null,
+      { code: 'ConditionalCheckFailedException' }
+    )
+
+    // Set the return for the created object.
+    const userWidget = new UserWidgetModel({
+      userId: userInfo.id,
+      widgetId: 'ab5082cc-151a-4a9a-9289-06906670fd4e',
+      enabled: false
+    })
+    const newEnabledStatus = true
+    const expectedWidget = Object.assign({}, userWidget, {
+      enabled: newEnabledStatus,
+      updated: moment.utc().toISOString()
+    })
+    setMockDBResponse(
+      DatabaseOperation.CREATE,
+      {
+        Attributes: expectedWidget
+      }
+    )
+
+    await updateUserWidgetEnabled(userContext, userInfo.id,
+      userWidget.widgetId, newEnabledStatus)
+    expect(userWidgetCreate).toHaveBeenCalledWith(userContext, {
+      userId: userInfo.id,
+      widgetId: userWidget.widgetId,
+      created: moment.utc().toISOString(),
+      updated: moment.utc().toISOString(),
+      enabled: newEnabledStatus
+    })
+  })
+
+  it('returns the created object if it does not already exist', async () => {
+    expect.assertions(1)
+
+    const userInfo = getMockUserInfo()
+
+    // Set that the item doesn't exist when updating.
+    setMockDBResponse(
+      DatabaseOperation.UPDATE,
+      null,
+      { code: 'ConditionalCheckFailedException' }
+    )
+
+    // Set the return for the created object.
+    const userWidget = new UserWidgetModel({
+      userId: userInfo.id,
+      widgetId: 'ab5082cc-151a-4a9a-9289-06906670fd4e',
+      enabled: false
+    })
+    const newEnabledStatus = true
+    const expectedWidget = Object.assign({}, userWidget, {
+      enabled: newEnabledStatus,
+      created: moment.utc().toISOString(),
+      updated: moment.utc().toISOString()
+    })
+    setMockDBResponse(
+      DatabaseOperation.CREATE,
+      {
+        Attributes: expectedWidget
+      }
+    )
+
+    const returnedUserWidget = await updateUserWidgetEnabled(userContext,
+      userInfo.id, userWidget.widgetId, newEnabledStatus)
+    expect(returnedUserWidget).toEqual(expectedWidget)
+  })
+
+  it('throws when there is an update error other than failed conditional check', async () => {
+    expect.assertions(1)
+
+    const userInfo = getMockUserInfo()
+    const widgetId = 'ab5082cc-151a-4a9a-9289-06906670fd4e'
+
+    // Set some other update error.
+    setMockDBResponse(
+      DatabaseOperation.UPDATE,
+      null,
+      { code: 'SomethingElse' }
+    )
+
+    return expect(updateUserWidgetEnabled(userContext,
+        userInfo.id, widgetId, true))
+      .rejects.toThrow()
+  })
+
+  it('throws when there is a create error', async () => {
+    expect.assertions(1)
+
+    const userInfo = getMockUserInfo()
+    const widgetId = 'ab5082cc-151a-4a9a-9289-06906670fd4e'
+
+    // Set that the item doesn't exist when updating.
+    setMockDBResponse(
+      DatabaseOperation.UPDATE,
+      null,
+      { code: 'ConditionalCheckFailedException' }
+    )
+
+    // Set some error during creation.
+    setMockDBResponse(
+      DatabaseOperation.CREATE,
+      null,
+      { code: 'SomethingWentWrong' }
+    )
+
+    return expect(updateUserWidgetEnabled(userContext,
+        userInfo.id, widgetId, true))
+      .rejects.toThrow()
+  })
 })
