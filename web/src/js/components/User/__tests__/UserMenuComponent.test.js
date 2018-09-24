8a783498656dee5ea830d14981d0f5179a5298ab
Kevin Jennison <kevin.jennison1@gmail.com>
2018-09-23 20:16:24 -0700

    Hide logout button for anonymous users

--- web/src/js/components/User/__tests__/UserMenuComponent.test.js
+++ web/src/js/components/User/__tests__/UserMenuComponent.test.js
@@ -4,23 +4,28 @@ import React from 'react'
 import {
   shallow
 } from 'enzyme'
+import MenuItem from 'material-ui/MenuItem'
 
-const mockProps = {
-  user: {
-    vcCurrent: 32,
-    level: 8,
-    heartsUntilNextLevel: 14,
-    vcDonatedAllTime: 90,
-    numUsersRecruited: 1,
-    tabsToday: 4
-  },
-  app: {
-    referralVcReward: 350
+const MockProps = () => {
+  return {
+    user: {
+      vcCurrent: 32,
+      level: 8,
+      heartsUntilNextLevel: 14,
+      vcDonatedAllTime: 90,
+      numUsersRecruited: 1,
+      tabsToday: 4
+    },
+    app: {
+      referralVcReward: 350
+    },
+    isUserAnonymous: false
   }
 }
 
 describe('User menu component', () => {
   it('renders without error', () => {
+    const mockProps = MockProps()
     const UserMenuComponent = require('../UserMenuComponent').default
     shallow(
       <UserMenuComponent {...mockProps} />
@@ -28,6 +33,7 @@ describe('User menu component', () => {
   })
 
   it('contains an ID for the new user tour (to showcase hearts)', () => {
+    const mockProps = MockProps()
     const UserMenuComponent = require('../UserMenuComponent').default
     const wrapper = shallow(
       <UserMenuComponent {...mockProps} />
@@ -40,6 +46,7 @@ describe('User menu component', () => {
   })
 
   it('contains an ID for the new user tour (to showcase the settings button)', () => {
+    const mockProps = MockProps()
     const UserMenuComponent = require('../UserMenuComponent').default
     const wrapper = shallow(
       <UserMenuComponent {...mockProps} />
@@ -49,5 +56,30 @@ describe('User menu component', () => {
     // new user tour. Do not change it without updating it elsewhere.
     expect(wrapper.find('[data-tour-id="settings-button"]').length)
       .toBe(1)
+  })
+
+  it('contains a "sign out" menu item', () => {
+    const mockProps = MockProps()
+    const UserMenuComponent = require('../UserMenuComponent').default
+    const wrapper = shallow(
+      <UserMenuComponent {...mockProps} />
+    )
+    const hasSignOut = wrapper.find(MenuItem).filterWhere(elem => {
+      return elem.prop('primaryText') === 'Sign Out'
+    }).length === 1
+    expect(hasSignOut).toBe(true)
+  })
+
+  it('does not contain a "sign out" menu item if the user is anonymous', () => {
+    const mockProps = MockProps()
+    mockProps.isUserAnonymous = true
+    const UserMenuComponent = require('../UserMenuComponent').default
+    const wrapper = shallow(
+      <UserMenuComponent {...mockProps} />
+    )
+    const hasSignOut = wrapper.find(MenuItem).filterWhere(elem => {
+      return elem.prop('primaryText') === 'Sign Out'
+    }).length === 1
+    expect(hasSignOut).toBe(false)
   })
 })
