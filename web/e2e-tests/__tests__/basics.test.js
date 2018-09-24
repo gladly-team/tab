729e86f42f20afb20bd1909e3a35b9a7e651b3a0
Kevin Jennison <kevin.jennison1@gmail.com>
2018-09-23 20:23:47 -0700

    Disable the authentication redirect acceptance test, which will break due to split-testing

--- web/e2e-tests/__tests__/basics.test.js
+++ web/e2e-tests/__tests__/basics.test.js
@@ -1,8 +1,11 @@
 /* eslint-env jest */
 /* globals jasmine */
 
-import driverUtils from '../utils/driver-utils'
-import { getDriver, getAppBaseUrl } from '../utils/driver-mgr'
+// import driverUtils from '../utils/driver-utils'
+// import {
+//   getDriver,
+//   getAppBaseUrl
+// } from '../utils/driver-mgr'
 
 let driver
 afterEach(() => {
@@ -11,15 +14,23 @@ afterEach(() => {
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 35e3
 
-const getAbsoluteUrl = (relativeUrl) => {
-  return `${getAppBaseUrl()}${relativeUrl}`
-}
+// const getAbsoluteUrl = (relativeUrl) => {
+//   return `${getAppBaseUrl()}${relativeUrl}`
+// }
 
 // Sanity checking that the app deployed and loads correctly
 describe('Basic integration tests', () => {
-  it('should load the auth page', async () => {
-    driver = getDriver('Basic integration tests: should load auth page')
-    await driverUtils(driver).navigateTo(getAbsoluteUrl('/newtab/'))
-    await driverUtils(driver).waitForElementExistsByTestId('authentication-page')
-  }, 30e3)
+  // @experiment-anon-sign-in
+  // TODO: re-enable after completing the anonymous sign-in experiement.
+  //   The random split-test will break this test.
+
+  // it('should load the auth page', async () => {
+  //   driver = getDriver('Basic integration tests: should load auth page')
+  //   await driverUtils(driver).navigateTo(getAbsoluteUrl('/newtab/'))
+  //   await driverUtils(driver).waitForElementExistsByTestId('authentication-page')
+  // }, 30e3)
+
+  it('should pass this placeholder test', () => {
+    expect(true).toBe(true)
+  })
 })
--- web/src/js/utils/feature-flags.js
+++ web/src/js/utils/feature-flags.js
@@ -1,4 +1,5 @@
 
+// @experiment-anon-sign-in
 export const isAnonymousUserSignInEnabled = () => {
   return process.env.FEATURE_FLAG_ANON_USER_SIGN_IN
     ? process.env.FEATURE_FLAG_ANON_USER_SIGN_IN === 'true'
