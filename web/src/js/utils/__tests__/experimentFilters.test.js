99edb603dceb8a6d79d9f1fb9005c2d1adebd3e6
Kevin Jennison <kevin.jennison1@gmail.com>
2018-11-05 16:02:46 -0800

    Add more experiment filter utils

--- web/src/js/utils/__tests__/experimentFilters.test.js
+++ web/src/js/utils/__tests__/experimentFilters.test.js
@@ -31,11 +31,75 @@ describe('experiment filter', () => {
 
   test('excludeUsersWhoJoinedRecently works as expected with "seconds" unit', () => {
     const { excludeUsersWhoJoinedRecently } = require('js/utils/experimentFilters')
-    const excludeLastThirtyDays = excludeUsersWhoJoinedRecently(10, 'seconds')
+    const excludeLastTenSeconds = excludeUsersWhoJoinedRecently(10, 'seconds')
     const mockUserInfo = getMockUserInfo()
     mockUserInfo.joined = '2017-05-19T13:59:56.000Z' // 2 seconds ago
-    expect(excludeLastThirtyDays(mockUserInfo)).toBe(false)
-    mockUserInfo.joined = '2017-05-19T13:59:46.000Z' // 12 seconds days ago
-    expect(excludeLastThirtyDays(mockUserInfo)).toBe(true)
+    expect(excludeLastTenSeconds(mockUserInfo)).toBe(false)
+    mockUserInfo.joined = '2017-05-19T13:59:46.000Z' // 12 seconds ago
+    expect(excludeLastTenSeconds(mockUserInfo)).toBe(true)
+  })
+
+  test('onlyIncludeNewUsers works as expected', () => {
+    const { onlyIncludeNewUsers } = require('js/utils/experimentFilters')
+    const mockUserInfo = getMockUserInfo()
+    mockUserInfo.isNewUser = true
+    expect(onlyIncludeNewUsers(mockUserInfo)).toBe(true)
+    mockUserInfo.isNewUser = false
+    expect(onlyIncludeNewUsers(mockUserInfo)).toBe(false)
+  })
+
+  test('includeIfAnyIsTrue works as expected', () => {
+    const { includeIfAnyIsTrue } = require('js/utils/experimentFilters')
+    const filtersWithNoneTrue = [
+      () => false,
+      () => false,
+      () => false
+    ]
+    const filtersWithOneTrue = [
+      () => false,
+      () => true,
+      () => false
+    ]
+    const filtersWithAllTrue = [
+      () => true,
+      () => true,
+      () => true
+    ]
+    const mockUserInfo = getMockUserInfo()
+    expect(includeIfAnyIsTrue(filtersWithNoneTrue)(mockUserInfo)).toBe(false)
+    expect(includeIfAnyIsTrue(filtersWithOneTrue)(mockUserInfo)).toBe(true)
+    expect(includeIfAnyIsTrue(filtersWithAllTrue)(mockUserInfo)).toBe(true)
+  })
+
+  test('includeIfAnyIsTrue works as expected with user info', () => {
+    const {
+      includeIfAnyIsTrue,
+      excludeUsersWhoJoinedRecently,
+      onlyIncludeNewUsers
+    } = require('js/utils/experimentFilters')
+    const excludeLastTenDays = excludeUsersWhoJoinedRecently(10, 'days')
+    const onlyNewUsersOrUsersWhoJoinedTenDaysAgo = [
+      excludeLastTenDays,
+      onlyIncludeNewUsers
+    ]
+    const mockUserInfo = getMockUserInfo()
+
+    mockUserInfo.joined = '2017-05-11T13:59:58.000Z' // ~8 days ago
+    mockUserInfo.isNewUser = false
+    expect(
+      includeIfAnyIsTrue(onlyNewUsersOrUsersWhoJoinedTenDaysAgo)(mockUserInfo))
+      .toBe(false)
+
+    mockUserInfo.joined = '2017-05-07T13:59:58.000Z' // ~12 days ago
+    mockUserInfo.isNewUser = false
+    expect(
+      includeIfAnyIsTrue(onlyNewUsersOrUsersWhoJoinedTenDaysAgo)(mockUserInfo))
+      .toBe(true)
+
+    mockUserInfo.joined = '2017-05-19T13:59:58.000Z' // just now
+    mockUserInfo.isNewUser = true
+    expect(
+      includeIfAnyIsTrue(onlyNewUsersOrUsersWhoJoinedTenDaysAgo)(mockUserInfo))
+      .toBe(true)
   })
 })
--- web/src/js/utils/experimentFilters.js
+++ web/src/js/utils/experimentFilters.js
@@ -14,3 +14,27 @@ import moment from 'moment'
 export const excludeUsersWhoJoinedRecently = (number, units = 'days') => {
   return userInfo => moment().diff(moment(userInfo.joined), units) > number
 }
+
+/**
+ * A filter to only include new users.
+ * @param {Object} userInfo
+ * @param {String} userInfo.joined - The ISO string of when the
+ *   user joined.
+ * @param {Boolean} userInfo.isNewUser - Whether this user has just
+ *   signed up.
+ * @returns {Boolean}
+ */
+export const onlyIncludeNewUsers = userInfo => !!userInfo.isNewUser
+
+/**
+ * A meta filter function to allow for "or" logic. If at least
+ * one of the included filters returns true, this filter will
+ * return true.
+ * @param {Function[]} An array of filter functions
+ * @returns {Function}
+ */
+export const includeIfAnyIsTrue = filterFuncs => {
+  return userInfo => {
+    return filterFuncs.some(filterFunc => filterFunc(userInfo))
+  }
+}
