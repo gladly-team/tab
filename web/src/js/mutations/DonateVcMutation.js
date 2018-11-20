a3b81f6e4ee9ac9b0b5b46462be2c662e9966aad
Kevin Jennison <kevin.jennison1@gmail.com>
2018-11-20 13:27:11 -0500

    TODO

--- web/src/js/mutations/DonateVcMutation.js
+++ web/src/js/mutations/DonateVcMutation.js
@@ -13,6 +13,14 @@ const mutation = graphql`
   }
 `
 
+// TODO: refactor
+// args: inputVars obj, otherVars obj
+// return a promise
+
+// Relay tool for mutations with promises:
+// https://github.com/relay-tools/relay-commit-mutation-promise
+// A good reference for mutation design:
+// https://medium.com/entria/wrangling-the-client-store-with-the-relay-modern-updater-function-5c32149a71ac
 function commit (environment, user, charityId, vc,
   onCompleted = () => {}, onError = () => {}) {
   const userId = user.id
