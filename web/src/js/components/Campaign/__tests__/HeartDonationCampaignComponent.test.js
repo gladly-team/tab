3271507b54a09c8355ea54c8a3d7ad08bbd2cc79
Kevin Jennison <kevin.jennison1@gmail.com>
2018-11-20 12:54:40 -0500

    Add tests for the campaign

--- web/src/js/components/Campaign/__tests__/HeartDonationCampaignComponent.test.js
+++ web/src/js/components/Campaign/__tests__/HeartDonationCampaignComponent.test.js
@@ -5,6 +5,9 @@ import moment from 'moment'
 import {
   shallow
 } from 'enzyme'
+import Typography from '@material-ui/core/Typography'
+import DonateHeartsControls from 'js/components/Donate/DonateHeartsControlsContainer'
+import LinearProgress from '@material-ui/core/LinearProgress'
 
 const getMockProps = () => ({
   app: {
@@ -31,5 +34,90 @@ describe('Heart donation campaign component', () => {
         <span>Some content</span>
       </HeartDonationCampaign>
     )
+  })
+
+  it('displays the provided title', () => {
+    const HeartDonationCampaign = require('js/components/Campaign/HeartDonationCampaignComponent').default
+    const mockProps = getMockProps()
+    mockProps.campaignTitle = 'Hello there!'
+    const wrapper = shallow(
+      <HeartDonationCampaign {...mockProps}>
+        <span>Some content</span>
+      </HeartDonationCampaign>
+    )
+    expect(
+      wrapper
+        .find(Typography)
+        .filterWhere(n => {
+          return n.prop('variant') === 'h6' && n.render().text() === 'Hello there!'
+        })
+        .length
+    ).toBe(1)
+  })
+
+  it('passes the expected props to DonateHeartsControls', () => {
+    const HeartDonationCampaign = require('js/components/Campaign/HeartDonationCampaignComponent').default
+    const mockProps = getMockProps()
+    mockProps.campaignTitle = 'Hello there!'
+    const wrapper = shallow(
+      <HeartDonationCampaign {...mockProps}>
+        <span>Some content</span>
+      </HeartDonationCampaign>
+    )
+    const donateControls = wrapper.find(DonateHeartsControls)
+    expect(donateControls.prop('charity')).toEqual(mockProps.app.charity)
+    expect(donateControls.prop('user')).toEqual(mockProps.user)
+  })
+
+  it('displays the Hearts donated so far', () => {
+    const HeartDonationCampaign = require('js/components/Campaign/HeartDonationCampaignComponent').default
+    const mockProps = getMockProps()
+    mockProps.app.charity.vcReceived = 23874
+    const wrapper = shallow(
+      <HeartDonationCampaign {...mockProps}>
+        <span>Some content</span>
+      </HeartDonationCampaign>
+    )
+    expect(
+      wrapper
+        .find(Typography)
+        .filterWhere(n => {
+          return n.render().text() === '23.9K Hearts donated'
+        })
+        .length
+    ).toBe(1)
+  })
+
+  it('displays the goal number of Hearts', () => {
+    const HeartDonationCampaign = require('js/components/Campaign/HeartDonationCampaignComponent').default
+    const mockProps = getMockProps()
+    mockProps.heartsGoal = 10e7
+    const wrapper = shallow(
+      <HeartDonationCampaign {...mockProps}>
+        <span>Some content</span>
+      </HeartDonationCampaign>
+    )
+    expect(
+      wrapper
+        .find(Typography)
+        .filterWhere(n => {
+          return n.render().text() === 'Goal: 100M'
+        })
+        .length
+    ).toBe(1)
+  })
+
+  it('sets the correct value on the progress bar', () => {
+    const HeartDonationCampaign = require('js/components/Campaign/HeartDonationCampaignComponent').default
+    const mockProps = getMockProps()
+    mockProps.heartsGoal = 10e5
+    mockProps.app.charity.vcReceived = 250000
+    const wrapper = shallow(
+      <HeartDonationCampaign {...mockProps}>
+        <span>Some content</span>
+      </HeartDonationCampaign>
+    )
+    const progressBar = wrapper.find(LinearProgress)
+    expect(progressBar.prop('value')).toEqual(25.0)
   })
 })
