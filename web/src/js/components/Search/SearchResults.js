98bd7777bc72b0d77f47ab818c27852141a09632
Kevin Jennison <kevin.jennison1@gmail.com>
2018-11-26 17:49:35 -0500

    Working search results

--- web/src/js/components/Search/SearchResults.js
+++ web/src/js/components/Search/SearchResults.js
@@ -1,32 +1,103 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Helmet } from 'react-helmet'
+import { withStyles } from '@material-ui/core/styles'
 
-class SearchResults extends React.PureComponent {
+const styles = theme => ({
+  searchAdsContainer: {
+    '& iframe': {
+      width: '100%'
+    }
+  },
+  searchResultsContainer: {
+    '& iframe': {
+      width: '100%'
+    }
+  }
+})
+
+class SearchResults extends React.Component {
+  // TODO: fetch new search results when the query changes
+  getSearchResults () {
+    if (!window.ypaAds) {
+      // TODO: show an error
+      return
+    }
+
+    // TODO: style the search results
+    window.ypaAds.insertMultiAd({
+      ypaAdConfig: '00000129a',
+      ypaAdTypeTag: '',
+      ypaAdSlotInfo: [
+        {
+          ypaAdSlotId: 'GY_Top_Center',
+          ypaAdDivId: 'ypaAdWrapper-GY_Top_Center',
+          ypaAdWidth: '600',
+          ypaAdHeight: '891'
+        },
+        {
+          ypaAdSlotId: 'GY_Algo',
+          ypaAdDivId: 'ypaAdWrapper-GY_Algo',
+          ypaAdWidth: '600',
+          ypaAdHeight: '827'
+        }
+      ]
+    })
+  }
+
   render () {
-    const { query } = this.props
+    const { query, classes } = this.props
     if (!query) {
       return null
     }
     return (
-      <div
-        data-test-id='search-results-container'
-        style={{
-          background: 'none',
-          marginLeft: 170,
-          maxWidth: 600,
-          paddingTop: 20
-        }}
-      >
-        <span>TODO!</span>
+      <div>
+        <Helmet
+          onChangeClientState={(newState, addedTags) => {
+            // Fetch search results after the external JS has loaded.
+            // https://github.com/nfl/react-helmet/issues/146#issuecomment-271552211
+            // This solution isn't great for page speed when we're not
+            // server-rendering this page.
+            const self = this
+            const { scriptTags } = addedTags
+            if (scriptTags && scriptTags.length) {
+              scriptTags[0].addEventListener('load', () => {
+                self.getSearchResults()
+              })
+            }
+          }
+          }
+        >
+          <script src='https://s.yimg.com/uv/dm/scripts/syndication.js' />
+        </Helmet>
+        <div
+          data-test-id='search-results-container'
+          style={{
+            background: 'none',
+            marginLeft: 170,
+            maxWidth: 600,
+            paddingTop: 20
+          }}
+        >
+          <div
+            id='ypaAdWrapper-GY_Top_Center'
+            className={classes.searchAdsContainer}
+          />
+          <div
+            id='ypaAdWrapper-GY_Algo'
+            className={classes.searchResultsContainer}
+          />
+        </div>
       </div>
     )
   }
 }
 
 SearchResults.propTypes = {
-  query: PropTypes.string
+  query: PropTypes.string,
+  classes: PropTypes.object.isRequired
 }
 
 SearchResults.defaultProps = {}
 
-export default SearchResults
+export default withStyles(styles)(SearchResults)
