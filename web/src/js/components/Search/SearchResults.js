d0edacb400431dd979a239b58aa6edcf84c4eb1f
Kevin Jennison <kevin.jennison1@gmail.com>
2018-11-28 16:53:48 -0800

    Add some search ad options

--- web/src/js/components/Search/SearchResults.js
+++ web/src/js/components/Search/SearchResults.js
@@ -16,21 +16,92 @@ const styles = theme => ({
   }
 })
 
-function fetchSearchResults (query = null) {
+const fetchSearchResults = (query = null) => {
   // TODO: handle zero search results or other fetch errors
   // TODO: style the search results
   window.ypaAds.insertMultiAd({
     ypaPubParams: {
       query: query
     },
+    ypaAdTagOptions: {
+      adultFilter: false // false means do not allow adult ads
+    },
     ypaAdConfig: '00000129a',
     ypaAdTypeTag: '',
+    // TODO: GDPR
+    // "When gdpr=0 or missing, Oath will perform an IP check on the user
+    // to determine jurisdiction. If euconsent is empty, or if the IAB string
+    // is missing, Oath will return non-personalized search ads and search web
+    // results."
+    // gdpr: false
+    // euconsent: '',
     ypaAdSlotInfo: [
       {
         ypaAdSlotId: 'GY_Top_Center',
         ypaAdDivId: 'search-ads',
         ypaAdWidth: '600',
-        ypaAdHeight: '891'
+        ypaAdHeight: '891',
+        // TODO
+        // Callback function for when there are no ads.
+        // ypaOnNoAd: foo,
+        ypaSlotOptions: {
+          AdOptions: {
+            Mobile: {
+              // The ad start rank and the ad end rank in the list of
+              // search results.
+              AdRange: '1-2',
+              // Whether to show favicons near ads.
+              Favicon: false,
+              // Whether to show local ads.
+              LocalAds: true,
+              // Whether to use the "long ad title".
+              Lat: true,
+              // Whether to show "site links" in ads.
+              SiteLink: true,
+              // Whether to show merchant star ratings on ads.
+              MerchantRating: true,
+              // Whether to show images in ads.
+              ImageInAds: false,
+              // Whether to show "enhanced site links" in ads.
+              EnhancedSiteLink: true,
+              // Whether to show "smart annotations" in ads.
+              SmartAnnotations: false,
+              // Whether to show an "official site badge" next to ads
+              // with a verified site.
+              OfficialSiteBadge: true,
+              // Whether to show an option to call the businesses next
+              // to their ads.
+              CallExtension: true
+            },
+            Desktop: {
+              // The ad start rank and the ad end rank in the list of
+              // search results.
+              AdRange: '1-2',
+              // Whether to show favicons near ads.
+              Favicon: false,
+              // Whether to show local ads.
+              LocalAds: true,
+              // Whether to use the "long ad title".
+              Lat: true,
+              // Whether to show "site links" in ads.
+              SiteLink: true,
+              // Whether to show merchant star ratings on ads.
+              MerchantRating: true,
+              // Whether to show images in ads.
+              ImageInAds: false,
+              // Whether to show "enhanced site links" in ads.
+              EnhancedSiteLink: true,
+              // Whether to show "smart annotations" in ads.
+              SmartAnnotations: false,
+              // Whether to show an "official site badge" next to ads
+              // with a verified site.
+              OfficialSiteBadge: true,
+              // Whether to show an option to call the businesses next
+              // to their ads.
+              CallExtension: true
+            }
+          }
+        }
       },
       {
         ypaAdSlotId: 'GY_Algo',
