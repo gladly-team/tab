43c2ef8aade534ecb0f506d5b00cef6c3f1d2fc5
Kevin Jennison <kevin.jennison1@gmail.com>
2018-12-04 13:57:09 -0800

    Add simple tests for footer

---
+++ web/src/js/components/General/__tests__/Footer.test.js
@@ -0,0 +1,18 @@
+/* eslint-env jest */
+
+import React from 'react'
+import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
+
+describe('Footer', () => {
+  it('renders without error', () => {
+    const Footer = require('../Footer').default
+    shallow(<Footer />)
+  })
+
+  it('matches expected snapshot', function () {
+    const Footer = require('../Footer').default
+    const wrapper = shallow(<Footer />)
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+})
