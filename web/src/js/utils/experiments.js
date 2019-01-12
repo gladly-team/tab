edbf235e8a8d75bba0f4aaba22e9a8298481f4f4
Kevin Jennison <kevin.jennison1@gmail.com>
2019-01-11 20:09:06 -0800

    FIXME

--- web/src/js/utils/experiments.js
+++ web/src/js/utils/experiments.js
@@ -292,7 +292,8 @@ export const experiments = [
 // We do this to be able to modify the experiments
 // variable during testing while keeping it in this file.
 // https://github.com/facebook/jest/issues/936#issuecomment-214939935
-const getExperiments = () => module.exports.experiments
+// FIXME: stop using this hack (test is broken)
+const getExperiments = () => experiments
 
 /**
  * Get the set of available group values for an experiment.
