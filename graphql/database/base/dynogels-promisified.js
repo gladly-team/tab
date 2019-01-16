37aa365987c7a7627bd4ea7fc4cceabaff5f880c
Kevin Jennison <kevin.jennison1@gmail.com>
2019-01-16 14:32:12 -0800

    Lint fixes

--- graphql/database/base/dynogels-promisified.js
+++ graphql/database/base/dynogels-promisified.js
@@ -12,11 +12,11 @@ Promise.promisifyAll(require('dynogels/lib/scan').prototype)
 Promise.promisifyAll(require('dynogels/lib/parallelScan').prototype)
 
 const dynogelsModel = dynogels.model
-dynogels.model = function(name, model) {
+dynogels.model = function modelFunc(name, model, ...args) {
   if (model) {
     Promise.promisifyAll(model)
   }
-  return dynogelsModel.apply(dynogels, arguments)
+  return dynogelsModel.apply(dynogels, [name, model, ...args])
 }
 
 Promise.promisifyAll(dynogels)
