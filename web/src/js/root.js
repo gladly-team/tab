08f9fe5860d359f8d2c417dc24bc20ca773e61fb
Kevin Jennison <kevin.jennison1@gmail.com>
2019-01-29 16:09:25 -0800

    Add the TTI polyfill to measure performance

--- web/src/js/root.js
+++ web/src/js/root.js
@@ -8,21 +8,34 @@ import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import defaultThemeLegacy from 'js/theme/default'
 import defaultTheme from 'js/theme/defaultV1'
 import BaseContainer from 'js/components/General/BaseContainer'
+import ttiPolyfill from 'tti-polyfill'
 
 const legacyMuiTheme = getMuiTheme(defaultThemeLegacy)
 const muiTheme = createMuiTheme(defaultTheme)
 
-// @material-ui-1-todo: remove legacy theme provider
-const Root = () => (
-  <MuiThemeProvider theme={muiTheme}>
-    <V0MuiThemeProvider muiTheme={legacyMuiTheme}>
-      <BaseContainer>
-        <Router history={browserHistory}>
-          <Routes />
-        </Router>
-      </BaseContainer>
-    </V0MuiThemeProvider>
-  </MuiThemeProvider>
-)
+class Root extends React.Component {
+  componentDidMount() {
+    if (process.env.NODE_ENV !== 'production') {
+      ttiPolyfill.getFirstConsistentlyInteractive().then(tti => {
+        console.log(`Time to interactive: ${tti}`)
+      })
+    }
+  }
+
+  render() {
+    // @material-ui-1-todo: remove legacy theme provider
+    return (
+      <MuiThemeProvider theme={muiTheme}>
+        <V0MuiThemeProvider muiTheme={legacyMuiTheme}>
+          <BaseContainer>
+            <Router history={browserHistory}>
+              <Routes />
+            </Router>
+          </BaseContainer>
+        </V0MuiThemeProvider>
+      </MuiThemeProvider>
+    )
+  }
+}
 
 export default Root
