65aed47e4d05752fc5bfbb96af5d0772c57d674f
Kevin Jennison <kevin.jennison1@gmail.com>
2019-01-29 16:40:21 -0800

    Move base routes to root.js

--- web/src/js/root.js
+++ web/src/js/root.js
@@ -1,8 +1,7 @@
 import React from 'react'
-import { Router } from 'react-router-dom'
+import { Redirect, Route, Router, Switch } from 'react-router-dom'
 import ttiPolyfill from 'tti-polyfill'
 import { browserHistory } from 'js/navigation/navigation'
-import Routes from 'js/routes/Route'
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 import { MuiThemeProvider as V0MuiThemeProvider } from 'material-ui'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
@@ -11,6 +10,8 @@ import defaultTheme from 'js/theme/defaultV1'
 import BaseContainer from 'js/components/General/BaseContainer'
 import { initializeFirebase } from 'js/authentication/firebaseConfig'
 import logger from 'js/utils/logger'
+import App from 'js/components/App/App'
+import SearchView from 'js/components/Search/SearchView'
 
 const legacyMuiTheme = getMuiTheme(defaultThemeLegacy)
 const muiTheme = createMuiTheme(defaultTheme)
@@ -44,12 +45,17 @@ class Root extends React.Component {
 
   render() {
     // @material-ui-1-todo: remove legacy theme provider
+    // TODO: Show 404 page
     return (
       <MuiThemeProvider theme={muiTheme}>
         <V0MuiThemeProvider muiTheme={legacyMuiTheme}>
           <BaseContainer>
             <Router history={browserHistory}>
-              <Routes />
+              <Switch>
+                <Route path="/newtab/" component={App} />
+                <Route path="/search/" component={SearchView} />
+                <Redirect from="*" to="/newtab" />
+              </Switch>
             </Router>
           </BaseContainer>
         </V0MuiThemeProvider>
