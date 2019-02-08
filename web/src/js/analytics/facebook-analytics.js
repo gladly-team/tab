78ff58c1d56ae3b3163b0dd8a69aa62ec0f6c764
Kevin Jennison <kevin.jennison1@gmail.com>
2019-02-07 16:54:41 -0800

    Move fbq initialization into function to avoid unnecessary error logs

--- web/src/js/analytics/facebook-analytics.js
+++ web/src/js/analytics/facebook-analytics.js
@@ -1,13 +1,12 @@
 import logger from 'js/utils/logger'
 
-const DEBUG = false
-
-var fbq = window.fbq
-if (!fbq) {
-  logger.error('Facebook analytics are not available on `window.fbq`.')
-}
-
 const facebookAnalytics = (...args) => {
+  const DEBUG = false
+
+  const fbq = window.fbq
+  if (!fbq) {
+    logger.error('Facebook analytics are not available on `window.fbq`.')
+  }
   try {
     if (DEBUG) {
       console.log('Logging Facebook event with args:', args)
