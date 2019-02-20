6d62242025761f71f8b2f24120c5f1b42426b206
Kevin Jennison <kevin.jennison1@gmail.com>
2019-02-20 11:50:38 -0800

    Add tests

--- web/src/js/components/Dashboard/__tests__/HeartsComponent.test.js
+++ web/src/js/components/Dashboard/__tests__/HeartsComponent.test.js
@@ -1,7 +1,12 @@
 /* eslint-env jest */
 
 import React from 'react'
-import { shallow } from 'enzyme'
+import { mount, shallow } from 'enzyme'
+import Typography from '@material-ui/core/Typography'
+import HeartBorderIcon from '@material-ui/icons/FavoriteBorder'
+
+jest.mock('js/components/Dashboard/HeartsDropdownContainer')
+jest.mock('js/components/Dashboard/MaxHeartsDropdownMessageComponent')
 
 const getMockProps = () => ({
   user: {
@@ -31,5 +36,53 @@ describe('HeartsComponent', () => {
     // Important: other code relies on the data-tour-id to show the
     // new user tour. Do not change it without updating it elsewhere.
     expect(wrapper.find('[data-tour-id="hearts"]').length).toBe(1)
+  })
+
+  it('shows the comma-formatted heart count', () => {
+    const HeartsComponent = require('js/components/Dashboard/HeartsComponent')
+      .default
+    const mockProps = getMockProps()
+    mockProps.user.vcCurrent = 15422
+    const wrapper = shallow(<HeartsComponent {...mockProps} />).dive()
+    expect(
+      wrapper
+        .find(Typography)
+        .first()
+        .render()
+        .text()
+    ).toEqual('15,422')
+  })
+
+  it('is the expected color when not hovering', () => {
+    const HeartsComponent = require('js/components/Dashboard/HeartsComponent')
+      .default
+    const mockProps = getMockProps()
+    const wrapper = mount(<HeartsComponent {...mockProps} />)
+    const typographyComputedStyle = window.getComputedStyle(
+      wrapper.find(Typography).getDOMNode()
+    )
+    expect(typographyComputedStyle).toHaveProperty(
+      'color',
+      'rgba(0, 0, 0, 0.87)'
+    )
+  })
+
+  it('is the expected color when hovering', () => {
+    const HeartsComponent = require('js/components/Dashboard/HeartsComponent')
+      .default
+    const mockProps = getMockProps()
+    const wrapper = mount(<HeartsComponent {...mockProps} />)
+
+    // Simulate hover on the hearts icon
+    wrapper
+      .find(HeartBorderIcon)
+      .first()
+      // .simulate('mouseenter')
+      .simulate('click')
+
+    const typographyComputedStyle = window.getComputedStyle(
+      wrapper.find(Typography).getDOMNode()
+    )
+    expect(typographyComputedStyle).toHaveProperty('color', 'white')
   })
 })
