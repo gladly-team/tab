46711efd97793182eb38d36f1485d4b2b83c8355
Kevin Jennison <kevin.jennison1@gmail.com>
2019-04-03 15:13:04 -0700

    Add documentation

--- web/src/js/components/Search/fetchWikipediaResults.js
+++ web/src/js/components/Search/fetchWikipediaResults.js
@@ -1,25 +1,60 @@
 import qs from 'qs'
 
-// TODO
+// TODO: add tests
+/**
+ * Build the request URL to fetch page information from
+ * the Wikipedia API.
+ * @param {String} query - The search query, unencoded.
+ * @return {String} The URL used in a GET request to fetch
+ *   data from Wikipedia.
+ */
 const constructWikiURL = query => {
   const params = {
+    // Get info about a page.
+    // https://www.mediawiki.org/wiki/API:Query
+    // We also use a generator to search for the most relevant
+    // page before fetching that page's info:
+    // https://www.mediawiki.org/wiki/API:Query#Generators
     action: 'query',
-    generator: 'prefixsearch',
-    gpssearch: query,
-    gpsnamespace: 0,
-    gpslimit: 1,
-    prop: 'pageimages|extracts|description|images|info|imageinfo',
-    pilimit: 2,
+    // Extracts (i.e. excerpts) settings:
+    // https://www.mediawiki.org/w/api.php?action=help&modules=query%2Bextracts
+    // Only get content in the first section of the page.
     exintro: true,
-    explaintext: true,
-    exsentences: 1,
-    exlimit: 3,
-    formatversion: 2,
-    pithumbsize: 300,
-    imlimit: 4,
-    inprop: 'url',
+    exlimit: 3, // The max number of extracts to fetch.
+    explaintext: true, // Get the extract as plain text.
+    exsentences: 1, // Only get the eextract's first sentence.
+    // For JSON, we should use formatversion=2:
+    // https://www.mediawiki.org/wiki/API:Page_info_in_search_results
     format: 'json',
+    formatversion: 2,
+    // Given user input, find the best-matching pages:
+    // https://www.mediawiki.org/wiki/API:Prefixsearch
+    generator: 'prefixsearch',
+    gpslimit: 1, // Limit results to one page.
+    // Namespace 0 is for "real" content articles:
+    // https://www.mediawiki.org/wiki/Manual:Namespace
+    gpsnamespace: 0,
+    gpssearch: query,
+    // Images settings:
+    // https://www.mediawiki.org/w/api.php?action=help&modules=query%2Bimages
+    // Note that if we want URLs of images, we probably have to
+    // make another API call:
+    // https://stackoverflow.com/a/8363589
+    // TODO: remove this if we don't use the images.
+    imlimit: 4, // The max number of images to fetch.
+    // Info settings:
+    // https://www.mediawiki.org/w/api.php?action=help&modules=query%2Binfo
+    inprop: 'url', // Get the URL of the Wikipedia page.
+    // Make an unauthenticated CORS request:
+    // https://www.mediawiki.org/wiki/API:Cross-site_requests
     origin: '*',
+    // Page images settings:
+    // https://www.mediawiki.org/w/api.php?action=help&modules=query%2Bpageimages
+    pilimit: 1, // The max number of page images to fetch.
+    pithumbsize: 300, // The maximum image width in pixels.
+    // All the properties to fetch for the page:
+    // https://www.mediawiki.org/w/api.php?action=help&modules=query
+    prop: 'pageimages|extracts|description|images|info|imageinfo',
     // Allow automatic page redirects:
     // https://www.mediawiki.org/wiki/API:Query#Resolving_redirects
     redirects: true,
@@ -29,6 +64,7 @@ const constructWikiURL = query => {
   return `${urlBase}?${searchStr}`
 }
 
+// TODO: add tests
 /**
  * Call Wikipedia to fetch the most relevant page(s).
  * @param {String} query - The search query, unencoded.
