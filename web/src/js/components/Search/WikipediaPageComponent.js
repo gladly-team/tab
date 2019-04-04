1896607592072eaf5a26c03c063d8ca2cd982a55
Kevin Jennison <kevin.jennison1@gmail.com>
2019-04-04 14:54:17 -0700

    Add tests

--- web/src/js/components/Search/WikipediaPageComponent.js
+++ web/src/js/components/Search/WikipediaPageComponent.js
@@ -25,7 +25,6 @@ const styles = theme => ({
   },
 })
 
-// TODO: add tests
 const WikipediaPageComponent = props => {
   const {
     classes,
@@ -38,7 +37,7 @@ const WikipediaPageComponent = props => {
     title,
   } = props
   return (
-    <Paper data-test-id={'search-wiki-page'} elevation={1} style={style}>
+    <Paper data-test-id={'search-wiki'} elevation={1} style={style}>
       <div
         style={{
           display: 'flex',
@@ -47,13 +46,18 @@ const WikipediaPageComponent = props => {
         }}
       >
         <div style={{ flex: 5, padding: 20 }}>
-          <Typography variant={'h5'}>{title}</Typography>
-          <Typography
-            variant={'caption'}
-            style={{ lineHeight: '110%', marginTop: 4 }}
-          >
-            {description}
+          <Typography variant={'h5'} data-test-id={'search-wiki-title'}>
+            {title}
           </Typography>
+          {description ? (
+            <Typography
+              variant={'caption'}
+              style={{ lineHeight: '110%', marginTop: 4 }}
+              data-test-id={'search-wiki-desc'}
+            >
+              {description}
+            </Typography>
+          ) : null}
         </div>
         {thumbnailURL ? (
           <div
@@ -67,16 +71,21 @@ const WikipediaPageComponent = props => {
               overflow: 'hidden',
             }}
           >
-            <img src={thumbnailURL} alt={'Thumbnail from Wikipedia'} />
+            <img
+              src={thumbnailURL}
+              alt={'Thumbnail from Wikipedia'}
+              data-test-id={'search-wiki-thumbnail'}
+            />
           </div>
         ) : null}
       </div>
       <div style={{ padding: 20, borderBottom: borderStyle }}>
         <span
+          data-test-id={'search-wiki-extract'}
           dangerouslySetInnerHTML={{ __html: extract }}
           className={classes.wikiExtractHTML}
         />{' '}
-        <Typography variant={'body2'}>
+        <Typography variant={'body2'} data-test-id={'search-wiki-read-more'}>
           <a
             href={pageURL}
             style={{
@@ -89,13 +98,21 @@ const WikipediaPageComponent = props => {
         </Typography>
       </div>
       <div style={{ padding: '12px 20px' }}>
-        <Typography variant={'caption'} className={classes.wikiAttribution}>
+        <Typography
+          variant={'caption'}
+          className={classes.wikiAttribution}
+          data-test-id={'search-wiki-attrib'}
+        >
           From{' '}
           <a href={pageURL} className={classes.wikiAttributionLink}>
             Wikipedia
           </a>
         </Typography>
-        <Typography variant={'caption'} className={classes.wikiAttribution}>
+        <Typography
+          variant={'caption'}
+          className={classes.wikiAttribution}
+          data-test-id={'search-wiki-license'}
+        >
           Content under{' '}
           <a
             href={'https://creativecommons.org/licenses/by-sa/3.0/'}
@@ -112,9 +129,8 @@ const WikipediaPageComponent = props => {
 }
 
 WikipediaPageComponent.propTypes = {
-  // TODO: make description optional
   classes: PropTypes.object.isRequired,
-  description: PropTypes.string.isRequired,
+  description: PropTypes.string,
   extract: PropTypes.string.isRequired,
   pageURL: PropTypes.string.isRequired,
   style: PropTypes.object,
--- web/src/js/components/Search/__tests__/WikipediaPageComponent.test.js
+++ web/src/js/components/Search/__tests__/WikipediaPageComponent.test.js
@@ -19,4 +19,147 @@ describe('WikipediaPageComponent', () => {
     const mockProps = getMockProps()
     shallow(<WikipediaPageComponent {...mockProps} />).dive()
   })
+
+  it('displays the title', () => {
+    const WikipediaPageComponent = require('js/components/Search/WikipediaPageComponent')
+      .default
+    const mockProps = getMockProps()
+    mockProps.title = 'George Washington'
+    const wrapper = shallow(<WikipediaPageComponent {...mockProps} />).dive()
+    expect(
+      wrapper
+        .find('[data-test-id="search-wiki-title"]')
+        .render()
+        .text()
+    ).toEqual('George Washington')
+  })
+
+  it('renders the thumbnail image', () => {
+    const WikipediaPageComponent = require('js/components/Search/WikipediaPageComponent')
+      .default
+    const mockProps = getMockProps()
+    mockProps.thumbnailURL = 'https://example.com/img/foo.png'
+    const wrapper = shallow(<WikipediaPageComponent {...mockProps} />).dive()
+    expect(
+      wrapper.find('[data-test-id="search-wiki-thumbnail"]').prop('src')
+    ).toEqual('https://example.com/img/foo.png')
+  })
+
+  it('sets the expected style on the thumbnail image parent', () => {
+    const WikipediaPageComponent = require('js/components/Search/WikipediaPageComponent')
+      .default
+    const mockProps = getMockProps()
+    mockProps.thumbnailURL = 'https://example.com/img/foo.png'
+    const wrapper = shallow(<WikipediaPageComponent {...mockProps} />).dive()
+    const imgParent = wrapper
+      .find('[data-test-id="search-wiki-thumbnail"]')
+      .parent()
+    expect(imgParent.prop('style')).toMatchObject({
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      maxWidth: 200,
+      height: 150,
+      overflow: 'hidden',
+    })
+  })
+
+  it('does not render the thumbnail image if there is no provided image', () => {
+    const WikipediaPageComponent = require('js/components/Search/WikipediaPageComponent')
+      .default
+    const mockProps = getMockProps()
+    mockProps.thumbnailURL = null
+    const wrapper = shallow(<WikipediaPageComponent {...mockProps} />).dive()
+    expect(
+      wrapper.find('[data-test-id="search-wiki-thumbnail"]').exists()
+    ).toBe(false)
+  })
+
+  it('renders the description', () => {
+    const WikipediaPageComponent = require('js/components/Search/WikipediaPageComponent')
+      .default
+    const mockProps = getMockProps()
+    const wrapper = shallow(<WikipediaPageComponent {...mockProps} />).dive()
+    expect(
+      wrapper
+        .find('[data-test-id="search-wiki-desc"]')
+        .render()
+        .text()
+    ).toEqual('Usually savory dish of flattened bread and toppings')
+  })
+
+  it('does not render the description if it is not provided', () => {
+    const WikipediaPageComponent = require('js/components/Search/WikipediaPageComponent')
+      .default
+    const mockProps = getMockProps()
+    delete mockProps.description
+    const wrapper = shallow(<WikipediaPageComponent {...mockProps} />).dive()
+    expect(wrapper.find('[data-test-id="search-wiki-desc"]').exists()).toBe(
+      false
+    )
+  })
+
+  it('renders the extract HTML', () => {
+    const WikipediaPageComponent = require('js/components/Search/WikipediaPageComponent')
+      .default
+    const mockProps = getMockProps()
+    const wrapper = shallow(<WikipediaPageComponent {...mockProps} />).dive()
+    const extractParent = wrapper
+      .find('[data-test-id="search-wiki-extract"]')
+      .render()
+    expect(
+      extractParent
+        .find('b')
+        .first()
+        .text()
+    ).toEqual('Pizza')
+    expect(extractParent.text()).toEqual(
+      expect.stringContaining(
+        'baked at a high temperature, traditionally in a wood-fired oven'
+      )
+    )
+  })
+
+  it('links to the Wikipedia page in the "read more" link', () => {
+    const WikipediaPageComponent = require('js/components/Search/WikipediaPageComponent')
+      .default
+    const mockProps = getMockProps()
+    const wrapper = shallow(<WikipediaPageComponent {...mockProps} />).dive()
+    const readMoreElem = wrapper.find('[data-test-id="search-wiki-read-more"]')
+    expect(readMoreElem.render().text()).toEqual('Read more')
+    expect(
+      readMoreElem
+        .find('a')
+        .first()
+        .prop('href')
+    ).toEqual('https://en.wikipedia.org/wiki/Pizza')
+  })
+
+  it('links to the Wikipedia page in the "Wikipedia" attribution', () => {
+    const WikipediaPageComponent = require('js/components/Search/WikipediaPageComponent')
+      .default
+    const mockProps = getMockProps()
+    const wrapper = shallow(<WikipediaPageComponent {...mockProps} />).dive()
+    const attribText = wrapper.find('[data-test-id="search-wiki-attrib"]')
+    expect(attribText.render().text()).toEqual('From Wikipedia')
+    const attribLink = attribText.find('a').first()
+    expect(attribLink.prop('href')).toEqual(
+      'https://en.wikipedia.org/wiki/Pizza'
+    )
+  })
+
+  it('links to the license', () => {
+    const WikipediaPageComponent = require('js/components/Search/WikipediaPageComponent')
+      .default
+    const mockProps = getMockProps()
+    const wrapper = shallow(<WikipediaPageComponent {...mockProps} />).dive()
+    const licenseText = wrapper.find('[data-test-id="search-wiki-license"]')
+    expect(licenseText.render().text()).toEqual('Content under CC BY-SA')
+    expect(
+      licenseText
+        .find('a')
+        .first()
+        .prop('href')
+    ).toEqual('https://creativecommons.org/licenses/by-sa/3.0/')
+  })
 })
