d10f14c0b52b1a1c378de42c6fa3382382e7c8ba
Kevin Jennison <kevin.jennison1@gmail.com>
2019-04-04 11:39:27 -0700

    Make sure fetch tests are async

--- web/src/js/components/Search/__tests__/fetchWikipediaResults.test.js
+++ web/src/js/components/Search/__tests__/fetchWikipediaResults.test.js
@@ -2,35 +2,39 @@
 
 import { mockFetchResponse } from 'js/utils/test-utils'
 
-afterEach(() => {
-  jest.clearAllMocks()
-})
-
 beforeEach(() => {
   global.fetch.mockImplementation(() => Promise.resolve(mockFetchResponse()))
 })
 
+afterEach(() => {
+  jest.clearAllMocks()
+  jest.resetModules()
+})
+
 describe('fetchWikipediaResults', () => {
-  it('calls fetch once', () => {
+  expect.assertions(1)
+  it('calls fetch once', async () => {
     const fetchWikipediaResults = require('js/components/Search/fetchWikipediaResults')
       .default
-    fetchWikipediaResults()
+    await fetchWikipediaResults()
     expect(fetch).toHaveBeenCalledTimes(1)
   })
 
-  it('fetches with a GET request', () => {
+  it('fetches with a GET request', async () => {
+    expect.assertions(1)
     const fetchWikipediaResults = require('js/components/Search/fetchWikipediaResults')
       .default
-    fetchWikipediaResults()
+    await fetchWikipediaResults()
     expect(fetch.mock.calls[0][1]).toMatchObject({
       method: 'GET',
     })
   })
 
-  it('fetches with the expected headers', () => {
+  it('fetches with the expected headers', async () => {
+    expect.assertions(1)
     const fetchWikipediaResults = require('js/components/Search/fetchWikipediaResults')
       .default
-    fetchWikipediaResults()
+    await fetchWikipediaResults()
     expect(fetch.mock.calls[0][1]).toMatchObject({
       headers: {
         Accept: 'application/json',
