fc68e188b231081d5d9f60ba82af814e3a7935f7
Kevin Jennison <kevin.jennison1@gmail.com>
2019-04-10 14:39:52 -0700

    Make search results a functional component

--- web/src/js/components/Search/SearchResultsBing.js
+++ web/src/js/components/Search/SearchResultsBing.js
@@ -30,167 +30,165 @@ const styles = theme => ({
   },
 })
 
-class SearchResultsBing extends React.Component {
-  render() {
-    const {
-      classes,
-      data,
-      isEmptyQuery,
-      isError,
-      isQueryInProgress,
-      noSearchResults,
-      onPageChange,
-      page,
-      query,
-      style,
-      theme,
-    } = this.props
+const SearchResultsBing = props => {
+  const {
+    classes,
+    data,
+    isEmptyQuery,
+    isError,
+    isQueryInProgress,
+    noSearchResults,
+    onPageChange,
+    page,
+    query,
+    style,
+    theme,
+  } = props
 
-    // eslint-disable-next-line no-unused-vars
-    const poleResults = get(data, 'rankingResponse.pole.items', [])
-    const mainResults = get(data, 'rankingResponse.mainline.items', [])
+  // eslint-disable-next-line no-unused-vars
+  const poleResults = get(data, 'rankingResponse.pole.items', [])
+  const mainResults = get(data, 'rankingResponse.mainline.items', [])
 
-    // eslint-disable-next-line no-unused-vars
-    const sidebarResults = get(data, 'rankingResponse.sidebar.items', [])
+  // eslint-disable-next-line no-unused-vars
+  const sidebarResults = get(data, 'rankingResponse.sidebar.items', [])
 
-    // Hiding until we make it functional.
-    const SHOW_PAGINATION = false
+  // Hiding until we make it functional.
+  const SHOW_PAGINATION = false
 
-    // Include 8 pages total, 4 lower and 4 higher when possible.
-    // Page 9999 is the maximum, so stop there.
-    const MIN_PAGE = 1
-    const MAX_PAGE = 9999
-    const paginationIndices = range(
-      Math.max(MIN_PAGE, Math.min(page - 4, MAX_PAGE - 8)),
-      Math.min(MAX_PAGE + 1, Math.max(page + 4, MIN_PAGE + 8))
-    )
+  // Include 8 pages total, 4 lower and 4 higher when possible.
+  // Page 9999 is the maximum, so stop there.
+  const MIN_PAGE = 1
+  const MAX_PAGE = 9999
+  const paginationIndices = range(
+    Math.max(MIN_PAGE, Math.min(page - 4, MAX_PAGE - 8)),
+    Math.min(MAX_PAGE + 1, Math.max(page + 4, MIN_PAGE + 8))
+  )
 
-    const noResultsToDisplay = isEmptyQuery || noSearchResults || isError
-    return (
-      <div
-        className={classes.searchResultsParentContainer}
-        style={Object.assign(
-          {},
-          {
-            // Min height prevents visibly shifting content below,
-            // like the footer.
-            minHeight: noResultsToDisplay ? 0 : 1000,
-          },
-          style
-        )}
-      >
-        {noSearchResults ? (
+  const noResultsToDisplay = isEmptyQuery || noSearchResults || isError
+  return (
+    <div
+      className={classes.searchResultsParentContainer}
+      style={Object.assign(
+        {},
+        {
+          // Min height prevents visibly shifting content below,
+          // like the footer.
+          minHeight: noResultsToDisplay ? 0 : 1000,
+        },
+        style
+      )}
+    >
+      {noSearchResults ? (
+        <Typography variant={'body1'} gutterBottom>
+          No results found for{' '}
+          <span style={{ fontWeight: 'bold' }}>{query}</span>
+        </Typography>
+      ) : null}
+      {isError ? (
+        <div data-test-id={'search-err-msg'}>
           <Typography variant={'body1'} gutterBottom>
-            No results found for{' '}
-            <span style={{ fontWeight: 'bold' }}>{query}</span>
+            Unable to search at this time.
           </Typography>
-        ) : null}
-        {isError ? (
-          <div data-test-id={'search-err-msg'}>
-            <Typography variant={'body1'} gutterBottom>
-              Unable to search at this time.
-            </Typography>
-            <Link
-              to={`https://www.google.com/search?q=${encodeURI(query)}`}
-              target="_top"
-            >
-              <Button color={'primary'} variant={'contained'} size={'small'}>
-                Search Google
-              </Button>
-            </Link>
-          </div>
-        ) : isEmptyQuery ? (
-          <Typography variant={'body1'} gutterBottom>
-            Search something to start raising money for charity!
-          </Typography>
-        ) : null}
-        {isQueryInProgress ? null : (
-          <div id="search-results" className={classes.searchResultsContainer}>
-            {mainResults.map(itemRankingData => {
-              // Get the data for this item.
-              // https://github.com/Azure-Samples/cognitive-services-REST-api-samples/blob/master/Tutorials/Bing-Web-Search/public/js/script.js#L168
-              const typeName =
-                itemRankingData.answerType[0].toLowerCase() +
-                itemRankingData.answerType.slice(1)
-              // https://github.com/Azure-Samples/cognitive-services-REST-api-samples/blob/master/Tutorials/Bing-Web-Search/public/js/script.js#L172
-              const itemData = !isNil(itemRankingData.resultIndex)
-                ? // One result of the specified type (e.g., one webpage link)
-                  get(data, `${typeName}.value[${itemRankingData.resultIndex}]`)
-                : // All results of the specified type (e.g., all videos)
-                  get(data, `${typeName}.value`)
-
-              // Return null if we couldn't find the result item data.
-              if (!(itemRankingData.answerType && itemData)) {
-                // console.error(`Couldn't find item data for:`, itemRankingData)
-                return null
-              }
-              const key = itemData.id
-                ? `${itemRankingData.answerType}-${itemData.id}`
-                : itemRankingData.answerType
-              return (
-                <SearchResultItem
-                  key={key}
-                  type={itemRankingData.answerType}
-                  itemData={itemData}
-                />
-              )
-            })}
-          </div>
-        )}
-        <div
-          data-test-id={'pagination-container'}
-          className={classes.paginationContainer}
-          style={{
-            display: !SHOW_PAGINATION || noResultsToDisplay ? 'none' : 'block',
-          }}
-        >
-          {page > MIN_PAGE ? (
-            <Button
-              data-test-id={'pagination-previous'}
-              className={classes.paginationButton}
-              onClick={() => {
-                onPageChange(page - 1)
-              }}
-            >
-              PREVIOUS
+          <Link
+            to={`https://www.google.com/search?q=${encodeURI(query)}`}
+            target="_top"
+          >
+            <Button color={'primary'} variant={'contained'} size={'small'}>
+              Search Google
             </Button>
-          ) : null}
-          {paginationIndices.map(pageNum => (
-            <Button
-              key={`page-${pageNum}`}
-              className={classes.paginationButton}
-              data-test-id={`pagination-${pageNum}`}
-              {...pageNum === page && {
-                color: 'secondary',
-                disabled: true,
-              }}
-              style={{
-                ...(pageNum === page && {
-                  color: theme.palette.secondary.main,
-                }),
-              }}
-              onClick={() => {
-                onPageChange(pageNum)
-              }}
-            >
-              {pageNum}
-            </Button>
-          ))}
-          {page < MAX_PAGE ? (
-            <Button
-              data-test-id={'pagination-next'}
-              className={classes.paginationButton}
-              onClick={() => {
-                onPageChange(page + 1)
-              }}
-            >
-              NEXT
-            </Button>
-          ) : null}
+          </Link>
         </div>
+      ) : isEmptyQuery ? (
+        <Typography variant={'body1'} gutterBottom>
+          Search something to start raising money for charity!
+        </Typography>
+      ) : null}
+      {isQueryInProgress ? null : (
+        <div id="search-results" className={classes.searchResultsContainer}>
+          {mainResults.map(itemRankingData => {
+            // Get the data for this item.
+            // https://github.com/Azure-Samples/cognitive-services-REST-api-samples/blob/master/Tutorials/Bing-Web-Search/public/js/script.js#L168
+            const typeName =
+              itemRankingData.answerType[0].toLowerCase() +
+              itemRankingData.answerType.slice(1)
+            // https://github.com/Azure-Samples/cognitive-services-REST-api-samples/blob/master/Tutorials/Bing-Web-Search/public/js/script.js#L172
+            const itemData = !isNil(itemRankingData.resultIndex)
+              ? // One result of the specified type (e.g., one webpage link)
+                get(data, `${typeName}.value[${itemRankingData.resultIndex}]`)
+              : // All results of the specified type (e.g., all videos)
+                get(data, `${typeName}.value`)
+
+            // Return null if we couldn't find the result item data.
+            if (!(itemRankingData.answerType && itemData)) {
+              // console.error(`Couldn't find item data for:`, itemRankingData)
+              return null
+            }
+            const key = itemData.id
+              ? `${itemRankingData.answerType}-${itemData.id}`
+              : itemRankingData.answerType
+            return (
+              <SearchResultItem
+                key={key}
+                type={itemRankingData.answerType}
+                itemData={itemData}
+              />
+            )
+          })}
+        </div>
+      )}
+      <div
+        data-test-id={'pagination-container'}
+        className={classes.paginationContainer}
+        style={{
+          display: !SHOW_PAGINATION || noResultsToDisplay ? 'none' : 'block',
+        }}
+      >
+        {page > MIN_PAGE ? (
+          <Button
+            data-test-id={'pagination-previous'}
+            className={classes.paginationButton}
+            onClick={() => {
+              onPageChange(page - 1)
+            }}
+          >
+            PREVIOUS
+          </Button>
+        ) : null}
+        {paginationIndices.map(pageNum => (
+          <Button
+            key={`page-${pageNum}`}
+            className={classes.paginationButton}
+            data-test-id={`pagination-${pageNum}`}
+            {...pageNum === page && {
+              color: 'secondary',
+              disabled: true,
+            }}
+            style={{
+              ...(pageNum === page && {
+                color: theme.palette.secondary.main,
+              }),
+            }}
+            onClick={() => {
+              onPageChange(pageNum)
+            }}
+          >
+            {pageNum}
+          </Button>
+        ))}
+        {page < MAX_PAGE ? (
+          <Button
+            data-test-id={'pagination-next'}
+            className={classes.paginationButton}
+            onClick={() => {
+              onPageChange(page + 1)
+            }}
+          >
+            NEXT
+          </Button>
+        ) : null}
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 SearchResultsBing.propTypes = {
