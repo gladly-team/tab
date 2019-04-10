46070522882ffe8ad437a8bd1ea4251352b6f867
Kevin Jennison <kevin.jennison1@gmail.com>
2019-04-10 15:56:47 -0700

    Don't mock search results on dev

--- scripts/assign-env-vars.js
+++ scripts/assign-env-vars.js
@@ -27,6 +27,7 @@ export const envVars = [
   { name: 'REACT_APP_SENTRY_DEBUG', optional: true },
   { name: 'REACT_APP_SENTRY_ENABLE_AUTO_BREADCRUMBS', optional: true },
   { name: 'REACT_APP_FEATURE_FLAG_SEARCH_PAGE_ENABLED', optional: true },
+  { name: 'REACT_APP_MOCK_SEARCH_RESULTS', optional: true },
   {
     name: 'REACT_APP_FEATURE_FLAG_REDIRECT_SEARCH_TO_THIRD_PARTY',
     optional: true,
