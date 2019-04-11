24c450fd51986056a1486e122c136ab04cae808c
Kevin Jennison <kevin.jennison1@gmail.com>
2019-04-11 11:27:22 -0700

    Add deeplinks

--- web/src/js/components/Search/WebPageSearchResult.js
+++ web/src/js/components/Search/WebPageSearchResult.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
+import CircleIcon from '@material-ui/icons/Lens'
 
 const styles = () => ({
   container: {
     fontFamily: 'arial, sans-serif',
     marginBottom: 26,
+  },
+  titleLink: {
+    textDecoration: 'none',
   },
   title: {
     fontFamily: 'Roboto, arial, sans-serif',
@@ -31,6 +35,27 @@ const styles = () => ({
     color: '#505050',
     overflowWrap: 'break-word',
   },
+  deepLinksContainer: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    marginTop: 2,
+  },
+  deepLinkParent: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  deepLink: {
+    color: '#1a0dab',
+    fontSize: 13,
+    lineHeight: 1.5,
+    textDecoration: 'none',
+  },
+  deepLinkDivider: {
+    color: 'rgba(0, 0, 0, 0.66)',
+    width: 3,
+    fontSize: 10,
+    margin: '0px 8px',
+  },
 })
 
 // TODO: use class styles
@@ -38,7 +63,7 @@ const styles = () => ({
 const WebPageSearchResult = props => {
   const {
     classes,
-    item: { displayUrl, name, snippet, url },
+    item: { deepLinks, displayUrl, name, snippet, url },
   } = props
 
   // If any required props are missing, don't render anything.
@@ -47,11 +72,27 @@ const WebPageSearchResult = props => {
   }
   return (
     <div className={classes.container}>
-      <a href={url} style={{ textDecoration: 'none' }}>
+      <a href={url} className={classes.titleLink}>
         <h3 className={classes.title}>{name}</h3>
       </a>
       <div className={classes.displayUrl}>{displayUrl}</div>
       <div className={classes.snippet}>{snippet}</div>
+      {deepLinks && deepLinks.length ? (
+        <div className={classes.deepLinksContainer}>
+          {deepLinks.map((deepLink, index) => {
+            return (
+              <div key={deepLink.url} className={classes.deepLinkParent}>
+                <a href={deepLink.url} className={classes.deepLink}>
+                  {deepLink.name}
+                </a>
+                {index < deepLinks.length - 1 ? (
+                  <CircleIcon className={classes.deepLinkDivider} />
+                ) : null}
+              </div>
+            )
+          })}
+        </div>
+      ) : null}
     </div>
   )
 }
