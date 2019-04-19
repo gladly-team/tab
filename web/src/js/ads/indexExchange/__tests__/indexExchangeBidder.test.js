5a4e2bd7dbdc49f511512224beede3dae5fd092f
Kevin Jennison <kevin.jennison1@gmail.com>
2019-04-19 14:56:02 -0700

    Don't use imports in tests

--- web/src/js/ads/indexExchange/__tests__/indexExchangeBidder.test.js
+++ web/src/js/ads/indexExchange/__tests__/indexExchangeBidder.test.js
@@ -1,23 +1,22 @@
 /* eslint-env jest */
 
-import getIndexExchangeTag from 'js/ads/indexExchange/getIndexExchangeTag'
-import getGoogleTag from 'js/ads/google/getGoogleTag'
-import { getDefaultTabGlobal } from 'js/utils/test-utils'
-import { getNumberOfAdsToShow } from 'js/ads/adSettings'
-
 jest.mock('js/ads/adSettings')
 jest.mock('js/ads/indexExchange/getIndexExchangeTag')
 
 beforeEach(() => {
   // Mock the IX tag
   delete window.headertag
+  const getIndexExchangeTag = require('js/ads/indexExchange/getIndexExchangeTag')
+    .default
   window.headertag = getIndexExchangeTag()
 
   // Mock tabforacause global
+  const { getDefaultTabGlobal } = require('js/utils/test-utils')
   window.tabforacause = getDefaultTabGlobal()
 
   // Set up googletag
   delete window.googletag
+  const getGoogleTag = require('js/ads/google/getGoogleTag').default
   window.googletag = getGoogleTag()
 })
 
@@ -40,8 +39,11 @@ describe('indexExchangeBidder', () => {
   })
 
   it('only gets bids for the leaderboard ad when one ad is enabled', async () => {
+    const { getNumberOfAdsToShow } = require('js/ads/adSettings')
     getNumberOfAdsToShow.mockReturnValue(1)
     const indexExchangeBidder = require('js/ads/indexExchange/indexExchangeBidder')
+      .default
+    const getIndexExchangeTag = require('js/ads/indexExchange/getIndexExchangeTag')
       .default
     const ixTag = getIndexExchangeTag()
     await indexExchangeBidder()
@@ -52,8 +54,11 @@ describe('indexExchangeBidder', () => {
   })
 
   it('gets bids for the leaderboard and rectangle ads when two ads are enabled', async () => {
+    const { getNumberOfAdsToShow } = require('js/ads/adSettings')
     getNumberOfAdsToShow.mockReturnValue(2)
     const indexExchangeBidder = require('js/ads/indexExchange/indexExchangeBidder')
+      .default
+    const getIndexExchangeTag = require('js/ads/indexExchange/getIndexExchangeTag')
       .default
     const ixTag = getIndexExchangeTag()
     await indexExchangeBidder()
@@ -65,8 +70,11 @@ describe('indexExchangeBidder', () => {
   })
 
   it('gets bids for the leaderboard and rectangle ads when three ads are enabled', async () => {
+    const { getNumberOfAdsToShow } = require('js/ads/adSettings')
     getNumberOfAdsToShow.mockReturnValue(3)
     const indexExchangeBidder = require('js/ads/indexExchange/indexExchangeBidder')
+      .default
+    const getIndexExchangeTag = require('js/ads/indexExchange/getIndexExchangeTag')
       .default
     const ixTag = getIndexExchangeTag()
     await indexExchangeBidder()
