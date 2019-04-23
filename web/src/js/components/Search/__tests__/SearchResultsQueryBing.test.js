8f553bacdd9afff972f66bd4b064d0958aac0127
Kevin Jennison <kevin.jennison1@gmail.com>
2019-04-23 15:10:23 -0700

    Add some tests

--- web/src/js/components/Search/__tests__/SearchResultsQueryBing.test.js
+++ web/src/js/components/Search/__tests__/SearchResultsQueryBing.test.js
@@ -4,16 +4,19 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import { getMockSuccessfulSearchQuery } from 'js/utils/test-utils-search'
 import fetchBingSearchResults from 'js/components/Search/fetchBingSearchResults'
+import { flushAllPromises } from 'js/utils/test-utils'
+import SearchResultsBing from 'js/components/Search/SearchResultsBing'
 
 jest.mock('js/components/Search/fetchBingSearchResults')
 jest.mock('js/components/Search/SearchResultsBing')
 jest.mock('js/authentication/user')
 jest.mock('js/mutations/LogSearchMutation')
+jest.mock('js/utils/logger')
 
 // TODO: add tests
 
 const getMockProps = () => ({
-  query: null,
+  query: 'tacos',
   page: null,
   onPageChange: jest.fn(),
   searchSource: null,
@@ -30,5 +33,41 @@ describe('SearchResultsQueryBing', () => {
       .default
     const mockProps = getMockProps()
     shallow(<SearchResultsQueryBing {...mockProps} />)
+  })
+
+  it('passes isError=false to SearchResultsBing when the query succeeds', async () => {
+    expect.assertions(1)
+    const SearchResultsQueryBing = require('js/components/Search/SearchResultsQueryBing')
+      .default
+    const mockProps = getMockProps()
+    const wrapper = shallow(<SearchResultsQueryBing {...mockProps} />)
+    await flushAllPromises()
+    expect(wrapper.find(SearchResultsBing).prop('isError')).toBe(false)
+  })
+
+  it('passes isError=true to SearchResultsBing when the query request throws', async () => {
+    expect.assertions(1)
+    const SearchResultsQueryBing = require('js/components/Search/SearchResultsQueryBing')
+      .default
+    const mockProps = getMockProps()
+    fetchBingSearchResults.mockImplementation(() => {
+      throw new Error('Search did not work.')
+    })
+    const wrapper = shallow(<SearchResultsQueryBing {...mockProps} />)
+    await flushAllPromises()
+    expect(wrapper.find(SearchResultsBing).prop('isError')).toBe(true)
+  })
+
+  it('passes the page number to SearchResultsBing', async () => {
+    expect.assertions(2)
+    const SearchResultsQueryBing = require('js/components/Search/SearchResultsQueryBing')
+      .default
+    const mockProps = getMockProps()
+    mockProps.page = 124
+    const wrapper = shallow(<SearchResultsQueryBing {...mockProps} />)
+    await flushAllPromises()
+    expect(wrapper.find(SearchResultsBing).prop('page')).toEqual(124)
+    wrapper.setProps({ page: 2 })
+    expect(wrapper.find(SearchResultsBing).prop('page')).toEqual(2)
   })
 })
