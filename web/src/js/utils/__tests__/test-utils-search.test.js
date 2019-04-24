101c3a38926bbb8e99846f2ef3ec9f2ab7b942a4
Kevin Jennison <kevin.jennison1@gmail.com>
2019-04-24 14:39:34 -0700

    Remove duplicate tests

--- web/src/js/utils/__tests__/test-utils-search.test.js
+++ web/src/js/utils/__tests__/test-utils-search.test.js
@@ -62,56 +62,6 @@ describe('getMockBingWebPageDeepLinkObject', () => {
   })
 })
 
-describe('getMockBingWebPageResult', () => {
-  it('includes the expected keys', () => {
-    const { getMockBingWebPageResult } = require('js/utils/test-utils-search')
-    expect(Object.keys(getMockBingWebPageResult()).sort()).toEqual([
-      'dateLastCrawled',
-      'deepLinks',
-      'displayUrl',
-      'id',
-      'name',
-      'searchTags',
-      'snippet',
-      'url',
-    ])
-  })
-
-  it('generates unique IDs for each mock webpage', () => {
-    const { getMockBingWebPageResult } = require('js/utils/test-utils-search')
-    expect(getMockBingWebPageResult().id).not.toEqual(
-      getMockBingWebPageResult().id
-    )
-  })
-
-  it('generates unique URLs for each mock webpage', () => {
-    const { getMockBingWebPageResult } = require('js/utils/test-utils-search')
-    expect(getMockBingWebPageResult().url).not.toEqual(
-      getMockBingWebPageResult().url
-    )
-  })
-
-  it('allows overriding values', () => {
-    const { getMockBingWebPageResult } = require('js/utils/test-utils-search')
-    const defaultData = getMockBingWebPageResult()
-    expect(defaultData).toMatchObject({
-      dateLastCrawled: '2018-12-24T15:23:39',
-      displayUrl: 'https://example.com',
-      name: 'A <b>Really Awesome</b> Webpage',
-    })
-    expect(
-      getMockBingWebPageResult({
-        dateLastCrawled: '2020-11-03T20:01:21',
-        displayUrl: 'https://another-example.com',
-      })
-    ).toMatchObject({
-      dateLastCrawled: '2020-11-03T20:01:21',
-      displayUrl: 'https://another-example.com',
-      name: 'A <b>Really Awesome</b> Webpage',
-    })
-  })
-})
-
 describe('getMockBingNewsArticleResult', () => {
   it('includes the expected keys', () => {
     const {
