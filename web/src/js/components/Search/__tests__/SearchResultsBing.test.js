99eabc98a8da418f03c019ea48f6870d82ac0f50
Kevin Jennison <kevin.jennison1@gmail.com>
2019-04-24 15:27:16 -0700

    Add tests

--- web/src/js/components/Search/__tests__/SearchResultsBing.test.js
+++ web/src/js/components/Search/__tests__/SearchResultsBing.test.js
@@ -2,6 +2,7 @@
 
 import React from 'react'
 import { mount, shallow } from 'enzyme'
+import { range } from 'lodash/util'
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
@@ -225,35 +226,6 @@ describe('SearchResultsBing: tests for non-results display', () => {
         .exists()
     ).toBe(true)
   })
-
-  it('uses our secondary color as the color of the button text of the current results page', () => {
-    const SearchResultsBing = require('js/components/Search/SearchResultsBing')
-      .default
-    const mockProps = getMockProps()
-    mockProps.query = 'ice cream'
-    mockProps.page = 3
-    const ourTheme = createMuiTheme({
-      palette: {
-        primary: {
-          main: '#dedede',
-        },
-        secondary: {
-          main: '#b94f4f',
-        },
-      },
-    })
-    const wrapper = mount(
-      <MuiThemeProvider theme={ourTheme}>
-        <SearchResultsBing {...mockProps} />
-      </MuiThemeProvider>
-    )
-    expect(
-      wrapper
-        .find('[data-test-id="pagination-3"]')
-        .first()
-        .prop('style')
-    ).toHaveProperty('color', '#b94f4f')
-  })
 })
 
 describe('SearchResultsBing: tests for pagination', () => {
@@ -337,6 +309,145 @@ describe('SearchResultsBing: tests for pagination', () => {
     ).toHaveProperty('display', 'none')
   })
 
+  it('does not render the "previous page" pagination button when on the first page', () => {
+    const SearchResultsBing = require('js/components/Search/SearchResultsBing')
+      .default
+    const mockProps = getMockProps()
+    mockProps.page = 1
+    const wrapper = shallow(<SearchResultsBing {...mockProps} />).dive()
+    expect(wrapper.find('[data-test-id="pagination-previous"]').exists()).toBe(
+      false
+    )
+  })
+
+  it('renders the "previous page" pagination button when on the second page', () => {
+    const SearchResultsBing = require('js/components/Search/SearchResultsBing')
+      .default
+    const mockProps = getMockProps()
+    mockProps.page = 2
+    const wrapper = shallow(<SearchResultsBing {...mockProps} />).dive()
+    expect(wrapper.find('[data-test-id="pagination-previous"]').exists()).toBe(
+      true
+    )
+  })
+
+  it('renders the "previous page" pagination button when on the eleventh page', () => {
+    const SearchResultsBing = require('js/components/Search/SearchResultsBing')
+      .default
+    const mockProps = getMockProps()
+    mockProps.page = 11
+    const wrapper = shallow(<SearchResultsBing {...mockProps} />).dive()
+    expect(wrapper.find('[data-test-id="pagination-previous"]').exists()).toBe(
+      true
+    )
+  })
+
+  it('does render the 9999th pagination button when on the final page (page 9999)', () => {
+    const SearchResultsBing = require('js/components/Search/SearchResultsBing')
+      .default
+    const mockProps = getMockProps()
+    mockProps.page = 9999
+    const wrapper = shallow(<SearchResultsBing {...mockProps} />).dive()
+    expect(wrapper.find('[data-test-id="pagination-9999"]').exists()).toBe(true)
+  })
+
+  it('does not render the "next page" pagination button when on the final page (page 9999)', () => {
+    const SearchResultsBing = require('js/components/Search/SearchResultsBing')
+      .default
+    const mockProps = getMockProps()
+    mockProps.page = 9999
+    const wrapper = shallow(<SearchResultsBing {...mockProps} />).dive()
+    expect(wrapper.find('[data-test-id="pagination-next"]').exists()).toBe(
+      false
+    )
+  })
+
+  it('renders the "next page" pagination button when on the first page', () => {
+    const SearchResultsBing = require('js/components/Search/SearchResultsBing')
+      .default
+    const mockProps = getMockProps()
+    mockProps.page = 1
+    const wrapper = shallow(<SearchResultsBing {...mockProps} />).dive()
+    expect(wrapper.find('[data-test-id="pagination-next"]').exists()).toBe(true)
+  })
+
+  it('renders the "next page" pagination button when on the second page', () => {
+    const SearchResultsBing = require('js/components/Search/SearchResultsBing')
+      .default
+    const mockProps = getMockProps()
+    mockProps.page = 2
+    const wrapper = shallow(<SearchResultsBing {...mockProps} />).dive()
+    expect(wrapper.find('[data-test-id="pagination-next"]').exists()).toBe(true)
+  })
+
+  it('renders the "next page" pagination button when on the eleventh page', () => {
+    const SearchResultsBing = require('js/components/Search/SearchResultsBing')
+      .default
+    const mockProps = getMockProps()
+    mockProps.page = 11
+    const wrapper = shallow(<SearchResultsBing {...mockProps} />).dive()
+    expect(wrapper.find('[data-test-id="pagination-next"]').exists()).toBe(true)
+  })
+
+  it('renders the expected pagination buttons when on the first page', () => {
+    const SearchResultsBing = require('js/components/Search/SearchResultsBing')
+      .default
+    const mockProps = getMockProps()
+    mockProps.page = 11
+    const wrapper = shallow(<SearchResultsBing {...mockProps} />).dive()
+    const expectedPages = range(7, 15)
+    expectedPages.forEach(pageNum => {
+      expect(
+        wrapper.find(`[data-test-id="pagination-${pageNum}"]`).exists()
+      ).toBe(true)
+    })
+
+    // Page 6 should not exist.
+    expect(wrapper.find(`[data-test-id="pagination-6"]`).exists()).toBe(false)
+
+    // Page 15 should not exist.
+    expect(wrapper.find(`[data-test-id="pagination-15"]`).exists()).toBe(false)
+  })
+
+  it('renders the expected pagination buttons when on the eleventh page', () => {
+    const SearchResultsBing = require('js/components/Search/SearchResultsBing')
+      .default
+    const mockProps = getMockProps()
+    mockProps.page = 11
+    const wrapper = shallow(<SearchResultsBing {...mockProps} />).dive()
+    const expectedPages = range(7, 15)
+    expectedPages.forEach(pageNum => {
+      expect(
+        wrapper.find(`[data-test-id="pagination-${pageNum}"]`).exists()
+      ).toBe(true)
+    })
+
+    // Page 6 should not exist.
+    expect(wrapper.find(`[data-test-id="pagination-6"]`).exists()).toBe(false)
+
+    // Page 15 should not exist.
+    expect(wrapper.find(`[data-test-id="pagination-15"]`).exists()).toBe(false)
+  })
+
+  it('renders the expected pagination buttons when on the 9998th page', () => {
+    const SearchResultsBing = require('js/components/Search/SearchResultsBing')
+      .default
+    const mockProps = getMockProps()
+    mockProps.page = 9998
+    const wrapper = shallow(<SearchResultsBing {...mockProps} />).dive()
+    const expectedPages = range(9991, 9999)
+    expectedPages.forEach(pageNum => {
+      expect(
+        wrapper.find(`[data-test-id="pagination-${pageNum}"]`).exists()
+      ).toBe(true)
+    })
+
+    // Page 10000 should not exist.
+    expect(wrapper.find(`[data-test-id="pagination-10000"]`).exists()).toBe(
+      false
+    )
+  })
+
   it('calls the onPageChange prop when clicking to a new results page', () => {
     const SearchResultsBing = require('js/components/Search/SearchResultsBing')
       .default
@@ -370,5 +481,58 @@ describe('SearchResultsBing: tests for pagination', () => {
     const wrapper = shallow(<SearchResultsBing {...mockProps} />).dive()
     wrapper.find('[data-test-id="pagination-previous"]').simulate('click')
     expect(mockProps.onPageChange).toHaveBeenCalledWith(6)
+  })
+
+  it('disables the button of the current results page', () => {
+    const SearchResultsBing = require('js/components/Search/SearchResultsBing')
+      .default
+    const mockProps = getMockProps()
+    mockProps.query = 'ice cream'
+    mockProps.page = 3
+    const wrapper = shallow(<SearchResultsBing {...mockProps} />).dive()
+    expect(wrapper.find('[data-test-id="pagination-3"]').prop('disabled')).toBe(
+      true
+    )
+  })
+
+  it('does not disable buttons for other results pages besides the one we are on', () => {
+    const SearchResultsBing = require('js/components/Search/SearchResultsBing')
+      .default
+    const mockProps = getMockProps()
+    mockProps.query = 'ice cream'
+    mockProps.page = 3
+    const wrapper = shallow(<SearchResultsBing {...mockProps} />).dive()
+    expect(
+      wrapper.find('[data-test-id="pagination-4"]').prop('disabled')
+    ).toBeUndefined()
+  })
+
+  it('uses our secondary color as the color of the button text of the current results page', () => {
+    const SearchResultsBing = require('js/components/Search/SearchResultsBing')
+      .default
+    const mockProps = getMockProps()
+    mockProps.query = 'ice cream'
+    mockProps.page = 3
+    const ourTheme = createMuiTheme({
+      palette: {
+        primary: {
+          main: '#dedede',
+        },
+        secondary: {
+          main: '#b94f4f',
+        },
+      },
+    })
+    const wrapper = mount(
+      <MuiThemeProvider theme={ourTheme}>
+        <SearchResultsBing {...mockProps} />
+      </MuiThemeProvider>
+    )
+    expect(
+      wrapper
+        .find('[data-test-id="pagination-3"]')
+        .first()
+        .prop('style')
+    ).toHaveProperty('color', '#b94f4f')
   })
 })
