fc13b0aa9810c85c621d0ff4c5dda0a145ac337e
Kevin Jennison <kevin.jennison1@gmail.com>
2019-06-03 12:25:45 -0700

    WIP function to check search rate limits

---
+++ graphql/database/users/__tests__/checkSearchRateLimit.test.js
@@ -0,0 +1,104 @@
+/* eslint-env jest */
+
+import {
+  DatabaseOperation,
+  clearAllMockDBResponses,
+  getMockUserContext,
+  getMockUserInfo,
+  mockDate,
+  setMockDBResponse,
+} from '../../test-utils'
+
+import UserSearchLogModel from '../UserSearchLogModel'
+import UserModel from '../UserModel'
+
+jest.mock('../../databaseClient')
+const userContext = getMockUserContext()
+const mockCurrentTime = '2017-06-22T01:13:28.000Z'
+
+beforeAll(() => {
+  mockDate.on(mockCurrentTime, {
+    mockCurrentTimeOnly: true,
+  })
+})
+
+afterEach(() => {
+  jest.clearAllMocks()
+  clearAllMockDBResponses()
+})
+
+afterAll(() => {
+  mockDate.off()
+})
+
+describe('checkSearchRateLimit', () => {
+  test('checkSearchRateLimit calls the database', async () => {
+    const userId = getMockUserInfo().id
+    const checkSearchRateLimit = require('../checkSearchRateLimit').default
+
+    // Spy on query methods
+    const query = jest.spyOn(UserSearchLogModel, 'query')
+    const queryExec = jest.spyOn(UserSearchLogModel, '_execAsync')
+
+    await checkSearchRateLimit(userContext, userId)
+
+    expect(query).toHaveBeenCalledWith(userContext, userId)
+    expect(queryExec).toHaveBeenCalled()
+  })
+
+  test('checkSearchRateLimit ReferralDataLog database queries as expected', async () => {
+    const userId = getMockUserInfo().id
+    const checkSearchRateLimit = require('../checkSearchRateLimit').default
+
+    // Mock UserSearchLogModel query
+    const referralLogQueryMock = setMockDBResponse(DatabaseOperation.QUERY, {
+      Items: [],
+    })
+    await checkSearchRateLimit(userContext, userId)
+
+    expect(referralLogQueryMock.mock.calls[0][0]).toEqual({
+      ExpressionAttributeNames: {
+        '#created': 'created',
+        '#userId': 'userId',
+      },
+      ExpressionAttributeValues: {
+        ':created': '2017-06-22T01:03:28.000Z', // 10 minutes earlier
+        ':created_2': '2017-06-22T01:13:28.000Z',
+        ':userId': userId,
+      },
+      KeyConditionExpression:
+        '(#created BETWEEN :created AND :created_2) AND (#userId = :userId)',
+      TableName: UserSearchLogModel.tableName,
+    })
+  })
+
+  test('checkSearchRateLimit (with no search logs) returns expected value', async () => {
+    const userId = getMockUserInfo().id
+    const checkSearchRateLimit = require('../checkSearchRateLimit').default
+
+    // Mock UserSearchLogModel query
+    const referralDataLogsToReturn = []
+    setMockDBResponse(DatabaseOperation.QUERY, {
+      Items: referralDataLogsToReturn,
+    })
+
+    // Mock User query
+    const recruitedUsersToReturn = []
+    const recruitedUsersQueryMock = setMockDBResponse(
+      DatabaseOperation.GET_BATCH,
+      {
+        Responses: {
+          [UserModel.tableName]: recruitedUsersToReturn,
+        },
+      }
+    )
+
+    const returnedVal = await checkSearchRateLimit(userContext, userId)
+    expect(recruitedUsersQueryMock).not.toHaveBeenCalled()
+    expect(returnedVal).toEqual({
+      limitReached: false,
+      reason: 'NONE',
+      checkIfHuman: false,
+    })
+  })
+})
---
+++ graphql/database/users/checkSearchRateLimit.js
@@ -0,0 +1,77 @@
+import moment from 'moment'
+
+import { isValidISOString } from '../../utils/utils'
+import UserSearchLogModel from './UserSearchLogModel'
+// import UserModel from './UserModel'
+
+/**
+ * Get an array of a user's search logs in a certain time period.
+ * @param {Object} userContext - The user authorizer object.
+ * @param {String} userId - The user ID.
+ * @param {Object} time
+ * @param {String} time.start - An ISO string of the earliest search log
+ *   to filter on
+ * @param {String} time.end - An ISO string of the latest search log
+ *   to filter on
+ * @return {Promise<Object[]>} searchLogs - A list of UserSearchLog objects
+ */
+const getSearchLogs = async (userContext, userId, time = {}) => {
+  // Build the basic query
+  const searchLogsQuery = UserSearchLogModel.query(userContext, userId)
+
+  // Validate startTime and/or endTime, if provided
+  if (time.start && !isValidISOString(time.start)) {
+    throw new Error('Invalid `time.start` argument. It must be an ISO string.')
+  }
+  if (time.end && !isValidISOString(time.end)) {
+    throw new Error('Invalid `time.end` argument. It must be an ISO string.')
+  }
+
+  // Filter by time.start and/or time.end, if provided
+  if (time.start && time.end) {
+    searchLogsQuery.where('created').between(time.start, time.end)
+  } else if (time.start) {
+    searchLogsQuery.where('created').gte(time.start)
+  } else if (time.end) {
+    searchLogsQuery.where('created').lte(time.end)
+  }
+  return searchLogsQuery.execute()
+}
+
+/**
+ * Get the count of recruits returned from the `getRecruits` query
+ *   who remained active for at least one day after joining.
+ * @param {Object} userContext - The user authorizer object.
+ * @param {String} userId - The user ID.
+ * @return {Promise<Object>} searchRateLimitInfo - A Promise that resolves
+ *   into info about whether the user has exceeded search rate limits.
+ * @param {Boolean} limitReached - Whether the user has reached some
+ *   rate limit
+ * @param {String} reason - The reason the user's search is rate-limited.
+ *   One of: ONE_MINUTE_MAX, FIVE_MINUTE_MAX, DAILY_MAX, or NONE (if
+ *   limitReached is false).
+ * @param {Boolean} checkIfHuman - Whether it's possible the user is a
+ *   bot, and we should chec,k
+ */
+const checkSearchRateLimit = async (userContext, userId) => {
+  const now = moment.utc()
+  const time = {
+    start: now
+      .clone()
+      .subtract(10, 'minutes')
+      .toISOString(),
+    end: now.clone().toISOString(),
+  }
+
+  // eslint-disable-next-line no-unused-vars
+  const searchLogs = await getSearchLogs(userContext, userId, time)
+
+  // TODO
+  return {
+    limitReached: false,
+    reason: 'NONE',
+    checkIfHuman: false,
+  }
+}
+
+export default checkSearchRateLimit
