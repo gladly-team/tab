89eb3f911e663e93712d641a749e68c8dae6a4b5
Kevin Jennison <kevin.jennison1@gmail.com>
2019-06-03 15:23:26 -0700

    Add logic to count searches within recent time buckets

--- graphql/database/users/__tests__/checkSearchRateLimit.test.js
+++ graphql/database/users/__tests__/checkSearchRateLimit.test.js
@@ -17,6 +17,21 @@ jest.mock('../../databaseClient')
 const userContext = getMockUserContext()
 const mockCurrentTime = '2017-06-22T01:13:28.000Z'
 
+const MAX_SEARCHES_ONE_MINUTE = 6
+const MAX_SEARCHES_FIVE_MINUTES = 15
+const MAX_SEARCHES_ONE_DAY = 150
+
+const createMockSearchLogItem = item =>
+  Object.assign(
+    {},
+    {
+      userId: 'abc123',
+      timestamp: moment.utc().toISOString(),
+      source: 'self',
+    },
+    item
+  )
+
 beforeAll(() => {
   mockDate.on(mockCurrentTime, {
     mockCurrentTimeOnly: true,
@@ -133,7 +148,7 @@ describe('checkSearchRateLimit', () => {
           },
           recentDay: {
             date: moment.utc().toISOString(),
-            numSearches: 153, // above daily maximum
+            numSearches: MAX_SEARCHES_ONE_DAY + 3, // above daily maximum
           },
         },
       }),
@@ -144,7 +159,7 @@ describe('checkSearchRateLimit', () => {
     expect(userSearchLogQuery).not.toHaveBeenCalled()
   })
 
-  it('returns the expected value when there are no search logs', async () => {
+  it('returns no search limit when there are no search logs', async () => {
     expect.assertions(1)
     const userId = getMockUserInfo().id
     const checkSearchRateLimit = require('../checkSearchRateLimit').default
@@ -157,6 +172,225 @@ describe('checkSearchRateLimit', () => {
     // Mock UserSearchLogModel query
     setMockDBResponse(DatabaseOperation.QUERY, {
       Items: [],
+    })
+
+    const returnedVal = await checkSearchRateLimit(userContext, userId)
+    expect(returnedVal).toEqual({
+      limitReached: false,
+      reason: 'NONE',
+      checkIfHuman: false,
+    })
+  })
+
+  it('returns no search limit when the user opened fewer than max search limits', async () => {
+    expect.assertions(1)
+    const userId = getMockUserInfo().id
+    const checkSearchRateLimit = require('../checkSearchRateLimit').default
+
+    // Mock getting this user.
+    setMockDBResponse(DatabaseOperation.GET, {
+      Item: getMockUserInstance(),
+    })
+
+    // Mock UserSearchLogModel query
+    setMockDBResponse(DatabaseOperation.QUERY, {
+      Items: Array.from({ length: MAX_SEARCHES_ONE_MINUTE - 3 }).map(() => {
+        return createMockSearchLogItem({
+          timestamp: moment.utc().subtract(23, 'seconds'),
+        })
+      }),
+    })
+
+    const returnedVal = await checkSearchRateLimit(userContext, userId)
+    expect(returnedVal).toEqual({
+      limitReached: false,
+      reason: 'NONE',
+      checkIfHuman: false,
+    })
+  })
+
+  it('returns no search limit when the user has opened exactly the max search limits', async () => {
+    expect.assertions(1)
+    const userId = getMockUserInfo().id
+    const checkSearchRateLimit = require('../checkSearchRateLimit').default
+
+    // Mock getting this user.
+    setMockDBResponse(DatabaseOperation.GET, {
+      Item: getMockUserInstance(),
+    })
+
+    // Mock UserSearchLogModel query
+    setMockDBResponse(DatabaseOperation.QUERY, {
+      Items: Array.from({ length: MAX_SEARCHES_ONE_MINUTE }).map(() => {
+        return createMockSearchLogItem({
+          timestamp: moment.utc().subtract(23, 'seconds'),
+        })
+      }),
+    })
+
+    const returnedVal = await checkSearchRateLimit(userContext, userId)
+    expect(returnedVal).toEqual({
+      limitReached: false,
+      reason: 'NONE',
+      checkIfHuman: false,
+    })
+  })
+
+  it('returns no search limit when the user opened above the max searches in a minute but that was over 1 minute ago', async () => {
+    expect.assertions(1)
+    const userId = getMockUserInfo().id
+    const checkSearchRateLimit = require('../checkSearchRateLimit').default
+
+    // Mock getting this user.
+    setMockDBResponse(DatabaseOperation.GET, {
+      Item: getMockUserInstance(),
+    })
+
+    // Mock UserSearchLogModel query
+    setMockDBResponse(DatabaseOperation.QUERY, {
+      Items: Array.from({ length: MAX_SEARCHES_ONE_MINUTE + 3 }).map(() => {
+        return createMockSearchLogItem({
+          timestamp: moment.utc().subtract(62, 'seconds'),
+        })
+      }),
+    })
+
+    const returnedVal = await checkSearchRateLimit(userContext, userId)
+    expect(returnedVal).toEqual({
+      limitReached: false,
+      reason: 'NONE',
+      checkIfHuman: false,
+    })
+  })
+
+  it('returns a search limit when the user opened more than the max searches in a minute', async () => {
+    expect.assertions(1)
+    const userId = getMockUserInfo().id
+    const checkSearchRateLimit = require('../checkSearchRateLimit').default
+
+    // Mock getting this user.
+    setMockDBResponse(DatabaseOperation.GET, {
+      Item: getMockUserInstance(),
+    })
+
+    // Mock UserSearchLogModel query
+    setMockDBResponse(DatabaseOperation.QUERY, {
+      Items: Array.from({ length: MAX_SEARCHES_ONE_MINUTE + 2 }).map(() => {
+        return createMockSearchLogItem({
+          timestamp: moment.utc().subtract(23, 'seconds'),
+        })
+      }),
+    })
+
+    const returnedVal = await checkSearchRateLimit(userContext, userId)
+    expect(returnedVal).toEqual({
+      limitReached: true,
+      reason: 'ONE_MINUTE_MAX',
+      checkIfHuman: false,
+    })
+  })
+
+  it('returns a search limit when the user opened more than the max searches in five minutes', async () => {
+    expect.assertions(1)
+    const userId = getMockUserInfo().id
+    const checkSearchRateLimit = require('../checkSearchRateLimit').default
+
+    // Mock getting this user.
+    setMockDBResponse(DatabaseOperation.GET, {
+      Item: getMockUserInstance(),
+    })
+
+    // Mock UserSearchLogModel query
+    setMockDBResponse(DatabaseOperation.QUERY, {
+      Items: []
+        .concat(
+          Array.from({ length: MAX_SEARCHES_FIVE_MINUTES - 4 }).map(() => {
+            return createMockSearchLogItem({
+              timestamp: moment.utc().subtract(2, 'minutes'),
+            })
+          })
+        )
+        .concat(
+          Array.from({ length: 7 }).map(() => {
+            return createMockSearchLogItem({
+              timestamp: moment.utc().subtract(4, 'minutes'),
+            })
+          })
+        ),
+    })
+
+    const returnedVal = await checkSearchRateLimit(userContext, userId)
+    expect(returnedVal).toEqual({
+      limitReached: true,
+      reason: 'FIVE_MINUTE_MAX',
+      checkIfHuman: false,
+    })
+  })
+
+  it('returns a "five minute" search limit when the user opened both too many tabs in 5 minutes and too many in one minute', async () => {
+    expect.assertions(1)
+    const userId = getMockUserInfo().id
+    const checkSearchRateLimit = require('../checkSearchRateLimit').default
+
+    // Mock getting this user.
+    setMockDBResponse(DatabaseOperation.GET, {
+      Item: getMockUserInstance(),
+    })
+
+    // Mock UserSearchLogModel query
+    setMockDBResponse(DatabaseOperation.QUERY, {
+      Items: []
+        .concat(
+          Array.from({ length: MAX_SEARCHES_ONE_MINUTE + 2 }).map(() => {
+            return createMockSearchLogItem({
+              timestamp: moment.utc().subtract(30, 'seconds'),
+            })
+          })
+        )
+        .concat(
+          Array.from({ length: MAX_SEARCHES_FIVE_MINUTES + 4 }).map(() => {
+            return createMockSearchLogItem({
+              timestamp: moment.utc().subtract(3, 'minutes'),
+            })
+          })
+        ),
+    })
+
+    const returnedVal = await checkSearchRateLimit(userContext, userId)
+    expect(returnedVal).toEqual({
+      limitReached: true,
+      reason: 'FIVE_MINUTE_MAX',
+      checkIfHuman: false,
+    })
+  })
+
+  it('does not return a search limit when the user opened more than the max searches in five minutes but not within the previous 5 minutes', async () => {
+    expect.assertions(1)
+    const userId = getMockUserInfo().id
+    const checkSearchRateLimit = require('../checkSearchRateLimit').default
+
+    // Mock getting this user.
+    setMockDBResponse(DatabaseOperation.GET, {
+      Item: getMockUserInstance(),
+    })
+
+    // Mock UserSearchLogModel query
+    setMockDBResponse(DatabaseOperation.QUERY, {
+      Items: []
+        .concat(
+          Array.from({ length: MAX_SEARCHES_FIVE_MINUTES - 1 }).map(() => {
+            return createMockSearchLogItem({
+              timestamp: moment.utc().subtract(6, 'minutes'),
+            })
+          })
+        )
+        .concat(
+          Array.from({ length: 3 }).map(() => {
+            return createMockSearchLogItem({
+              timestamp: moment.utc().subtract(4, 'minutes'),
+            })
+          })
+        ),
     })
 
     const returnedVal = await checkSearchRateLimit(userContext, userId)
--- graphql/database/users/checkSearchRateLimit.js
+++ graphql/database/users/checkSearchRateLimit.js
@@ -5,12 +5,12 @@ import UserSearchLogModel from './UserSearchLogModel'
 import UserModel from './UserModel'
 import { getTodaySearchCount } from './user-utils'
 
-// const MAX_SEARCHES_ONE_MINUTE = 6
-// const MAX_SEARCHES_FIVE_MINUTES = 15
+const MAX_SEARCHES_ONE_MINUTE = 6
+const MAX_SEARCHES_FIVE_MINUTES = 15
 const MAX_SEARCHES_ONE_DAY = 150
 
-// const LIMIT_REASON_ONE_MINUTE_MAX = 'ONE_MINUTE_MAX'
-// const LIMIT_REASON_FIVE_MINUTE_MAX = 'FIVE_MINUTE_MAX'
+const LIMIT_REASON_ONE_MINUTE_MAX = 'ONE_MINUTE_MAX'
+const LIMIT_REASON_FIVE_MINUTE_MAX = 'FIVE_MINUTE_MAX'
 const LIMIT_REASON_DAILY_MAX = 'DAILY_MAX'
 const LIMIT_REASON_NONE = 'NONE'
 
@@ -93,15 +93,60 @@ const checkSearchRateLimit = async (userContext, userId) => {
     })
   }
 
-  // eslint-disable-next-line no-unused-vars
+  // Determine the number of searches the user has made in
+  // previous time buckets.
   const searchLogs = await getSearchLogs(userContext, userId, time)
+  const searchesPerTimePeriod = searchLogs.reduce(
+    (accumulator, curVal) => {
+      const searchLogTime = moment(curVal.timestamp)
+      const updatedCount = {
+        previousOneMinute: searchLogTime.isBetween(
+          now.clone().subtract(1, 'minute'),
+          now
+        )
+          ? accumulator.previousOneMinute + 1
+          : accumulator.previousOneMinute,
+        previousFiveMinutes: searchLogTime.isBetween(
+          now.clone().subtract(5, 'minutes'),
+          now
+        )
+          ? accumulator.previousFiveMinutes + 1
+          : accumulator.previousFiveMinutes,
+        previousTenMinutes: searchLogTime.isBetween(
+          now.clone().subtract(10, 'minutes'),
+          now
+        )
+          ? accumulator.previousTenMinutes + 1
+          : accumulator.previousTenMinutes,
+      }
+      return updatedCount
+    },
+    {
+      previousOneMinute: 0,
+      previousFiveMinutes: 0,
+      previousTenMinutes: 0,
+    }
+  )
 
-  // TODO
-  return {
-    limitReached: false,
-    reason: 'NONE',
-    checkIfHuman: false,
+  if (searchesPerTimePeriod.previousFiveMinutes > MAX_SEARCHES_FIVE_MINUTES) {
+    return createSearchRateLimit({
+      limitReached: true,
+      reason: LIMIT_REASON_FIVE_MINUTE_MAX,
+      checkIfHuman: false,
+    })
   }
+  if (searchesPerTimePeriod.previousOneMinute > MAX_SEARCHES_ONE_MINUTE) {
+    return createSearchRateLimit({
+      limitReached: true,
+      reason: LIMIT_REASON_ONE_MINUTE_MAX,
+      checkIfHuman: false,
+    })
+  }
+  return createSearchRateLimit({
+    limitReached: false,
+    reason: LIMIT_REASON_NONE,
+    checkIfHuman: false,
+  })
 }
 
 export default checkSearchRateLimit
