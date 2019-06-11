c53ee3227c0f78a9ef226334dd3db50a97ff35bf
Kevin Jennison <kevin.jennison1@gmail.com>
2019-06-11 11:15:58 -0700

    Update documentation

--- web/src/js/components/Search/getBingMarketCode.js
+++ web/src/js/components/Search/getBingMarketCode.js
@@ -1,7 +1,11 @@
 // import { getCountry } from 'js/utils/client-location'
 
 /**
- * Call our search API endpoint.
+ * Get the market code for the Bing search request. This parameter
+ * isn't required but is highly encouraged. Bing has a set list of
+ * supported market codes, but if "you specify a market that is not
+ * listed in Market Codes, Bing uses a best fit market code based on
+ * an internal mapping".
  * https://docs.microsoft.com/en-us/rest/api/cognitiveservices-bingsearch/bing-web-api-v7-reference#mkt
  * @return {Promise<String|null>} The 5-character market code of structure
  *   <ISO language code>-<ISO country code>, if both language and country
