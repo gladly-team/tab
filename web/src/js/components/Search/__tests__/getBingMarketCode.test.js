f6013029de5bc80f71425068007dc1713c70d806
Kevin Jennison <kevin.jennison1@gmail.com>
2019-06-11 16:19:16 -0700

    Get a functional market code

--- web/src/js/components/Search/__tests__/getBingMarketCode.test.js
+++ web/src/js/components/Search/__tests__/getBingMarketCode.test.js
@@ -1,17 +1,172 @@
 /* eslint-env jest */
 
+import { getCountry } from 'js/utils/client-location'
+
 jest.mock('js/utils/client-location')
+
+beforeEach(() => {
+  Object.defineProperty(window.navigator, 'languages', {
+    value: ['es-MX', 'en'],
+    configurable: true,
+    writable: true,
+  })
+  Object.defineProperty(window.navigator, 'language', {
+    value: 'es-MX',
+    configurable: true,
+    writable: true,
+  })
+})
 
 afterEach(() => {
   jest.clearAllMocks()
 })
 
 describe('getBingMarketCode', () => {
-  it('returns en-US', async () => {
+  it('returns the expected mkt when we know the country and 2-character language preference', async () => {
     expect.assertions(1)
+
+    // Belgium
+    getCountry.mockImplementation(() => Promise.resolve('BE'))
+
+    // French
+    Object.defineProperty(window.navigator, 'languages', {
+      value: ['fr'],
+      configurable: true,
+      writable: true,
+    })
+    Object.defineProperty(window.navigator, 'language', {
+      value: 'fr',
+      configurable: true,
+      writable: true,
+    })
+
     const getBingMarketCode = require('js/components/Search/getBingMarketCode')
       .default
     const mkt = await getBingMarketCode()
-    expect(mkt).toEqual('en-US')
+    expect(mkt).toEqual('fr-BE')
+  })
+
+  it('returns the expected mkt when we know the country and 5-character language preference', async () => {
+    expect.assertions(2)
+
+    // Set to U.S.; we should not use this when the language setting
+    // already defines the country/region.
+    getCountry.mockImplementation(() => Promise.resolve('US'))
+
+    // French
+    Object.defineProperty(window.navigator, 'languages', {
+      value: ['es-ES'],
+      configurable: true,
+      writable: true,
+    })
+    Object.defineProperty(window.navigator, 'language', {
+      value: 'es-ES',
+      configurable: true,
+      writable: true,
+    })
+
+    const getBingMarketCode = require('js/components/Search/getBingMarketCode')
+      .default
+    const mkt = await getBingMarketCode()
+    expect(mkt).toEqual('es-ES')
+    expect(getCountry).not.toHaveBeenCalled()
+  })
+
+  it('returns null when we know the language but not the country', async () => {
+    expect.assertions(1)
+
+    // Belgium
+    getCountry.mockImplementation(() => Promise.resolve(null))
+
+    // French
+    Object.defineProperty(window.navigator, 'languages', {
+      value: ['fr'],
+      configurable: true,
+      writable: true,
+    })
+    Object.defineProperty(window.navigator, 'language', {
+      value: 'fr',
+      configurable: true,
+      writable: true,
+    })
+
+    const getBingMarketCode = require('js/components/Search/getBingMarketCode')
+      .default
+    const mkt = await getBingMarketCode()
+    expect(mkt).toBeNull()
+  })
+
+  it('returns null when getting the country code throws an error', async () => {
+    expect.assertions(1)
+
+    // Belgium
+    getCountry.mockImplementation(() => {
+      throw new Error('eeeee!')
+    })
+
+    // French
+    Object.defineProperty(window.navigator, 'languages', {
+      value: ['fr'],
+      configurable: true,
+      writable: true,
+    })
+    Object.defineProperty(window.navigator, 'language', {
+      value: 'fr',
+      configurable: true,
+      writable: true,
+    })
+
+    const getBingMarketCode = require('js/components/Search/getBingMarketCode')
+      .default
+    const mkt = await getBingMarketCode()
+    expect(mkt).toBeNull()
+  })
+
+  it('returns null when we know the country but not the language', async () => {
+    expect.assertions(1)
+
+    // Belgium
+    getCountry.mockImplementation(() => Promise.resolve('FR'))
+
+    // French
+    Object.defineProperty(window.navigator, 'languages', {
+      value: [],
+      configurable: true,
+      writable: true,
+    })
+    Object.defineProperty(window.navigator, 'language', {
+      value: undefined,
+      configurable: true,
+      writable: true,
+    })
+
+    const getBingMarketCode = require('js/components/Search/getBingMarketCode')
+      .default
+    const mkt = await getBingMarketCode()
+    expect(mkt).toBeNull()
+  })
+
+  it('prioritizes the top language preference from navigator.languages over navigator.language', async () => {
+    expect.assertions(1)
+
+    // Belgium
+    getCountry.mockImplementation(() => Promise.resolve('MX'))
+
+    // French
+    Object.defineProperty(window.navigator, 'languages', {
+      value: ['es'],
+      configurable: true,
+      writable: true,
+    })
+    Object.defineProperty(window.navigator, 'language', {
+      value: 'en',
+      configurable: true,
+      writable: true,
+    })
+
+    const getBingMarketCode = require('js/components/Search/getBingMarketCode')
+      .default
+    const mkt = await getBingMarketCode()
+    expect(mkt).toEqual('es-MX')
   })
 })
--- web/src/js/components/Search/getBingMarketCode.js
+++ web/src/js/components/Search/getBingMarketCode.js
@@ -1,4 +1,4 @@
-// import { getCountry } from 'js/utils/client-location'
+import { getCountry } from 'js/utils/client-location'
 
 /**
  * Get the market code for the Bing search request. This parameter
@@ -12,13 +12,32 @@
  *   are known; else, null.
  */
 const getBingMarketCode = async () => {
-  // TODO
-  // // This may be a 2-character or 5-character code.
-  // // https://stackoverflow.com/a/25603630
-  // const language = navigator.languages
-  //     ? navigator.languages[0]
-  //     : navigator.language || navigator.userLanguage
-  return 'en-US'
+  // This may be a 2-character or 5-character code.
+  // https://stackoverflow.com/a/25603630
+  const language = navigator.languages
+    ? navigator.languages[0]
+    : navigator.language || navigator.userLanguage
+  // We do not know the language.
+  if (!language) {
+    return null
+    // The language preference includes a country code.
+  } else if (language.length === 5) {
+    return language
+    // The language preference does not include a country code.
+    // Get the country code ourselves.
+  } else {
+    try {
+      const countryCode = await getCountry()
+      if (!countryCode) {
+        // We don't know the country.
+        return null
+      } else {
+        return `${language}-${countryCode}`
+      }
+    } catch (e) {
+      return null
+    }
+  }
 }
 
 export default getBingMarketCode
