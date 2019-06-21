d96379cdeec4f98b71830758ed8fad5ccdbe46e6
Kevin Jennison <kevin.jennison1@gmail.com>
2019-06-21 11:13:18 -0700

    Add a calculator icon

--- web/src/js/components/Search/ComputationSearchResult.js
+++ web/src/js/components/Search/ComputationSearchResult.js
@@ -3,11 +3,17 @@ import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
+import CalculatorIcon from 'mdi-material-ui/Calculator'
 
 const styles = () => ({
   container: {
     padding: 20,
     marginBottom: 26,
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  calculatorIcon: {
+    color: 'rgba(0, 0, 0, 0.46)',
   },
 })
 
@@ -23,18 +29,22 @@ const ComputationSearchResult = props => {
   }
   return (
     <Paper className={classes.container} elevation={1}>
-      <Typography
-        variant={'body2'}
-        data-test-id={'search-result-computation-expression'}
-      >
-        {expression} =
-      </Typography>
-      <Typography
-        variant={'h4'}
-        data-test-id={'search-result-computation-value'}
-      >
-        {value}
-      </Typography>
+      <div>
+        <Typography
+          variant={'body2'}
+          data-test-id={'search-result-computation-expression'}
+          gutterBottom
+        >
+          {expression} =
+        </Typography>
+        <Typography
+          variant={'h4'}
+          data-test-id={'search-result-computation-value'}
+        >
+          {value}
+        </Typography>
+      </div>
+      <CalculatorIcon className={classes.calculatorIcon} />
     </Paper>
   )
 }
