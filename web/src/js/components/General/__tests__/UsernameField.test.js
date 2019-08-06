bb799499edd2ba0b386c1d776edfa1d33de6d61c
Kevin Jennison <kevin.jennison1@gmail.com>
2019-08-06 14:50:27 -0700

    Remove snapshot test

--- web/src/js/components/General/__tests__/UsernameField.test.js
+++ web/src/js/components/General/__tests__/UsernameField.test.js
@@ -2,19 +2,16 @@
 
 import React from 'react'
 import { shallow } from 'enzyme'
-import toJson from 'enzyme-to-json'
+
+const getMockProps = () => ({
+  usernameDuplicate: false,
+  otherError: false,
+})
 
 describe('UsernameField tests', function() {
   it('renders without error', function() {
     const UsernameField = require('js/components/General/UsernameField').default
-    shallow(<UsernameField usernameDuplicate={false} otherError={false} />)
-  })
-
-  it('matches expected snapshot', function() {
-    const UsernameField = require('js/components/General/UsernameField').default
-    const wrapper = shallow(
-      <UsernameField usernameDuplicate={false} otherError={false} />
-    )
-    expect(toJson(wrapper)).toMatchSnapshot()
+    const mockProps = getMockProps()
+    shallow(<UsernameField {...mockProps} />)
   })
 })
