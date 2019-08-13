ef0010fcf4fbf9fdb8ad590390129aa1828cf45c
Kevin Jennison <kevin.jennison1@gmail.com>
2019-08-13 15:05:52 -0700

    Use functional component

--- web/src/js/components/Settings/Profile/ProfileStatsComponent.js
+++ web/src/js/components/Settings/Profile/ProfileStatsComponent.js
@@ -54,102 +54,100 @@ const styles = theme => ({
   },
 })
 
-class ProfileStats extends React.Component {
-  render() {
-    const { classes, user } = this.props
-    const now = moment().utc()
-    const daysSinceJoined = now.diff(moment(user.joined), 'days')
-    const dayWord = daysSinceJoined === 1 ? 'day' : 'days'
-    const tabberWord = user.numUsersRecruited === 1 ? 'Tabber' : 'Tabbers'
-    const heartsWord = user.vcDonatedAllTime === 1 ? 'Heart' : 'Hearts'
-    const statStyle = {
-      flex: 1,
-      flexBasis: '20%',
-      margin: spacingPx,
-    }
-    const greeting = user.username ? `Hi, ${user.username}!` : 'Hi!'
-    return (
-      <div>
-        <Paper className={classes.messageContainer}>
-          <ChartIcon className={classes.chartIcon} />
-          <Typography variant={'body2'} className={classes.messageText}>
-            <span className={classes.greetingText}>{greeting}</span>
-            <span> Here's all your great work Tabbing, by the numbers.</span>
-          </Typography>
-        </Paper>
-        <span className={classes.statsWrapper}>
-          <Stat
-            stat={commaFormatted(daysSinceJoined)}
-            statText={`${dayWord} as a Tabber`}
-            style={statStyle}
-          />
-          <Stat
-            stat={abbreviateNumber(user.tabs, 1)}
-            statText={'tabs all time'}
-            style={statStyle}
-          />
-          <Stat
-            stat={commaFormatted(user.maxTabsDay.numTabs)}
-            statText={'max tabs in one day'}
-            extraContent={
-              <Typography
-                variant={'body2'}
-                className={classes.statExtraContentText}
-              >
-                on {moment(user.maxTabsDay.date).format('LL')}
-              </Typography>
-            }
-            style={statStyle}
-          />
-          <Stat
-            stat={user.level}
-            statText={'your level'}
-            extraContent={
-              <Typography
-                variant={'body2'}
-                className={classes.statExtraContentText}
-              >
-                <span>{user.heartsUntilNextLevel}</span>
-                <HeartBorderIcon className={classes.heartIcon} />
-                <span>until next level</span>
-              </Typography>
-            }
-            style={statStyle}
-          />
-          <Stat
-            stat={commaFormatted(user.numUsersRecruited)}
-            statText={`${tabberWord} recruited`}
-            style={statStyle}
-            extraContent={
-              <Button
-                color={'primary'}
-                variant={'contained'}
-                onClick={goToInviteFriends}
-                style={{ marginTop: 14 }}
-              >
-                Invite Friends
-              </Button>
-            }
-          />
-          <Stat
-            stat={abbreviateNumber(user.vcDonatedAllTime, 1)}
-            statText={`${heartsWord} donated`}
-            style={statStyle}
-            extraContent={
-              <Button
-                color={'primary'}
-                variant={'contained'}
-                onClick={goToDonate}
-                style={{ marginTop: 14 }}
-              >
-                Donate Hearts
-              </Button>
-            }
-          />
-        </span>
-      </div>
-    )
+const ProfileStats = props => {
+  const { classes, user } = props
+  const now = moment().utc()
+  const daysSinceJoined = now.diff(moment(user.joined), 'days')
+  const dayWord = daysSinceJoined === 1 ? 'day' : 'days'
+  const tabberWord = user.numUsersRecruited === 1 ? 'Tabber' : 'Tabbers'
+  const heartsWord = user.vcDonatedAllTime === 1 ? 'Heart' : 'Hearts'
+  const statStyle = {
+    flex: 1,
+    flexBasis: '20%',
+    margin: spacingPx,
   }
+  const greeting = user.username ? `Hi, ${user.username}!` : 'Hi!'
+  return (
+    <div>
+      <Paper className={classes.messageContainer}>
+        <ChartIcon className={classes.chartIcon} />
+        <Typography variant={'body2'} className={classes.messageText}>
+          <span className={classes.greetingText}>{greeting}</span>
+          <span> Here's all your great work Tabbing, by the numbers.</span>
+        </Typography>
+      </Paper>
+      <span className={classes.statsWrapper}>
+        <Stat
+          stat={commaFormatted(daysSinceJoined)}
+          statText={`${dayWord} as a Tabber`}
+          style={statStyle}
+        />
+        <Stat
+          stat={abbreviateNumber(user.tabs, 1)}
+          statText={'tabs all time'}
+          style={statStyle}
+        />
+        <Stat
+          stat={commaFormatted(user.maxTabsDay.numTabs)}
+          statText={'max tabs in one day'}
+          extraContent={
+            <Typography
+              variant={'body2'}
+              className={classes.statExtraContentText}
+            >
+              on {moment(user.maxTabsDay.date).format('LL')}
+            </Typography>
+          }
+          style={statStyle}
+        />
+        <Stat
+          stat={user.level}
+          statText={'your level'}
+          extraContent={
+            <Typography
+              variant={'body2'}
+              className={classes.statExtraContentText}
+            >
+              <span>{user.heartsUntilNextLevel}</span>
+              <HeartBorderIcon className={classes.heartIcon} />
+              <span>until next level</span>
+            </Typography>
+          }
+          style={statStyle}
+        />
+        <Stat
+          stat={commaFormatted(user.numUsersRecruited)}
+          statText={`${tabberWord} recruited`}
+          style={statStyle}
+          extraContent={
+            <Button
+              color={'primary'}
+              variant={'contained'}
+              onClick={goToInviteFriends}
+              style={{ marginTop: 14 }}
+            >
+              Invite Friends
+            </Button>
+          }
+        />
+        <Stat
+          stat={abbreviateNumber(user.vcDonatedAllTime, 1)}
+          statText={`${heartsWord} donated`}
+          style={statStyle}
+          extraContent={
+            <Button
+              color={'primary'}
+              variant={'contained'}
+              onClick={goToDonate}
+              style={{ marginTop: 14 }}
+            >
+              Donate Hearts
+            </Button>
+          }
+        />
+      </span>
+    </div>
+  )
 }
 
 ProfileStats.propTypes = {
