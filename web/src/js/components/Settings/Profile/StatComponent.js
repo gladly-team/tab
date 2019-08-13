eee47fa621b948e331136503a6d7ce58d7a79136
Kevin Jennison <kevin.jennison1@gmail.com>
2019-08-13 13:25:12 -0700

    WIP - upgrade MUI components in StatComponent

--- web/src/js/components/Settings/Profile/StatComponent.js
+++ web/src/js/components/Settings/Profile/StatComponent.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Paper from 'material-ui/Paper'
-import { alternateAccentColor } from 'js/theme/default'
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
 
-class Stat extends React.Component {
-  render() {
-    const containerStyle = Object.assign(
-      {},
-      {
+const Stat = props => {
+  const { extraContent, stat, statText, style } = props
+  return (
+    <Paper
+      style={{
         paddingTop: 50,
         paddingBottom: 50,
         minWidth: 200,
@@ -15,43 +15,43 @@ class Stat extends React.Component {
         flexDirection: 'column',
         justifyContent: 'center',
         alignContent: 'middle',
-      },
-      this.props.style
-    )
-    const statStyle = {
-      color: alternateAccentColor,
-      display: 'block',
-      fontSize: 50,
-      textAlign: 'center',
-    }
-    const statTextStyle = {
-      display: 'block',
-      textAlign: 'center',
-      maxWidth: '70%',
-    }
-    const extraContentStyle = {
-      display: 'block',
-      marginTop: 4,
-      minHeight: 16,
-      alignSelf: 'center',
-      textAlign: 'center',
-    }
-    return (
-      <Paper style={containerStyle}>
-        <span
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
+        ...style,
+      }}
+    >
+      <span
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Typography
+          variant={'h2'}
+          color={'secondary'}
+          style={{ textAlign: 'center' }}
         >
-          <span style={statStyle}>{this.props.stat}</span>
-          <span style={statTextStyle}>{this.props.statText}</span>
-        </span>
-        <span style={extraContentStyle}>{this.props.extraContent}</span>
-      </Paper>
-    )
-  }
+          {stat}
+        </Typography>
+        <Typography
+          variant={'body2'}
+          style={{ textAlign: 'center', maxWidth: '70%' }}
+        >
+          {statText}
+        </Typography>
+      </span>
+      <span
+        style={{
+          display: 'block',
+          marginTop: 4,
+          minHeight: 16,
+          alignSelf: 'center',
+          textAlign: 'center',
+        }}
+      >
+        {extraContent}
+      </span>
+    </Paper>
+  )
 }
 
 Stat.propTypes = {
