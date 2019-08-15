ad008ae2fd9ceb722ff706e596e0228ec4453277
Kevin Jennison <kevin.jennison1@gmail.com>
2019-08-15 14:26:18 -0700

    Add tests

--- web/src/js/components/Settings/Profile/__tests__/ProfileStatsComponent.test.js
+++ web/src/js/components/Settings/Profile/__tests__/ProfileStatsComponent.test.js
@@ -2,8 +2,27 @@
 
 import React from 'react'
 import { shallow } from 'enzyme'
+import moment from 'moment'
+import MockDate from 'mockdate'
 import Stat from 'js/components/Settings/Profile/StatComponent'
 import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+
+jest.mock('@material-ui/icons/FavoriteBorder', () => () => '[HEART-ICON] ')
+
+const mockNow = '2019-05-19T13:59:58.000Z'
+
+beforeAll(() => {
+  MockDate.set(moment(mockNow))
+})
+
+beforeEach(() => {
+  jest.clearAllMocks()
+})
+
+afterAll(() => {
+  MockDate.reset()
+})
 
 const getMockProps = () => ({
   user: {
@@ -65,6 +84,86 @@ describe('Profile stats component', () => {
         .render()
         .text()
     ).toEqual("Hi! Here's all your great work Tabbing, by the numbers.")
+  })
+
+  it('shows the correct "days since joining" stat', () => {
+    const ProfileStatsComponent = require('js/components/Settings/Profile/ProfileStatsComponent')
+      .default
+    const mockProps = getMockProps()
+    mockProps.user.joined = '2017-12-25T07:08:11.472Z'
+    const wrapper = shallow(<ProfileStatsComponent {...mockProps} />).dive()
+    const statElem = wrapper.find(Stat).first()
+    expect(statElem.prop('stat')).toEqual('510')
+    expect(statElem.prop('statText')).toEqual('days as a Tabber')
+  })
+
+  it('shows the correct "days since joining" stat when it is only one day', () => {
+    const ProfileStatsComponent = require('js/components/Settings/Profile/ProfileStatsComponent')
+      .default
+    const mockProps = getMockProps()
+    mockProps.user.joined = '2019-05-18T08:00:58.000Z'
+    const wrapper = shallow(<ProfileStatsComponent {...mockProps} />).dive()
+    const statElem = wrapper.find(Stat).first()
+    expect(statElem.prop('stat')).toEqual('1')
+    expect(statElem.prop('statText')).toEqual('day as a Tabber')
+  })
+
+  it('shows the correct "tabs all time" stat', () => {
+    const ProfileStatsComponent = require('js/components/Settings/Profile/ProfileStatsComponent')
+      .default
+    const mockProps = getMockProps()
+    mockProps.user.tabs = 3121
+    const wrapper = shallow(<ProfileStatsComponent {...mockProps} />).dive()
+    const statElem = wrapper.find(Stat).at(1)
+    expect(statElem.prop('stat')).toEqual('3.1K')
+    expect(statElem.prop('statText')).toEqual('tabs all time')
+  })
+
+  it('shows the correct "max tabs in one day" stat', () => {
+    const ProfileStatsComponent = require('js/components/Settings/Profile/ProfileStatsComponent')
+      .default
+    const mockProps = getMockProps()
+    mockProps.user.maxTabsDay = {
+      date: '2018-01-01T10:50:44.942Z',
+      numTabs: 431,
+    }
+    const wrapper = shallow(<ProfileStatsComponent {...mockProps} />).dive()
+    const statElem = wrapper.find(Stat).at(2)
+    expect(statElem.prop('stat')).toEqual('431')
+    expect(statElem.prop('statText')).toEqual('max tabs in one day')
+    expect(
+      shallow(statElem.prop('extraContent'))
+        .render()
+        .text()
+    ).toEqual('on January 1, 2018')
+  })
+
+  it('shows the correct "level" stat', () => {
+    const ProfileStatsComponent = require('js/components/Settings/Profile/ProfileStatsComponent')
+      .default
+    const mockProps = getMockProps()
+    mockProps.user.heartsUntilNextLevel = 23
+    mockProps.user.level = 8
+    const wrapper = shallow(<ProfileStatsComponent {...mockProps} />).dive()
+    const statElem = wrapper.find(Stat).at(3)
+    expect(statElem.prop('stat')).toEqual(8)
+    expect(statElem.prop('statText')).toEqual('your level')
+    expect(
+      shallow(statElem.prop('extraContent'))
+        .render()
+        .text()
+    ).toEqual('23[HEART-ICON] until next level')
+  })
+
+  it('shows the correct "friends recruited" stat', () => {
+    const ProfileStatsComponent = require('js/components/Settings/Profile/ProfileStatsComponent')
+      .default
+    const mockProps = getMockProps()
+    mockProps.user.numUsersRecruited = 2
+    const wrapper = shallow(<ProfileStatsComponent {...mockProps} />).dive()
+    const statElem = wrapper.find(Stat).at(4)
+    expect(statElem.prop('stat')).toEqual('2')
+    expect(statElem.prop('statText')).toEqual('Tabbers recruited')
   })
 
   // TODO: add tests for stats and stat-related buttons
