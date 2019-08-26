145ce1a1cdeefeab420a235d66cf4de84a916287
Kevin Jennison <kevin.jennison1@gmail.com>
2019-08-26 11:13:27 -0700

    WIP

--- web/src/js/components/Search/TimeZoneSearchResult.js
+++ web/src/js/components/Search/TimeZoneSearchResult.js
@@ -24,6 +24,101 @@ const styles = () => ({
   },
 })
 
+// E.g. query: "time in florida"
+const TimeZoneCity = props => {
+  const {
+    classes,
+    item: {
+      otherCityTimes,
+      primaryCityTime: { location, time, utcOffset },
+    },
+  } = props
+
+  // If any required props are missing, don't render anything.
+  if (!(location && utcOffset && time)) {
+    return null
+  }
+  const formatDate = isoTime => moment.utc(isoTime).format('dddd, MMMM D, YYYY')
+  const formatTime = isoTime => moment.utc(isoTime).format('h:mm A')
+  return (
+    <Paper className={classes.container} elevation={1}>
+      <Typography
+        variant={'h4'}
+        data-test-id={'search-result-time-zone-time'}
+        gutterBottom
+      >
+        {formatTime(time)}
+      </Typography>
+      <Typography
+        variant={'body2'}
+        data-test-id={'search-result-time-zone-date'}
+      >
+        {formatDate(time)}
+      </Typography>
+      <Typography
+        variant={'body2'}
+        data-test-id={'search-result-time-zone-location'}
+      >
+        Time in {location} ({utcOffset})
+      </Typography>
+      {otherCityTimes && otherCityTimes.length ? (
+        <div data-test-id={'search-result-time-zone-other-locations'}>
+          <Divider className={classes.otherCityTimesDivider} />
+          <table>
+            <tbody>
+              {otherCityTimes.map(cityTime => {
+                return (
+                  <tr key={cityTime.location}>
+                    <td>
+                      <Typography
+                        variant={'body2'}
+                        className={classes.otherCityTime}
+                        gutterBottom
+                      >
+                        {formatTime(cityTime.time)}
+                      </Typography>
+                    </td>
+                    <td>
+                      <Typography
+                        variant={'body2'}
+                        className={classes.otherCityTimeLocation}
+                        gutterBottom
+                      >
+                        {cityTime.location} ({cityTime.utcOffset})
+                      </Typography>
+                    </td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
+      ) : null}
+    </Paper>
+  )
+}
+
+TimeZoneCity.propTypes = {
+  classes: PropTypes.object.isRequired,
+  item: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    otherCityTimes: PropTypes.arrayOf(
+      PropTypes.shape({
+        location: PropTypes.string.isRequired,
+        time: PropTypes.string.isRequired,
+        utcOffset: PropTypes.string.isRequired,
+      })
+    ),
+    primaryCityTime: PropTypes.shape({
+      location: PropTypes.string.isRequired,
+      time: PropTypes.string.isRequired,
+      utcOffset: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+}
+
+TimeZoneCity.defaultProps = {}
+
 const TimeZoneSearchResult = props => {
   const {
     classes,
@@ -32,6 +127,9 @@ const TimeZoneSearchResult = props => {
       primaryCityTime: { location, time, utcOffset },
     },
   } = props
+
+  // Determine the type of time zone info to display.
+  // TODO
 
   // If any required props are missing, don't render anything.
   if (!(location && utcOffset && time)) {
