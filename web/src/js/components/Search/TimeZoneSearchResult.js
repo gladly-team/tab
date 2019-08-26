14a8e6e23c73d80c7d813fbde3b93781d83c7d3d
Kevin Jennison <kevin.jennison1@gmail.com>
2019-08-26 15:42:50 -0700

    Create various versions of the time zone widget

--- web/src/js/components/Search/TimeZoneSearchResult.js
+++ web/src/js/components/Search/TimeZoneSearchResult.js
@@ -6,6 +6,13 @@ import Divider from '@material-ui/core/Divider'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
 
+const TimeZoneInfoPropType = PropTypes.shape({
+  location: PropTypes.string.isRequired,
+  time: PropTypes.string.isRequired,
+  timeZoneName: PropTypes.string,
+  utcOffset: PropTypes.string.isRequired,
+})
+
 const styles = () => ({
   container: {
     padding: 20,
@@ -41,7 +48,11 @@ const TimeZoneCity = props => {
   const formatDate = isoTime => moment.utc(isoTime).format('dddd, MMMM D, YYYY')
   const formatTime = isoTime => moment.utc(isoTime).format('h:mm A')
   return (
-    <Paper className={classes.container} elevation={1}>
+    <Paper
+      data-test-id={'search-result-time-zone-city'}
+      className={classes.container}
+      elevation={1}
+    >
       <Typography
         variant={'h4'}
         data-test-id={'search-result-time-zone-time'}
@@ -102,115 +113,119 @@ TimeZoneCity.propTypes = {
   classes: PropTypes.object.isRequired,
   item: PropTypes.shape({
     id: PropTypes.string.isRequired,
-    otherCityTimes: PropTypes.arrayOf(
-      PropTypes.shape({
-        location: PropTypes.string.isRequired,
-        time: PropTypes.string.isRequired,
-        utcOffset: PropTypes.string.isRequired,
-      })
-    ),
-    primaryCityTime: PropTypes.shape({
-      location: PropTypes.string.isRequired,
-      time: PropTypes.string.isRequired,
-      utcOffset: PropTypes.string.isRequired,
-    }).isRequired,
+    otherCityTimes: PropTypes.arrayOf(TimeZoneInfoPropType),
+    primaryCityTime: TimeZoneInfoPropType.isRequired,
   }).isRequired,
 }
 
 TimeZoneCity.defaultProps = {}
 
+// TODO
+const TimeZoneGeneralInfo = props => {
+  const { classes } = props
+  return (
+    <Paper
+      data-test-id={'search-result-time-zone-general-info'}
+      className={classes.container}
+      elevation={1}
+    >
+      TODO
+    </Paper>
+  )
+}
+TimeZoneGeneralInfo.propTypes = {
+  classes: PropTypes.object.isRequired,
+  item: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+  }).isRequired,
+}
+TimeZoneGeneralInfo.defaultProps = {}
+
+// TODO
+const TimeZoneDifference = props => {
+  const { classes } = props
+  return (
+    <Paper
+      data-test-id={'search-result-time-zone-difference'}
+      className={classes.container}
+      elevation={1}
+    >
+      TODO
+    </Paper>
+  )
+}
+TimeZoneDifference.propTypes = {
+  classes: PropTypes.object.isRequired,
+  item: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+  }).isRequired,
+}
+TimeZoneDifference.defaultProps = {}
+
+// TODO
+const TimeZoneTimeBetween = props => {
+  const { classes } = props
+  return (
+    <Paper
+      data-test-id={'search-result-time-zone-time-between'}
+      className={classes.container}
+      elevation={1}
+    >
+      TODO
+    </Paper>
+  )
+}
+TimeZoneTimeBetween.propTypes = {
+  classes: PropTypes.object.isRequired,
+  item: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+  }).isRequired,
+}
+TimeZoneTimeBetween.defaultProps = {}
+
 const TimeZoneSearchResult = props => {
+  const { item } = props
   const {
     classes,
     item: {
-      otherCityTimes,
-      primaryCityTime: { location, time, utcOffset },
+      description,
+      primaryCityTime,
+      primaryResponse,
+      primaryTimeZone,
+      timeZoneDifference,
     },
   } = props
 
   // Determine the type of time zone info to display.
-  // TODO
-
-  // If any required props are missing, don't render anything.
-  if (!(location && utcOffset && time)) {
+  var TzComponent = null
+  if (primaryCityTime) {
+    TzComponent = TimeZoneCity
+  } else if (timeZoneDifference) {
+    TzComponent = TimeZoneDifference
+  } else if (description && primaryResponse) {
+    TzComponent = TimeZoneTimeBetween
+  } else if (primaryTimeZone) {
+    TzComponent = TimeZoneGeneralInfo
+  } else {
     return null
   }
-  const formatDate = isoTime => moment.utc(isoTime).format('dddd, MMMM D, YYYY')
-  const formatTime = isoTime => moment.utc(isoTime).format('h:mm A')
-  return (
-    <Paper className={classes.container} elevation={1}>
-      <Typography
-        variant={'h4'}
-        data-test-id={'search-result-time-zone-time'}
-        gutterBottom
-      >
-        {formatTime(time)}
-      </Typography>
-      <Typography
-        variant={'body2'}
-        data-test-id={'search-result-time-zone-date'}
-      >
-        {formatDate(time)}
-      </Typography>
-      <Typography
-        variant={'body2'}
-        data-test-id={'search-result-time-zone-location'}
-      >
-        Time in {location} ({utcOffset})
-      </Typography>
-      {otherCityTimes && otherCityTimes.length ? (
-        <div data-test-id={'search-result-time-zone-other-locations'}>
-          <Divider className={classes.otherCityTimesDivider} />
-          <table>
-            <tbody>
-              {otherCityTimes.map(cityTime => {
-                return (
-                  <tr key={cityTime.location}>
-                    <td>
-                      <Typography
-                        variant={'body2'}
-                        className={classes.otherCityTime}
-                        gutterBottom
-                      >
-                        {formatTime(cityTime.time)}
-                      </Typography>
-                    </td>
-                    <td>
-                      <Typography
-                        variant={'body2'}
-                        className={classes.otherCityTimeLocation}
-                        gutterBottom
-                      >
-                        {cityTime.location} ({cityTime.utcOffset})
-                      </Typography>
-                    </td>
-                  </tr>
-                )
-              })}
-            </tbody>
-          </table>
-        </div>
-      ) : null}
-    </Paper>
-  )
+  return <TzComponent classes={classes} item={item} />
 }
 
 TimeZoneSearchResult.propTypes = {
   classes: PropTypes.object.isRequired,
   item: PropTypes.shape({
     id: PropTypes.string.isRequired,
-    otherCityTimes: PropTypes.arrayOf(
-      PropTypes.shape({
-        location: PropTypes.string.isRequired,
-        time: PropTypes.string.isRequired,
-        utcOffset: PropTypes.string.isRequired,
-      })
-    ),
-    primaryCityTime: PropTypes.shape({
-      location: PropTypes.string.isRequired,
-      time: PropTypes.string.isRequired,
-      utcOffset: PropTypes.string.isRequired,
-    }).isRequired,
+    description: PropTypes.string,
+    otherCityTimes: PropTypes.arrayOf(TimeZoneInfoPropType),
+    primaryCityTime: TimeZoneInfoPropType,
+    primaryResponse: PropTypes.string,
+    primaryTimeZone: TimeZoneInfoPropType,
+    text: PropTypes.string,
+    timeZoneDifference: PropTypes.shape({
+      location1: TimeZoneInfoPropType,
+      location2: TimeZoneInfoPropType,
+    }),
   }).isRequired,
 }
 
