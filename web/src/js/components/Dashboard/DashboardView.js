9616ab390cebe9c64a2a199fc5e71ec510b80db9
Kevin Jennison <kevin.jennison1@gmail.com>
2019-10-03 14:42:19 -0700

    Use QueryRendererWithUser in dashboard

--- web/src/js/components/Dashboard/DashboardView.js
+++ web/src/js/components/Dashboard/DashboardView.js
@@ -1,31 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import graphql from 'babel-plugin-relay/macro'
-import { QueryRenderer } from 'react-relay'
 import { get } from 'lodash/object'
-import environment from 'js/relay-env'
 
+import QueryRendererWithUser from 'js/components/General/QueryRendererWithUser'
 import ErrorMessage from 'js/components/General/ErrorMessage'
 import DashboardContainer from 'js/components/Dashboard/DashboardContainer'
-import { createNewUser } from 'js/authentication/helpers'
-import { goTo, loginURL } from 'js/navigation/navigation'
-import { ERROR_USER_DOES_NOT_EXIST } from 'js/constants'
 import withUser from 'js/components/General/withUser'
-import logger from 'js/utils/logger'
-import { makePromiseCancelable } from 'js/utils/utils'
 
 class DashboardView extends React.Component {
-  constructor(props) {
-    super(props)
-    this.createNewUserPromise = null
-  }
-
-  componentWillUnmount() {
-    if (this.createNewUserPromise && this.createNewUserPromise.cancel) {
-      this.createNewUserPromise.cancel()
-    }
-  }
-
   render() {
     const { authUser } = this.props
     return (
@@ -35,8 +18,7 @@ class DashboardView extends React.Component {
           width: '100%',
         }}
       >
-        <QueryRenderer
-          environment={environment}
+        <QueryRendererWithUser
           query={graphql`
             query DashboardViewQuery($userId: String!) {
               app {
@@ -52,34 +34,8 @@ class DashboardView extends React.Component {
           }}
           render={({ error, props, retry }) => {
             if (error && get(error, 'source.errors')) {
-              // If any of the errors is because the user does not exist
-              // on the server side, create the user and re-query if
-              // possible. If it's not possible to create a new user,
-              // redirect to the authentication page.
-              const userDoesNotExistError = error.source.errors.some(
-                err => err.code === ERROR_USER_DOES_NOT_EXIST
-              )
-
-              if (userDoesNotExistError) {
-                // Cancel new user creation on unmount.
-                this.createNewUserPromise = makePromiseCancelable(
-                  createNewUser()
-                )
-                this.createNewUserPromise.promise
-                  .then(user => {
-                    retry()
-                  })
-                  .catch(e => {
-                    if (e && e.isCanceled) {
-                      return
-                    }
-                    logger.error(e)
-                    goTo(loginURL)
-                  })
-              } else {
-                const errMsg = 'We had a problem loading your dashboard :('
-                return <ErrorMessage message={errMsg} />
-              }
+              const errMsg = 'We had a problem loading your dashboard :('
+              return <ErrorMessage message={errMsg} />
             }
             if (!props) {
               props = {}
