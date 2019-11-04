8cc541046028bce852d123da93994434c0af005b
Kevin Jennison <kevin.jennison1@gmail.com>
2019-11-04 13:46:59 -0800

    Try to import the Redis service endpoint in the GraphQL service

--- graphql/utils/redis.js
+++ graphql/utils/redis.js
@@ -1,32 +1,15 @@
-const aws = require('aws-sdk')
-// import { Lambda } from 'aws-sdk'
+// FIXME: debugging only.
+/* eslint-disable */
+// import fetch from 'node-fetch'
 
 const callRedis = async data => {
-  const lambda = new aws.Lambda({
-    region: 'us-west-2', // TODO: env var
-  })
-  const event = {
-    type: 'foo',
-    data,
-  }
   try {
-    lambda.invoke(
-      {
-        FunctionName: 'tab-redis-dev-tabRedis', // TODO: env var
-        Payload: JSON.stringify(event, null, 2),
-      },
-      (error, response) => {
-        if (error) {
-          throw new Error(error)
-        }
-        if (response) {
-          return response.Payload
-        }
-        return null
-      }
+    console.log(
+      '===== Calling Redis service =====',
+      process.env.redisServiceEndpoint
     )
   } catch (e) {
-    console.log('Error invoking', e)
+    console.log('Error calling Redis service', e)
   }
 }
 
