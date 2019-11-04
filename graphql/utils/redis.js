c4affba6c1794ac80bf5b88bb84001c96d94470c
Kevin Jennison <kevin.jennison1@gmail.com>
2019-11-04 14:53:23 -0800

    Add test request to Redis service

--- graphql/utils/redis.js
+++ graphql/utils/redis.js
@@ -1,16 +1,22 @@
 // FIXME: debugging only.
 /* eslint-disable */
-// import fetch from 'node-fetch'
+import fetch from 'node-fetch'
 
 const callRedis = async data => {
+  let responseData
   try {
     console.log(
       '===== Calling Redis service =====',
       process.env.redisServiceEndpoint
     )
+    const response = await fetch(process.env.redisServiceEndpoint, {
+      method: 'POST',
+    })
+    responseData = response.json()
   } catch (e) {
     console.log('Error calling Redis service', e)
   }
+  console.log('===== Redis service response data =====', responseData)
 }
 
 export default callRedis
