063f58bfa6a56e0e548612fbecb281ed293c2ca4
Kevin Jennison <kevin.jennison1@gmail.com>
2019-11-05 15:14:40 -0800

    Add IAM authorization to fetch

--- graphql/utils/redis.js
+++ graphql/utils/redis.js
@@ -1,7 +1,8 @@
+/* eslint no-console:0 */
 // TODO: remove eslint-disable
-/* eslint-disable */
 
 import fetch from 'node-fetch'
+import aws4 from 'aws4'
 
 const redisAccessEndpoint = `${process.env.REDIS_SERVICE_ENDPOINT}/redis/`
 
@@ -11,9 +12,26 @@ const redisAccessEndpoint = `${process.env.REDIS_SERVICE_ENDPOINT}/redis/`
 const callRedis = async data => {
   let responseData
   try {
+    // Add AWS IAM authorization.
+    // https://docs.aws.amazon.com/general/latest/gr/sigv4_signing.html
+    // https://docs.aws.amazon.com/general/latest/gr/signature-v4-examples.html#signature-v4-examples-javascript
+    // https://github.com/mhart/aws4
+    const endpointURL = new URL(redisAccessEndpoint)
+    const body = JSON.stringify(data)
+    const authorizationInfo = aws4.sign({
+      host: endpointURL.host,
+      path: endpointURL.pathname,
+      method: 'POST',
+      body,
+    })
+
     console.log('===== Calling Redis service =====', redisAccessEndpoint)
     const response = await fetch(redisAccessEndpoint, {
       method: 'POST',
+      headers: {
+        ...authorizationInfo.headers,
+      },
+      body,
     })
     responseData = await response.json()
   } catch (e) {
