81f87e98371f491e9a6bdfc9f5aa9965714fbcde
Kevin Jennison <kevin.jennison1@gmail.com>
2019-11-04 16:02:50 -0800

    Change env var and fix async response.json()

--- graphql/utils/redis.js
+++ graphql/utils/redis.js
@@ -2,17 +2,16 @@
 /* eslint-disable */
 import fetch from 'node-fetch'
 
+const redisAccessEndpoint = `${process.env.REDIS_SERVICE_ENDPOINT}/redis/`
+
 const callRedis = async data => {
   let responseData
   try {
-    console.log(
-      '===== Calling Redis service =====',
-      process.env.redisServiceEndpoint
-    )
-    const response = await fetch(process.env.redisServiceEndpoint, {
+    console.log('===== Calling Redis service =====', redisAccessEndpoint)
+    const response = await fetch(redisAccessEndpoint, {
       method: 'POST',
     })
-    responseData = response.json()
+    responseData = await response.json()
   } catch (e) {
     console.log('Error calling Redis service', e)
   }
