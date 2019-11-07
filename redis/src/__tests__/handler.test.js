8c3bf66b64e545e843f6e2b680f94c8a09209f84
Kevin Jennison <kevin.jennison1@gmail.com>
2019-11-06 16:51:17 -0800

    Add tests

--- redis/src/__tests__/handler.test.js
+++ redis/src/__tests__/handler.test.js
@@ -1,20 +1,45 @@
 /* eslint-env jest */
 
+import redis from 'redis'
 import { handler } from '../handler'
 
-const mockEventObj = { body: JSON.stringify({ some: 'data' }) }
+jest.mock('bluebird')
+
+// Using "Async" methods created by Bluebird.
+// Manual mocking because of problems with automock:
+// https://github.com/facebook/jest/issues/8983
+const mockRedisClient = {
+  getAsync: jest.fn(),
+  setAsync: jest.fn(),
+  incrAsync: jest.fn(),
+  quitAsync: jest.fn(),
+}
+
+jest.mock('redis', () => ({
+  createClient: jest.fn(() => mockRedisClient),
+}))
+
+const getMockEventObj = () => ({
+  body: JSON.stringify({ operation: 'GET', key: 'foo' }),
+})
+
+beforeEach(() => {
+  process.env.REDIS_HOST = 'some-fake.host'
+  process.env.REDIS_PORT = '6379'
+})
 
 afterEach(() => {
   jest.clearAllMocks()
 })
 
-// TODO
 describe('Redis Lambda handler', () => {
-  it('is a placeholder', async () => {
+  it('connects the client with the expected host and port', async () => {
     expect.assertions(1)
-    expect(await handler(mockEventObj)).toEqual({
-      statusCode: 200,
-      body: JSON.stringify({ just: 'a-test' }),
+    const eventData = getMockEventObj()
+    await handler(eventData)
+    expect(redis.createClient).toHaveBeenCalledWith({
+      host: 'some-fake.host',
+      port: '6379',
     })
   })
 })
