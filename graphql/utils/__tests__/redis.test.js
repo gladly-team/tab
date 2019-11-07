c2204ca7afdad317f693248eb03ba38584a2bbca
Kevin Jennison <kevin.jennison1@gmail.com>
2019-11-07 12:01:42 -0800

    WIP - graphql interface with Redis service

--- graphql/utils/__tests__/redis.test.js
+++ graphql/utils/__tests__/redis.test.js
@@ -1,15 +1,52 @@
 /* eslint-env jest */
 
-// import callRedis from '../redis'
+import fetch from 'node-fetch'
+import aws4 from 'aws4'
+import logger from '../logger'
+import { getMockFetchResponse } from '../../database/test-utils'
+
+jest.mock('node-fetch')
+jest.mock('aws4')
+jest.mock('../logger')
+
+const getMockInputData = () => ({
+  operation: 'GET',
+  key: 'foo',
+})
+
+beforeAll(() => {
+  process.env.REDIS_SERVICE_ENDPOINT = 'https://some.endpoint.example.com/dev'
+  aws4.sign.mockReturnValue({
+    host: 'some.endpoint.example.com',
+    path: '/dev/redis',
+    headers: {
+      Host: 'some.endpoint.example.com',
+      'X-Amz-Date': '20191224T162032Z',
+      Authorization:
+        'AWS4-HMAC-SHA256 Credential=ABCDEF/20191224/us-west-2/execute-api/aws4_request, etc.',
+    },
+  })
+})
+
+beforeEach(() => {
+  fetch.mockResolvedValue(getMockFetchResponse())
+})
 
 afterEach(() => {
   jest.clearAllMocks()
 })
 
-// TODO
 describe('Redis outbound', () => {
-  it('is a placeholder', async () => {
+  it('calls aws4.sign with the expected params', async () => {
     expect.assertions(1)
-    expect(true).toBe(true)
+    const callRedis = require('../redis').default
+    const mockInputData = getMockInputData()
+    await callRedis(mockInputData)
+    expect(aws4.sign).toHaveBeenCalledWith({
+      host: 'some.endpoint.example.com',
+      path: '/dev/redis',
+      method: 'POST',
+      body: JSON.stringify(mockInputData),
+    })
   })
 })
--- graphql/utils/redis.js
+++ graphql/utils/redis.js
@@ -1,17 +1,19 @@
-/* eslint no-console:0 */
-// TODO: remove eslint-disable
-
 import { URL } from 'url'
 import fetch from 'node-fetch'
 import aws4 from 'aws4'
+import logger from './logger'
 
 const redisAccessEndpoint = `${process.env.REDIS_SERVICE_ENDPOINT}/redis`
 
-// TODO:
-//   - call to get/set values
-//   - gracefully handle Redis downtime or missing cache value
+/**
+ * Call the Redis service.
+ * @param {Object} data - Data on the operation to perform on Redis.
+ * @param {String} data.key - The Redis item key
+ * @param {String} data.operation - The Redis operation. One of: GET, INCR
+ * @return {Object|String|Number} The Redis return value.
+ */
 const callRedis = async data => {
-  let responseData
+  let responseData = null
   try {
     // Add AWS IAM authorization.
     // https://docs.aws.amazon.com/general/latest/gr/sigv4_signing.html
@@ -26,7 +28,6 @@ const callRedis = async data => {
       body,
     })
 
-    console.log('===== Calling Redis service =====', redisAccessEndpoint)
     const response = await fetch(redisAccessEndpoint, {
       method: 'POST',
       headers: {
@@ -35,10 +36,22 @@ const callRedis = async data => {
       body,
     })
     responseData = await response.json()
+
+    // Log any error message returned from the Redis service.
+    if (!response.ok) {
+      throw new Error(
+        `Bad request data sent to the Redis service: ${responseData.message}`
+      )
+    }
   } catch (e) {
-    console.log('Error calling Redis service', e)
+    logger.error(e)
+    throw e
   }
+
+  // eslint-disable-next-line no-console
   console.log('===== Redis service response data =====', responseData)
+
+  return responseData
 }
 
 export default callRedis
