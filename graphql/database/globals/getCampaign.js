2fee78c46c8e454bfc51780043f28fff65e2931b
Kevin Jennison <kevin.jennison1@gmail.com>
2019-11-08 15:04:31 -0800

    Update documentation

--- graphql/database/globals/getCampaign.js
+++ graphql/database/globals/getCampaign.js
@@ -6,10 +6,10 @@ import logger from '../../utils/logger'
  * Return data about any currently-live campaign.
  * @return {Promise<Object>} campaign - a Promise that resolves into a
  *   Campaign object.
+ * @return {String|undefined} campaign.campaignId - the unique ID of the campaign.
+ *   This may be undefined if the campaign is not live.
  * @return {Boolean} campaign.isLive - whether we should show the campaign
  *   on the new tab page.
- * @return {String|undefined} campaign.id - the unique ID of the campaign.
- *   This may be undefined if the campaign is not live.
  */
 const getCampaign = async () => {
   const campaign = getCurrentCampaign()
--- graphql/database/globals/getCampaignData.js
+++ graphql/database/globals/getCampaignData.js
@@ -12,6 +12,17 @@ const campaigns = {
 // Hardcode the currently-active campaign here.
 const CURRENT_CAMPAIGN = campaigns.testNov2019
 
+/**
+ * Return the hardcoded campaign info for the current campaign.
+ * @return {Promise<Object>} campaign
+ * @return {String|undefined} campaign.campaignId - the unique ID of the
+ *   campaign.
+ * @return {Boolean} campaign.isLive - whether we should show the campaign
+ *   on the new tab page.
+ * @return {Function} campaign.getNewUsersRedisKey - a function that returns
+ *   a string of the Redis key value. The Redis item stores the number of new
+ *   users who joined during this campaign.
+ */
 // eslint-disable-next-line import/prefer-default-export
 export const getCurrentCampaign = () => {
   const isLive = process.env.IS_GLOBAL_CAMPAIGN_LIVE === 'true' || false
