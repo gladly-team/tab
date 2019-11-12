82ecaf6f1b0f56e60d981254ad39066051041b2b
Kevin Jennison <kevin.jennison1@gmail.com>
2019-11-12 15:18:43 -0800

    Break out campaign base theming into its own component

--- web/src/js/components/Campaign/CampaignBase.js
+++ web/src/js/components/Campaign/CampaignBase.js
@@ -13,114 +13,92 @@ import theme from 'js/theme/defaultV1'
 
 const defaultTheme = createMuiTheme(theme)
 
-class CampaignBase extends React.Component {
-  render() {
-    const {
-      app,
-      user,
-      theme,
-      onDismiss,
-      campaignStartTimeISO,
-      campaignEndTimeISO,
-    } = this.props
+const CampaignBaseContent = props => {
+  const {
+    app,
+    user,
+    theme,
+    onDismiss,
+    campaignStartTimeISO,
+    campaignEndTimeISO,
+  } = props
 
-    // Hardcode campaign component here when running one.
-    const currentCampaign = (
-      <TreePlantingCampaign
-        app={app}
-        user={user}
-        campaign={{
-          time: {
-            start: moment(campaignStartTimeISO),
-            end: moment(campaignEndTimeISO),
-          },
-          treesPlantedGoal: 20000,
-        }}
-        showError={this.props.showError}
-      />
-    )
+  // Hardcode campaign component here when running one.
+  const currentCampaign = (
+    <TreePlantingCampaign
+      app={app}
+      user={user}
+      campaign={{
+        time: {
+          start: moment(campaignStartTimeISO),
+          end: moment(campaignEndTimeISO),
+        },
+        treesPlantedGoal: 20000,
+      }}
+      showError={props.showError}
+    />
+  )
 
-    return (
-      <MuiThemeProvider
-        theme={{
-          ...defaultTheme,
-          palette: {
-            ...defaultTheme.palette,
-            primary: {
-              ...defaultTheme.palette.primary,
-              main: '#FF0000',
-              light: '#FF0000',
-            },
-            secondary: {
-              ...defaultTheme.palette.secondary,
-              main: '#FF0000',
-              light: '#FF0000',
-            },
-          },
-        }}
-      >
-        <div
+  return (
+    <div
+      style={{
+        zIndex: 2,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        textAlign: 'center',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        boxSizing: 'border-box',
+        pointerEvents: 'none',
+      }}
+    >
+      <FadeInDashboardAnimation>
+        <Paper
+          elevation={1}
           style={{
-            zIndex: 2,
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            textAlign: 'center',
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            width: '100%',
-            height: '100%',
-            boxSizing: 'border-box',
-            pointerEvents: 'none',
+            position: 'relative',
+            pointerEvents: 'all',
+            minWidth: 400,
+            margin: 0,
+            marginBottom: 100,
+            padding: 0,
+            background: '#FFF',
+            border: 'none',
           }}
         >
-          <FadeInDashboardAnimation>
-            <Paper
-              elevation={1}
-              style={{
-                position: 'relative',
-                pointerEvents: 'all',
-                minWidth: 400,
-                margin: 0,
-                marginBottom: 100,
-                padding: 0,
-                background: '#FFF',
-                border: 'none',
-              }}
-            >
-              <div
-                style={{
-                  width: '100%',
-                  height: 3,
-                  borderTopLeftRadius: 2,
-                  borderTopRightRadius: 2,
-                  backgroundColor: theme.palette.secondary.main,
-                }}
-              />
-              <IconButton
-                onClick={() => {
-                  setCampaignDismissTime()
-                  onDismiss()
-                }}
-                style={{
-                  position: 'absolute',
-                  top: 5,
-                  right: 2,
-                }}
-              >
-                <CloseIcon />
-              </IconButton>
-              {currentCampaign}
-            </Paper>
-          </FadeInDashboardAnimation>
-        </div>
-      </MuiThemeProvider>
-    )
-  }
+          <div
+            style={{
+              width: '100%',
+              height: 3,
+              borderTopLeftRadius: 2,
+              borderTopRightRadius: 2,
+              backgroundColor: theme.palette.secondary.main,
+            }}
+          />
+          <IconButton
+            onClick={() => {
+              setCampaignDismissTime()
+              onDismiss()
+            }}
+            style={{
+              position: 'absolute',
+              top: 5,
+              right: 2,
+            }}
+          >
+            <CloseIcon />
+          </IconButton>
+          {currentCampaign}
+        </Paper>
+      </FadeInDashboardAnimation>
+    </div>
+  )
 }
-
-CampaignBase.propTypes = {
+CampaignBaseContent.propTypes = {
   app: PropTypes.object.isRequired,
   user: PropTypes.object.isRequired,
   showError: PropTypes.func.isRequired,
@@ -129,9 +107,44 @@ CampaignBase.propTypes = {
   campaignStartTimeISO: PropTypes.string.isRequired,
   campaignEndTimeISO: PropTypes.string.isRequired,
 }
+const CampaignBaseContentWithTheme = withTheme()(CampaignBaseContent)
 
+// Separate component for theming logic.
+const CampaignBase = props => (
+  // Modify this for any campaign-specific theming.
+  <MuiThemeProvider
+    theme={{
+      ...defaultTheme,
+      palette: {
+        ...defaultTheme.palette,
+        primary: {
+          ...defaultTheme.palette.primary,
+          // TODO
+          main: '#FF0000',
+          light: '#FF0000',
+        },
+        secondary: {
+          ...defaultTheme.palette.secondary,
+          // TODO
+          main: '#FF0000',
+          light: '#FF0000',
+        },
+      },
+    }}
+  >
+    <CampaignBaseContentWithTheme {...props} />
+  </MuiThemeProvider>
+)
+CampaignBase.propTypes = {
+  app: PropTypes.object.isRequired,
+  user: PropTypes.object.isRequired,
+  showError: PropTypes.func.isRequired,
+  onDismiss: PropTypes.func.isRequired,
+  campaignStartTimeISO: PropTypes.string.isRequired,
+  campaignEndTimeISO: PropTypes.string.isRequired,
+}
 CampaignBase.defaultProps = {
   onDismiss: () => {},
 }
 
-export default withTheme()(CampaignBase)
+export default CampaignBase
