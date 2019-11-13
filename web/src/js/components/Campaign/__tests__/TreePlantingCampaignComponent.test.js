5779c5b3c5fe5b9b50a7f579c6bb8b828cfbeb3f
Kevin Jennison <kevin.jennison1@gmail.com>
2019-11-13 11:03:12 -0800

    Add tests

--- web/src/js/components/Campaign/__tests__/TreePlantingCampaignComponent.test.js
+++ web/src/js/components/Campaign/__tests__/TreePlantingCampaignComponent.test.js
@@ -7,6 +7,9 @@ import { shallow } from 'enzyme'
 import Typography from '@material-ui/core/Typography'
 import TreeIcon from 'mdi-material-ui/PineTree'
 // import LinearProgress from '@material-ui/core/LinearProgress'
+import InviteFriend from 'js/components/Settings/Profile/InviteFriendContainer'
+
+jest.mock('js/components/General/Link')
 
 const mockNow = '2017-05-19T13:59:58.000Z'
 
@@ -126,6 +129,70 @@ describe('Tree planting campaign component', () => {
     treeIcons.forEach(icon => {
       expect(icon.prop('style').color).toEqual('#f50057') // green
     })
+  })
+
+  it('displays the "invite friend" component when the campaign is in progress', () => {
+    const TreePlantingCampaign = require('js/components/Campaign/TreePlantingCampaignComponent')
+      .default
+    const mockProps = getMockProps()
+    const wrapper = shallow(<TreePlantingCampaign {...mockProps} />).dive()
+    const comp = wrapper.find(InviteFriend)
+    expect(comp.exists()).toBe(true)
+    expect(comp.prop('label')).toBe('Share this link with a friend')
+    expect(comp.prop('helperText')).toBe(
+      "and you'll plant a tree when they join!"
+    )
+  })
+
+  it('does not display the "invite friend" component when the campaign is complete', () => {
+    const TreePlantingCampaign = require('js/components/Campaign/TreePlantingCampaignComponent')
+      .default
+
+    // Mock that now is after the campaign end time.
+    MockDate.set(moment('2017-05-23T12:14:00.000Z'))
+
+    const mockProps = getMockProps()
+    const wrapper = shallow(<TreePlantingCampaign {...mockProps} />).dive()
+    const comp = wrapper.find(InviteFriend)
+    expect(comp.exists()).toBe(false)
+  })
+
+  it('displays the expected text when the campaign is in progress', () => {
+    const TreePlantingCampaign = require('js/components/Campaign/TreePlantingCampaignComponent')
+      .default
+    const mockProps = getMockProps()
+    const wrapper = shallow(<TreePlantingCampaign {...mockProps} />).dive()
+    expect(
+      wrapper
+        .find(Typography)
+        .filterWhere(e => e.prop('variant') === 'body2')
+        .last()
+        .render()
+        .text()
+    ).toEqual(
+      "Now until January 10, we're planting a tree for every person who joins Tab for a Cause!"
+    )
+  })
+
+  it('displays the expected text when the campaign is complete', () => {
+    const TreePlantingCampaign = require('js/components/Campaign/TreePlantingCampaignComponent')
+      .default
+
+    // Mock that now is after the campaign end time.
+    MockDate.set(moment('2017-05-23T12:14:00.000Z'))
+
+    const mockProps = getMockProps()
+    const wrapper = shallow(<TreePlantingCampaign {...mockProps} />).dive()
+    expect(
+      wrapper
+        .find(Typography)
+        .filterWhere(e => e.prop('variant') === 'body2')
+        .last()
+        .render()
+        .text()
+    ).toEqual(
+      'Together, we planted 192 trees this holiday season! Great job, and thank you for spreading the word about Tab for a Cause!'
+    )
   })
 
   // TODO: progress bar for trees planted
