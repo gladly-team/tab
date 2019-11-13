440600722c7ef5fcadf4bb7aa1352b10ecb10e39
Kevin Jennison <kevin.jennison1@gmail.com>
2019-11-13 11:46:38 -0800

    WIP - add campaign start/end time to server-side

--- graphql/database/globals/__tests__/getCampaignData.test.js
+++ graphql/database/globals/__tests__/getCampaignData.test.js
@@ -1,17 +1,31 @@
 /* eslint-env jest */
+import moment from 'moment'
+import MockDate from 'mockdate'
 import { getCurrentCampaign } from '../getCampaignData'
 
+const mockNow = '2017-05-19T13:59:58.000Z'
+
 beforeEach(() => {
+  MockDate.set(moment(mockNow))
   process.env.IS_GLOBAL_CAMPAIGN_LIVE = false
+})
+
+afterEach(() => {
+  MockDate.reset()
 })
 
 describe('getCurrentCampaign', () => {
   it('returns an object with the expected properties', () => {
     expect.assertions(1)
-    expect(getCurrentCampaign()).toMatchObject({
+    expect(getCurrentCampaign()).toEqual({
       campaignId: expect.any(String),
       isLive: expect.any(Boolean),
       getNewUsersRedisKey: expect.any(Function),
+      isActive: expect.any(Function),
+      time: {
+        start: expect.any(String),
+        end: expect.any(String),
+      },
     })
   })
 
@@ -38,4 +52,13 @@ describe('getCurrentCampaign', () => {
       `campaign:${campaign.campaignId}:newUsers`
     )
   })
+
+  // TODO
+  // it("the isActive method returns true when the current time is between the campaign's start and end times", () => {
+  //   expect.assertions(1)
+  //   const campaign = getCurrentCampaign()
+  //   expect(getCurrentCampaign().getNewUsersRedisKey()).toEqual(
+  //     `campaign:${campaign.campaignId}:newUsers`
+  //   )
+  // })
 })
--- graphql/database/globals/getCampaignData.js
+++ graphql/database/globals/getCampaignData.js
@@ -1,12 +1,41 @@
+import moment from 'moment'
+
 const createCampaign = data => ({
   campaignId: data.campaignId,
+
+  /**
+   * Return the Redis key used to store the new user count during this
+   * campaign.
+   * @return {String} the Redis key
+   */
   getNewUsersRedisKey() {
     return `campaign:${this.campaignId}:newUsers`
   },
+
+  /**
+   * Return whether the current time is between the campaign's start and
+   * end times.
+   * @return {Boolean}
+   */
+  isActive() {
+    return moment().isAfter(this.time.start) && moment().isBefore(this.time.end)
+  },
+
+  // Include other properties.
+  ...data,
 })
 
+// Hardcode campaign data here.
+// Note: currently, we need to hardcode campaign start and end times
+// both here and on the client side.
 const campaigns = {
-  testNov2019: createCampaign({ campaignId: 'testNov2019' }),
+  testNov2019: createCampaign({
+    campaignId: 'testNov2019',
+    time: {
+      start: '2019-11-12T10:00:00.000Z',
+      end: '2020-01-10T20:00:00.000Z',
+    },
+  }),
 }
 
 // Hardcode the currently-active campaign here.
