0caaab92b03c44bf65022b24d76be819e28ab26d
Kevin Jennison <kevin.jennison1@gmail.com>
2019-12-13 09:44:53 -1000

    Remove the random queries

--- web/src/js/components/Search/SearchRandomQueryView.js
+++ web/src/js/components/Search/SearchRandomQueryView.js
@@ -2,26 +2,11 @@ import React from 'react'
 import { goTo, searchBaseURL } from 'js/navigation/navigation'
 
 // The browser extension opens this page when the user
-// clicks on the browser extension icon. It selects a
-// random pleasant search query and redirects to the
-// search results.
+// clicks on the browser extension icon. We should not
+// prepopulate any search queries.
 class SearchRandomQueryView extends React.Component {
   componentDidMount() {
-    const queries = [
-      'volunteer opportunities near me',
-      'how to have a bake sale for charity',
-      'ways to raise money for non-profits',
-      'random acts of kindness',
-      'ways to make someone smile',
-    ]
-    const query = queries[Math.floor(Math.random() * queries.length)]
-    goTo(
-      searchBaseURL,
-      {
-        q: query,
-      },
-      { keepURLParams: true }
-    )
+    goTo(searchBaseURL)
   }
 
   render() {
