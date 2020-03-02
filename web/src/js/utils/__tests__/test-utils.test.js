cbd7b9a21c84c3105b0d3d3cecba9900d2af26d1
Kevin Jennison <kevin.jennison1@gmail.com>
2020-03-02 14:01:59 -0800

    Remove unneeded test-utils

--- web/src/js/utils/__tests__/test-utils.test.js
+++ web/src/js/utils/__tests__/test-utils.test.js
@@ -5,69 +5,6 @@ afterEach(() => {
 })
 
 describe('web test-utils', () => {
-  test('mockGoogleTagSlotRenderEndedData returns expected defaults', () => {
-    const mockGoogleTagSlotRenderEndedData = require('js/utils/test-utils')
-      .mockGoogleTagSlotRenderEndedData
-    const slotRenderEndedData = mockGoogleTagSlotRenderEndedData()
-    expect(slotRenderEndedData).toMatchObject({
-      advertiserId: 1234,
-      campaignId: 99887766,
-      creativeId: 111222333444555,
-      isEmpty: false,
-      lineItemId: 123456,
-      serviceName: 'something',
-      size: '728x90',
-      sourceAgnosticCreativeId: null,
-      sourceAgnosticLineItemId: null,
-    })
-    expect(slotRenderEndedData).toHaveProperty('slot.getSlotElementId')
-    expect(slotRenderEndedData.slot.getSlotElementId()).toBe('abc-123')
-  })
-
-  test('mockGoogleTagSlotRenderEndedData allows overriding slot ID and properties', () => {
-    const mockGoogleTagSlotRenderEndedData = require('js/utils/test-utils')
-      .mockGoogleTagSlotRenderEndedData
-    const slotRenderEndedData = mockGoogleTagSlotRenderEndedData(
-      'foobar',
-      '/some/ad-unit/',
-      {
-        advertiserId: null,
-        campaignId: 1357,
-        creativeId: 2468,
-        lineItemId: 1001,
-      }
-    )
-    expect(slotRenderEndedData).toMatchObject({
-      advertiserId: null,
-      campaignId: 1357,
-      creativeId: 2468,
-      isEmpty: false,
-      lineItemId: 1001,
-      serviceName: 'something',
-      size: '728x90',
-      sourceAgnosticCreativeId: null,
-      sourceAgnosticLineItemId: null,
-    })
-    expect(slotRenderEndedData).toHaveProperty('slot.getSlotElementId')
-    expect(slotRenderEndedData.slot.getSlotElementId()).toBe('foobar')
-  })
-
-  test('mockAmazonBidResponse returns expected mock', () => {
-    const mockAmazonBidResponse = require('js/utils/test-utils')
-      .mockAmazonBidResponse
-    const mockAmazonBid = mockAmazonBidResponse({
-      slotID: 'abx-xyz',
-    })
-    expect(mockAmazonBid).toEqual({
-      amznbid: '1',
-      amzniid: 'some-id',
-      amznp: '1',
-      amznsz: '0x0',
-      size: '0x0',
-      slotID: 'abx-xyz',
-    })
-  })
-
   test('flushAllPromises works as expected', async () => {
     expect.assertions(1)
 
@@ -278,26 +215,6 @@ describe('web test-utils', () => {
     expect(json).toEqual({
       my: 'data!',
     })
-  })
-})
-
-describe('mockIndexExchangeBidResponse', () => {
-  it('returns expected properties', () => {
-    const { mockIndexExchangeBidResponse } = require('js/utils/test-utils')
-    expect(Object.keys(mockIndexExchangeBidResponse()).sort()).toEqual([
-      'identity',
-      'page',
-      'slot',
-    ])
-  })
-
-  it('returns expected slots', () => {
-    const { mockIndexExchangeBidResponse } = require('js/utils/test-utils')
-    expect(Object.keys(mockIndexExchangeBidResponse().slot).sort()).toEqual([
-      'd-1-728x90-atf-bottom-leaderboard',
-      'd-2-300x250-atf-middle-right_rectangle',
-      'd-3-300x250-atf-bottom-right_rectangle',
-    ])
   })
 })
 
