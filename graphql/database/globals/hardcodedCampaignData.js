55808ee5fb0d6002981f1a511c0eab1ffd02c659
Kevin Jennison <kevin.jennison1@gmail.com>
2020-03-27 17:12:39 -0700

    Generalize variable name

--- graphql/database/globals/hardcodedCampaignData.js
+++ graphql/database/globals/hardcodedCampaignData.js
@@ -1,13 +1,13 @@
 import createCampaignConfiguration from './createCampaignConfiguration'
 
-const covid19CampaignTitle = '## COVID-19 Solidarity.'
-const covid19CampaignDescription = `
+const campaignTitle = '## COVID-19 Solidarity.'
+const campaignDescription = `
 #### The spread of COVID-19 has been swift and destructive. We need a global response to support the health systems working to keep us all safe. As a free, simple, and at-home way to raise money for important causes, we will be running a special campaign for the foreseeable future to raise funds for the response efforts.
 #### Donate your hearts to the COVID-19 solidarity fund and support the [World Health Organization](https://www.who.int/) and their partners in a massive effort to help countries prevent, detect, and manage the novel coronavirus—particularly where the needs are the greatest.
 #### Please join us in supporting the [COVID-19 Solidarity Response Fund](https://www.who.int/emergencies/diseases/novel-coronavirus-2019/donate).
 `
-const covid19CampaignEndTitle = '## Thank You for Supporting the WHO'
-const covid19CampaignEndDescription = `
+const campaignEndTitle = '## Thank You for Supporting the WHO'
+const campaignEndDescription = `
 #### With your help, the World Health Organization will continue to provide COVID-19 relief, prevention, and detection.
 `
 
@@ -16,14 +16,14 @@ const CURRENT_CAMPAIGN = createCampaignConfiguration({
   campaignId: 'covid19March2020',
   charityId: '6667eb86-ea37-4d3d-9259-910bea0b5e38',
   content: {
-    titleMarkdown: covid19CampaignTitle,
-    descriptionMarkdown: covid19CampaignDescription,
+    titleMarkdown: campaignTitle,
+    descriptionMarkdown: campaignDescription,
   },
   countNewUsers: false,
   countTabsOpened: false,
   endContent: {
-    titleMarkdown: covid19CampaignEndTitle,
-    descriptionMarkdown: covid19CampaignEndDescription,
+    titleMarkdown: campaignEndTitle,
+    descriptionMarkdown: campaignEndDescription,
   },
   goal: {
     targetNumber: 10e6,
