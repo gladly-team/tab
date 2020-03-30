acb9689c0e65aef4a834202fa2851c413d6dda0f
Kevin Jennison <kevin.jennison1@gmail.com>
2020-03-30 16:13:05 -0700

    Revert "Example: set up NYC Food Bank campaign"
    This reverts commit 1e1480e24e2a6bf142ac8fe246de4c0599bc06e6.

--- graphql/database/globals/getCurrentCampaignConfig.js
+++ graphql/database/globals/getCurrentCampaignConfig.js
@@ -1,47 +1,44 @@
 import createCampaignConfiguration from './createCampaignConfiguration'
 
-const campaignTitle = '## COVID-19 Food Bank Support'
+const campaignTitle = '## COVID-19 Solidarity.'
 const campaignDescription = `
-#### Thanks to you, we raised thousands of dollars for the [World Health Organization](https://www.who.int/) over the last few days. In addition to health systems, COVID-19 has strained the ability of food banks to take care of people in need. The next phase of our support will help the [Food Bank for New York City](https://www.foodbanknyc.org/covid-19/) keep families fed during this crisis.
-#### During this campaign, tabs you open are providing meals for our compatriots in NYC. Together, we can feed thousands of people in need, so please open a few tabs and encourage your friends to do the same!
+#### The spread of COVID-19 has been swift and destructive. We need a global response to support the health systems working to keep us all safe. As a free, simple, and at-home way to raise money for important causes, we will be running a special campaign for the foreseeable future to raise funds for the response efforts.
+#### Donate your hearts to the COVID-19 solidarity fund and support the [World Health Organization](https://www.who.int/) and their partners in a massive effort to help countries prevent, detect, and manage the novel coronavirus—particularly where the needs are the greatest.
+#### Please join us in supporting the [COVID-19 Solidarity Response Fund](https://www.who.int/emergencies/diseases/novel-coronavirus-2019/donate).
 `
-const campaignEndTitle = '## Thank You for Giving Food'
+const campaignEndTitle = '## Thank You for Supporting the WHO'
 const campaignEndDescription = `
-#### With your help, we gave thousands of meals to people in New York City who have been hurt by the COVID-19 crisis.
+#### With your help, the World Health Organization will continue to provide COVID-19 relief, prevention, and detection.
 `
 
 // Hardcode campaign data here.
 const CURRENT_CAMPAIGN = createCampaignConfiguration({
-  campaignId: 'NYCFoodBank2020',
-  charityId: null,
+  campaignId: 'covid19March2020',
+  charityId: '6667eb86-ea37-4d3d-9259-910bea0b5e38',
   content: {
     titleMarkdown: campaignTitle,
     descriptionMarkdown: campaignDescription,
   },
-  countMoneyRaised: true,
+  countMoneyRaised: false,
   countNewUsers: false,
-  countTabsOpened: true,
+  countTabsOpened: false,
   endContent: {
     titleMarkdown: campaignEndTitle,
     descriptionMarkdown: campaignEndDescription,
   },
   goal: {
-    impactUnitSingular: 'meal',
-    impactUnitPlural: 'meals',
-    impactVerbPastTense: 'given',
-    numberSource: 'moneyRaised',
-    targetNumber: 1000,
-    transformNumberSourceValue: moneyRaised => {
-      // The moneyRaised value is in $USD, and it costs $0.20 per meal.
-      return Math.floor(moneyRaised * 5)
-    },
+    impactUnitSingular: 'Heart',
+    impactUnitPlural: 'Hearts',
+    impactVerbPastTense: 'donated',
+    numberSource: 'hearts',
+    targetNumber: 10e6,
   },
   showCountdownTimer: false,
-  showHeartsDonationButton: false,
+  showHeartsDonationButton: true,
   showProgressBar: true,
   time: {
-    start: '2020-03-30T18:00:00.000Z',
-    end: '2020-04-14T18:00:00.000Z',
+    start: '2020-03-25T18:00:00.000Z',
+    end: '2020-05-01T18:00:00.000Z',
   },
 })
 
