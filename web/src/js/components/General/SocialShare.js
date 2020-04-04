8dd2d6dc6e2d1792dc38e39d85dce1f3b3252332
Kevin Jennison <kevin.jennison1@gmail.com>
2020-04-04 11:37:11 -0700

    Add react-share and add placeholder component

---
+++ web/src/js/components/General/SocialShare.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const SocialShare = props => {
+  return <div />
+}
+
+SocialShare.propTypes = {}
+SocialShare.defaultProps = {}
+
+export default SocialShare
---
+++ web/src/js/components/General/__tests__/SocialShare.test.js
@@ -0,0 +1,18 @@
+/* eslint-env jest */
+
+import React from 'react'
+import { shallow } from 'enzyme'
+
+const getMockProps = () => ({})
+
+afterEach(() => {
+  jest.clearAllMocks()
+})
+
+describe('SocialShare page component', () => {
+  it('renders without error', () => {
+    const SocialShare = require('js/components/General/SocialShare').default
+    const mockProps = getMockProps()
+    shallow(<SocialShare {...mockProps} />)
+  })
+})
