c6516d67032911bf91262fdd496b72143b1245d3
Kevin Jennison <kevin.jennison1@gmail.com>
2020-04-06 08:25:58 -0700

    Revert to 10k target number

--- graphql/database/globals/getCurrentCampaignConfig.js
+++ graphql/database/globals/getCurrentCampaignConfig.js
@@ -38,8 +38,7 @@ const CURRENT_CAMPAIGN = createCampaignConfiguration({
     numberSource: 'moneyRaised',
     showProgressBarLabel: true,
     showProgressBarEndText: false,
-    // targetNumber: 10000,
-    targetNumber: 30, // TODO: revert to 10k; this is for testing only
+    targetNumber: 10000,
     transformNumberSourceValue: moneyRaised => {
       // The moneyRaised value is in $USD, and it costs $0.20 per meal.
       return Math.floor(moneyRaised * 5)
