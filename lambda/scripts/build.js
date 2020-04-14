40a0c180e069e74498cccecf571f2d692b33a1f7
Kevin Jennison <kevin.jennison1@gmail.com>
2020-04-14 11:27:04 -0700

    WIP - use env var in newtab lambda edge

---
+++ lambda/scripts/build.js
@@ -0,0 +1,17 @@
+const path = require('path')
+const { spawnSync } = require('child_process')
+
+// Load environment variables from .env file.
+// https://github.com/keithmorris/node-dotenv-extended
+require('dotenv-extended').load({
+  path: path.join(__dirname, '../', '.env.local'),
+  defaults: path.join(__dirname, '../', '.env'),
+})
+
+process.env.NODE_ENV = 'production'
+
+spawnSync('babel', [
+  '--out-dir=build',
+  "--ignore='build,coverage,__mocks__,__tests__,node_modules'",
+  './src/',
+])
