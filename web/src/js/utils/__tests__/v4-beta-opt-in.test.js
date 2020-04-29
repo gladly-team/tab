035893ef10560ff8c24fc84e4107a7f1b825c09a
Kevin Jennison <kevin.jennison1@gmail.com>
2020-04-28 17:12:17 -0700

    Add a function to call the API endpoint for a Tab v4 cookie

---
+++ web/src/js/utils/__tests__/v4-beta-opt-in.test.js
@@ -0,0 +1,57 @@
+/* eslint-env jest */
+/* global fetch */
+import { mockFetchResponse } from 'js/utils/test-utils'
+
+beforeEach(() => {
+  global.fetch = jest.fn(() => Promise.resolve(mockFetchResponse()))
+})
+
+afterEach(() => {
+  jest.clearAllMocks()
+})
+
+describe('optIntoV4Beta', () => {
+  it('calls the expected endpoint', async () => {
+    expect.assertions(1)
+    const optIntoV4Beta = require('js/utils/v4-beta-opt-in').default
+    await optIntoV4Beta()
+    expect(fetch.mock.calls[0][0]).toEqual('/v4/api/beta-opt-in')
+  })
+
+  it('calls with the expected data', async () => {
+    expect.assertions(1)
+    const optIntoV4Beta = require('js/utils/v4-beta-opt-in').default
+    await optIntoV4Beta()
+    expect(fetch.mock.calls[0][1]).toEqual({
+      method: 'POST',
+      // eslint-disable-next-line no-undef
+      headers: new Headers({
+        'X-Gladly-Requested-By': 'tab-web-nextjs',
+        'Content-Type': 'application/json',
+      }),
+      credentials: 'include',
+      body: '{"optIn":true}',
+    })
+  })
+
+  it('calls the expected endpoint', async () => {
+    expect.assertions(1)
+    const optIntoV4Beta = require('js/utils/v4-beta-opt-in').default
+    await optIntoV4Beta()
+    expect(fetch.mock.calls[0][0]).toEqual('/v4/api/beta-opt-in')
+  })
+
+  it('throws if the response is not 200', async () => {
+    expect.assertions(1)
+
+    // Mock a bad response.
+    global.fetch.mockResolvedValue({
+      ...mockFetchResponse(),
+      ok: false,
+      statusText: 'The server messed up.',
+    })
+
+    const optIntoV4Beta = require('js/utils/v4-beta-opt-in').default
+    await expect(optIntoV4Beta()).rejects.toThrow('The server messed up.')
+  })
+})
--- web/src/js/utils/v4-beta-opt-in.js
+++ web/src/js/utils/v4-beta-opt-in.js
@@ -1,9 +1,34 @@
+/* global fetch */
+
+import { tabV4BetaOptInURL } from 'js/navigation/navigation'
+
 /**
  * Calls an endpoint to set a cookie that opts the user
  * into Tab V4.
  */
 const optIntoV4Beta = async () => {
-  // TODO
+  let response
+  try {
+    response = await fetch(tabV4BetaOptInURL, {
+      method: 'POST',
+      // eslint-disable-next-line no-undef
+      headers: new Headers({
+        // This custom header provides modest CSRF protection. See:
+        // https://github.com/gladly-team/tab-web#authentication-approach
+        'X-Gladly-Requested-By': 'tab-web-nextjs',
+        'Content-Type': 'application/json',
+      }),
+      credentials: 'include',
+      body: JSON.stringify({ optIn: true }),
+    })
+    if (!response.ok) {
+      throw new Error(response.statusText)
+    }
+    await response.json()
+  } catch (e) {
+    throw e
+  }
+  return response
 }
 
 export default optIntoV4Beta
