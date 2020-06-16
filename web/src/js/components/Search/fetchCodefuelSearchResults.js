efa07fd012d5f69cd5d939f54e67255641df0211
Kevin Jennison <kevin.jennison1@gmail.com>
2020-06-16 15:26:29 -0700

    Add video results

--- web/src/js/components/Search/fetchCodefuelSearchResults.js
+++ web/src/js/components/Search/fetchCodefuelSearchResults.js
@@ -8,30 +8,98 @@ const PLACEMENT_SIDEBAR = 'Sidebar'
 
 // CodeFuel's top-level keys for each result type.
 const CODEFUEL_RESULT_TYPE_KEY_WEBPAGE = 'OrganicResults'
+const CODEFUEL_RESULT_TYPE_KEY_VIDEO = 'VideoResults'
 
 // Result types used internally in components.
 const RESULT_TYPE_WEBPAGES = 'WebPages'
+const RESULT_TYPE_VIDEOS = 'Videos'
 
-const createWebpageResult = webpageData => {
-  return {
+// Create a generic search result object
+const createSearchResult = ({
+  type,
+  key,
+  pixelUrl,
+  placement,
+  rank,
+  value,
+}) => ({
+  type,
+  key,
+  pixelUrl,
+  placement,
+  rank,
+  value,
+})
+
+// Takes an object (raw CodeFuel organic webpage data) and returns our
+// internal webpage result object.
+const createWebpageResult = data => {
+  return createSearchResult({
     type: RESULT_TYPE_WEBPAGES,
-    key: webpageData.TargetedUrl, // assume unique
+    key: data.TargetedUrl, // assume unique
+    pixelUrl: data.PixelUrl, // TODO: utilize this in the result component
+    placement: data.PlacementHint,
+    rank: data.Rank,
     value: {
-      displayUrl: webpageData.DisplayUrl,
-      deepLinks: get(webpageData, 'DeepLinks', []).map(deepLink => ({
+      displayUrl: data.DisplayUrl,
+      deepLinks: get(data, 'DeepLinks', []).map(deepLink => ({
         name: deepLink.Title,
         url: deepLink.TargetedUrl,
         snippet: deepLink.Description,
       })),
-      id: webpageData.TargetedUrl, // assume unique
-      name: webpageData.Title,
-      pixelUrl: webpageData.PixelUrl, // TODO: utilize this in the result component
-      snippet: webpageData.Description,
-      url: webpageData.TargetedUrl,
+      id: data.TargetedUrl, // assume unique
+      name: data.Title,
+      snippet: data.Description,
+      url: data.TargetedUrl,
     },
-    placement: webpageData.PlacementHint,
-    rank: webpageData.Rank,
+  })
+}
+
+// Takes an array (all videos, raw CodeFuel data) and returns an array
+// of our video result objects.
+const createVideoResults = (data = []) => {
+  if (!data.length) {
+    return []
   }
+  const firstItem = data[0]
+  return createSearchResult({
+    type: RESULT_TYPE_VIDEOS,
+    key: firstItem.TargetedUrl,
+    pixelUrl: firstItem.PixelUrl, // TODO: utilize this in the result component
+    placement: firstItem.PlacementHint, // all items have the same placement
+    value: data.map(videoItem => ({
+      allowHttpsEmbed: videoItem.AllowHttpsEmbed,
+      allowMobileEmbed: videoItem.AllowMobileEmbed,
+      // contentUrl: undefined,
+      datePublished: videoItem.DatePublished,
+      description: videoItem.Description,
+      duration: videoItem.Duration,
+      embedHtml: videoItem.EmbedHtml,
+      // encodingFormat: undefined,
+      // height: undefined,
+      // hostPageDisplayUrl: '',
+      hostPageUrl: videoItem.TargetedUrl,
+      // hostPageUrlPingSuffix: undefined,
+      // isAccessibleForFree: undefined,
+      // isSuperfresh: undefined,
+      // motionThumbnailUrl: undefined,
+      name: videoItem.Title,
+      publisher: [
+        {
+          name: videoItem.Publisher,
+        },
+      ],
+      thumbnail: {
+        width: videoItem.ThumbnailWidth,
+        height: videoItem.ThumbnailHeight,
+      },
+      thumbnailUrl: videoItem.ThumbnailUrl,
+      viewCount: videoItem.ViewCount,
+      // webSearchUrl: undefined,
+      // webSearchUrlPingSuffix: undefined,
+      // width: undefined,
+    })),
+  })
 }
 
 const formatSearchResults = rawSearchResults => {
@@ -50,8 +118,11 @@ const formatSearchResults = rawSearchResults => {
     []
   ).map(item => createWebpageResult(item))
 
-  const allResults = [].concat(webpageResults)
+  const videoResults = createVideoResults(
+    get(rawSearchResults, [CODEFUEL_RESULT_TYPE_KEY_VIDEO, ITEMS], [])
+  )
 
+  const allResults = [].concat(webpageResults).concat(videoResults)
   allResults.forEach(item => {
     switch (item.placement) {
       case PLACEMENT_POLE: {
