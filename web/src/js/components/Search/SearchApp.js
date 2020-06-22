0efbc778d487ef34b8a9400c512a7b4af25a930d
Kevin Jennison <kevin.jennison1@gmail.com>
2020-06-22 16:10:41 -0700

    Try using random CSS class names to fix hydration errors

--- web/src/js/components/Search/SearchApp.js
+++ web/src/js/components/Search/SearchApp.js
@@ -2,7 +2,13 @@ import React, { Suspense, lazy } from 'react'
 import PropTypes from 'prop-types'
 import { Helmet } from 'react-helmet'
 import { Redirect, Route, Switch } from 'react-router-dom'
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import {
+  MuiThemeProvider,
+  createMuiTheme,
+  jssPreset,
+} from '@material-ui/core/styles'
+import JssProvider from 'react-jss/lib/JssProvider'
+import { create } from 'jss'
 import ErrorBoundary from 'js/components/General/ErrorBoundary'
 import defaultSearchTheme from 'js/theme/searchTheme'
 import FullPageLoader from 'js/components/General/FullPageLoader'
@@ -27,88 +33,107 @@ const FirstSearchView = lazy(() =>
 
 const muiTheme = createMuiTheme(defaultSearchTheme)
 
+const createGenerateClassName = () => {
+  let counter = 0
+  return (rule, sheet) =>
+    `c${Math.random()
+      .toString(36)
+      .substring(2, 4) +
+      Math.random()
+        .toString(36)
+        .substring(2, 4)}-${rule.key}-${counter++}`
+}
+
+const jss = create(jssPreset())
+jss.options.createGenerateClassName = createGenerateClassName
+
 class SearchApp extends React.Component {
   render() {
     const { location } = this.props
     return (
-      <MuiThemeProvider theme={muiTheme}>
-        <ErrorBoundary brand={SEARCH_APP}>
-          <Helmet
-            titleTemplate={`%s - Search for a Cause`}
-            defaultTitle={'Search for a Cause'}
-          >
-            <link rel="icon" href={searchFavicon} />
-          </Helmet>
-          <div>
-            <Suspense
-              fallback={<FullPageLoader app={SEARCH_APP} delay={350} />}
+      <JssProvider jss={jss}>
+        <MuiThemeProvider theme={muiTheme}>
+          <ErrorBoundary brand={SEARCH_APP}>
+            <Helmet
+              titleTemplate={`%s - Search for a Cause`}
+              defaultTitle={'Search for a Cause'}
             >
-              <Switch>
-                <Route exact path="/search" component={SearchPageComponent} />
-                <Route
-                  exact
-                  path="/search/bing"
-                  render={props => (
-                    <SearchPageComponent
-                      {...props}
-                      searchProvider={SEARCH_PROVIDER_BING}
-                    />
-                  )}
-                />
-                <Route
-                  exact
-                  path="/search/codefuel"
-                  render={props => (
-                    <SearchPageComponent
-                      {...props}
-                      searchProvider={SEARCH_PROVIDER_CODEFUEL}
-                    />
-                  )}
-                />
-                <Route
-                  exact
-                  path="/search/yahoo"
-                  render={props => (
-                    <SearchPageComponent
-                      {...props}
-                      searchProvider={SEARCH_PROVIDER_YAHOO}
-                    />
-                  )}
-                />
-                <Route
-                  exact
-                  path="/search/uninstalled/"
-                  component={SearchPostUninstallView}
-                />
-                <Route
-                  exact
-                  path="/search/first-search/"
-                  component={FirstSearchView}
-                />
-                <Route
-                  exact
-                  path="/search/auth/"
-                  component={SearchAuthRedirect}
-                />
-                <Route
-                  exact
-                  path="/search/random/"
-                  component={SearchRandomQueryView}
-                />
-                <Route
-                  path="/search/account/"
-                  component={SearchSettingsPageComponent}
-                />
-                <Route
-                  path="/search/profile/"
-                  component={SearchSettingsPageComponent}
-                />
-                <Redirect from="*" to={{ ...location, pathname: '/search' }} />
-              </Switch>
-            </Suspense>
-          </div>
-        </ErrorBoundary>
-      </MuiThemeProvider>
+              <link rel="icon" href={searchFavicon} />
+            </Helmet>
+            <div>
+              <Suspense
+                fallback={<FullPageLoader app={SEARCH_APP} delay={350} />}
+              >
+                <Switch>
+                  <Route exact path="/search" component={SearchPageComponent} />
+                  <Route
+                    exact
+                    path="/search/bing"
+                    render={props => (
+                      <SearchPageComponent
+                        {...props}
+                        searchProvider={SEARCH_PROVIDER_BING}
+                      />
+                    )}
+                  />
+                  <Route
+                    exact
+                    path="/search/codefuel"
+                    render={props => (
+                      <SearchPageComponent
+                        {...props}
+                        searchProvider={SEARCH_PROVIDER_CODEFUEL}
+                      />
+                    )}
+                  />
+                  <Route
+                    exact
+                    path="/search/yahoo"
+                    render={props => (
+                      <SearchPageComponent
+                        {...props}
+                        searchProvider={SEARCH_PROVIDER_YAHOO}
+                      />
+                    )}
+                  />
+                  <Route
+                    exact
+                    path="/search/uninstalled/"
+                    component={SearchPostUninstallView}
+                  />
+                  <Route
+                    exact
+                    path="/search/first-search/"
+                    component={FirstSearchView}
+                  />
+                  <Route
+                    exact
+                    path="/search/auth/"
+                    component={SearchAuthRedirect}
+                  />
+                  <Route
+                    exact
+                    path="/search/random/"
+                    component={SearchRandomQueryView}
+                  />
+                  <Route
+                    path="/search/account/"
+                    component={SearchSettingsPageComponent}
+                  />
+                  <Route
+                    path="/search/profile/"
+                    component={SearchSettingsPageComponent}
+                  />
+                  <Redirect
+                    from="*"
+                    to={{ ...location, pathname: '/search' }}
+                  />
+                </Switch>
+              </Suspense>
+            </div>
+          </ErrorBoundary>
+        </MuiThemeProvider>
+      </JssProvider>
     )
   }
 }
