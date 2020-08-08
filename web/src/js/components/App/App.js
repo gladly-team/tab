ed13c2cbd2116b71cfaaab0336615d0fd5c815e3
Kevin Jennison <kevin.jennison1@gmail.com>
2020-08-07 17:46:51 -0700

    Fix acceptance tests: only init the CMP on the dashboard page

--- web/src/js/components/App/App.js
+++ web/src/js/components/App/App.js
@@ -15,10 +15,6 @@ import tabFavicon from 'js/assets/logos/favicon.ico'
 import { TAB_APP } from 'js/constants'
 import { parseUrlSearchString, validateAppName } from 'js/utils/utils'
 
-import('tab-cmp').then(tabCMP => {
-  tabCMP.initializeCMP({ some: 'options' })
-})
-
 const AuthenticationView = lazy(() =>
   import('js/components/Authentication/AuthenticationView')
 )
