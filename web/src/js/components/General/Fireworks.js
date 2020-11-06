0da020c61817474e15c63bd6dc40d4b65a40fda9
Kevin Jennison <kevin.jennison1@gmail.com>
2020-11-06 15:24:38 -0800

    Add a close button to the fireworks

--- web/src/js/components/General/Fireworks.js
+++ web/src/js/components/General/Fireworks.js
@@ -1,7 +1,9 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import * as FireworksCanvas from 'fireworks-canvas'
 import { withStyles } from '@material-ui/core/styles'
 import FadeInDashboardAnimation from 'js/components/General/FadeInDashboardAnimation'
+import IconButton from '@material-ui/core/IconButton'
+import CloseIcon from '@material-ui/icons/Close'
 
 const styles = {
   container: {
@@ -13,9 +15,21 @@ const styles = {
     left: 0,
     background: 'rgba(0, 0, 0, 0.85)',
   },
+  closeIconButton: {
+    position: 'absolute',
+    zIndex: 6000,
+    top: 10,
+    right: 10,
+  },
+  closeIcon: {
+    color: 'rgba(255, 255, 255, 0.8)',
+    width: 36,
+    height: 36,
+  },
 }
 
 const Fireworks = ({ classes, children, options }) => {
+  const [active, setActive] = useState(true)
   const containerEl = useRef(null)
   if (containerEl.current) {
     const fireworks = new FireworksCanvas(containerEl.current, options)
@@ -23,9 +37,19 @@ const Fireworks = ({ classes, children, options }) => {
   }
   return (
     <FadeInDashboardAnimation>
-      <div ref={containerEl} className={classes.container}>
-        {children}
-      </div>
+      {active ? (
+        <div ref={containerEl} className={classes.container}>
+          <IconButton
+            className={classes.closeIconButton}
+            onClick={() => {
+              setActive(false)
+            }}
+          >
+            <CloseIcon className={classes.closeIcon} />
+          </IconButton>
+          {children}
+        </div>
+      ) : null}
     </FadeInDashboardAnimation>
   )
 }
