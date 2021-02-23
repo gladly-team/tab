c3a9c97b72988257dcdbc50733a9df43f650a6d4
Alec Maliwanag <alecmaliwanag@alecs-mbp.lan>
2021-02-22 17:15:13 -0800

    fix failing tests

--- dynamodb/scripts/__tests__/getTableInfo.test.js
+++ dynamodb/scripts/__tests__/getTableInfo.test.js
@@ -11,19 +11,19 @@ describe('getting table info', () => {
     process.env.DB_TABLE_NAME_APPENDIX = ''
     const tableNames = getTableInfo()
     expect(tableNames[0].TableName).toBe('Users')
-    expect(tableNames[2].TableName).toBe('Widgets')
+    expect(tableNames[2].TableName).toBe('UserImpact')
   })
 
   test('uses an appendix on table names when it is set', () => {
     process.env.DB_TABLE_NAME_APPENDIX = '-staging'
     const tableNames = getTableInfo()
     expect(tableNames[0].TableName).toBe('Users-staging')
-    expect(tableNames[2].TableName).toBe('Widgets-staging')
+    expect(tableNames[2].TableName).toBe('UserImpact-staging')
   })
 
   test('uses no table name appendix when the config value is not in config', () => {
     const tableNames = getTableInfo()
     expect(tableNames[0].TableName).toBe('Users')
-    expect(tableNames[2].TableName).toBe('Widgets')
+    expect(tableNames[2].TableName).toBe('UserImpact')
   })
 })
