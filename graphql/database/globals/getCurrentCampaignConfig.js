7e1e31c5cfd17e0d5bef0774e8bc7cd5df832122
Kevin Jennison <kevin.jennison1@gmail.com>
2021-05-04 10:24:11 -0700

    Set up campaign

--- graphql/database/globals/getCurrentCampaignConfig.js
+++ graphql/database/globals/getCurrentCampaignConfig.js
@@ -1,26 +1,26 @@
 import createCampaignConfiguration from './createCampaignConfiguration'
 
-const campaignTitle = '## Spotlight: The Uyghur Human Rights Project'
+const campaignTitle = '## COVID-19 Relief: CARE India'
 const campaignDescription = `
-#### With another month comes another amazing organization to support!
-#### [The Uyghur Human Rights Project](https://uhrp.org/) advocates for and works to protect the Uyghurs, ethnically and culturally Turkic people living in the areas of Central Asia, commonly known as East Turkistan.
-#### Reports from East Turkistan document a pattern of abuse, including political imprisonment, torture, and disappearances. We ask you to join us in supporting an organization amplifying the voices affected by these conditions and advocating for positive change!
-
+#### As COVID-19 continues to spread throughout India, communities are desperate for frontline workers and PPE. Please join us in supporting [CARE India](https://www.careindia.org/) to help this community during an incredibly difficult time.
+#### CARE India has supplied frontline workers with more than 39,000 PPE kits, along with masks and other supplies so far. They will continue to supply these necessary materials as well as set up temporary COVID-19 hospitals and Care Centres.
+#### Already donated all your hearts? Invite your friends to make an impact as well:
 `
 // const campaignDescriptionTwo = ``
 
-const campaignEndTitle = '## Thanks for supporting the UHRP!'
+const campaignEndTitle = '## COVID-19 Relief: CARE India'
 const campaignEndDescription = `
-#### Thank you! Every Heart donated towards charitable causes like [The Uyghur Human Rights Project](https://uhrp.org/) adds up, and the result is something to be proud of. These funds raised will aid in advocacy against the abuse of the Uyghur people in East Turkistan. 
-#### If you have a charitable cause that is close to your heart and want it featured as a Spotlight Charity, contact us or drop a comment through our Instagram [@tabforacause](https://www.instagram.com/tabforacause/) to tell us about it!
+#### With your help, we raised thousands of dollars to provide PPE and access to frontline workers to those affected by COVID-19 in India. We thank you for helping support [CARE India](https://www.careindia.org/) and for continuing to make a positive impact.
 `
+// #### If you have a charitable cause that is close to your heart and want it featured as a Spotlight Charity, contact us or drop a comment through our Instagram [@tabforacause](https://www.instagram.com/tabforacause/) to tell us about it!
+
 // const campaignEndDescriptionTwo =
 //   '#### This is only part of [our relief efforts](https://tab.gladly.io/covid-19/) for this global health crisis—more to come soon.'
 
 // Hardcode campaign data here.
 const CURRENT_CAMPAIGN = createCampaignConfiguration({
-  campaignId: 'UHRPApr2021',
-  charityId: '839030b5-b0e2-43f7-8ce4-c2a50645cd54',
+  campaignId: 'CAREIndiaMay2021',
+  charityId: 'b95be5b4-2812-4529-bb13-9c9d981bfe2c',
   content: {
     titleMarkdown: campaignTitle,
     descriptionMarkdown: campaignDescription,
@@ -43,7 +43,7 @@ const CURRENT_CAMPAIGN = createCampaignConfiguration({
     numberSource: 'hearts', // One of: hearts, moneyRaised, newUsers, tabsOpened
     showProgressBarLabel: true,
     showProgressBarEndText: false,
-    targetNumber: 7e6,
+    targetNumber: 8e6,
     // transformNumberSourceValue: num => {
     //   return 9e6
     // },
@@ -68,20 +68,21 @@ const CURRENT_CAMPAIGN = createCampaignConfiguration({
     //   url: 'https://tab.gladly.io',
     //   FacebookShareButtonProps: {
     //     quote:
-    //       'We are helping extend an immediate lifeline to people who are legally presumed innocent but cannot afford bail! And you can too, by supporting The Bail Project on Tab for a Cause.',
+    //       'I just helped raise money for CARE India to help provide access to PPE & temporary hospitals for those affected by COVID-19. With numbers rising every day, these resources are needed more than ever, & I was able to make a difference by just opening browser tabs!',
     //   },
-    //   RedditShareButtonProps: {
-    //     title:
-    //       'Browser tabs transformed into bailouts for low-income people through Tab for a Cause & The Bail Project',
-    //   },
+    //   // RedditShareButtonProps: {
+    //   //   title:
+    //   //     'Browser tabs transformed into bailouts for low-income people through Tab for a Cause & The Bail Project',
+    //   // },
     //   TumblrShareButtonProps: {
-    //     title: 'Browser tabs transformed into bailouts for low-income people',
+    //     title:
+    //       'Browser tabs transformed into PPE & temporary hospitals in India',
     //     caption:
-    //       'We are helping extend an immediate lifeline to people who are legally presumed innocent but cannot afford bail! And you can too, by supporting The Bail Project on Tab for a Cause.',
+    //       'I just helped raise money for CARE India to help provide access to PPE & temporary hospitals for those affected by COVID-19. With numbers rising every day, these resources are needed more than ever, & I was able to make a difference by just opening browser tabs!',
     //   },
     //   TwitterShareButtonProps: {
     //     title:
-    //       'On @TabForACause, we’re supporting people who are legally presumed innocent but cannot afford bail via @BailProject.  All we had to do? Open browser tabs!',
+    //       'I just helped raise money for CARE India to help provide access to PPE & temporary hospitals for those affected by COVID-19. With numbers rising every day, these resources are needed more than ever, & I was able to make a difference by just opening browser tabs! @TabForACause',
     //     related: ['@TabForACause'],
     //   },
     // },
@@ -89,8 +90,28 @@ const CURRENT_CAMPAIGN = createCampaignConfiguration({
   showCountdownTimer: true,
   showHeartsDonationButton: true,
   showProgressBar: true,
-  showSocialSharing: false,
-  // socialSharing: undefined,
+  showSocialSharing: true,
+  socialSharing: {
+    url: 'https://tab.gladly.io',
+    FacebookShareButtonProps: {
+      quote:
+        'I just helped raise money for CARE India to help provide access to PPE & temporary hospitals for those affected by COVID-19. With numbers rising every day, these resources are needed more than ever, & I was able to make a difference by just opening browser tabs!',
+    },
+    // RedditShareButtonProps: {
+    //   title:
+    //     'Browser tabs transformed into bailouts for low-income people through Tab for a Cause & The Bail Project',
+    // },
+    TumblrShareButtonProps: {
+      title: 'Browser tabs transformed into PPE & temporary hospitals in India',
+      caption:
+        'I just helped raise money for CARE India to help provide access to PPE & temporary hospitals for those affected by COVID-19. With numbers rising every day, these resources are needed more than ever, & I was able to make a difference by just opening browser tabs!',
+    },
+    TwitterShareButtonProps: {
+      title:
+        'I just helped raise money for CARE India to help provide access to PPE & temporary hospitals for those affected by COVID-19. Tthese resources are needed more than ever, & I was able to make a difference by just opening browser tabs! @TabForACause',
+      related: ['@TabForACause'],
+    },
+  },
   theme: {
     color: {
       main: '#0d4c6b',
@@ -98,8 +119,8 @@ const CURRENT_CAMPAIGN = createCampaignConfiguration({
     },
   },
   time: {
-    start: '2021-04-26T08:00:00.000Z',
-    end: '2021-05-03T16:00:00.000Z',
+    start: '2021-05-04T08:00:00.000Z',
+    end: '2021-05-11T16:00:00.000Z',
   },
 })
 
