3c39dd152c17c4d2f1072ccc2bc8293236b9c695
Alec Maliwanag <alecmaliwanag@alecs-mbp.lan>
2021-07-27 12:43:21 -0700

    pause

---
+++ graphql/database/missions/__tests__/inviteUserToMission.test.js
@@ -0,0 +1,80 @@
+/* eslint-env jest */
+import { verifyAndSendInvite, getUserInvites } from '../../invitedUsers/utils'
+import inviteUserToMission from '../inviteUserToMission'
+import { getMockUserContext, getMockUserInstance } from '../../test-utils'
+
+jest.mock('../utils', () => ({
+  verifyAndSendInvite: jest.fn(),
+  sanitize: string => string,
+  getUserInvites: jest.fn(),
+}))
+jest.mock('../../databaseClient')
+jest.mock('../../globals/globals')
+const userContext = getMockUserContext()
+
+afterEach(() => {
+  jest.clearAllMocks()
+})
+const userId = userContext.id
+const mockParams = [userContext, userId, ['test123', 'test124'], 'alec']
+describe('invite users to mission', () => {
+  it('it successfully emails and invites new users', async () => {
+    getUserInvites.mockReturnValueOnce([
+      getMockUserInstance(),
+      [{ user: 'test' }],
+    ])
+    verifyAndSendInvite.mockReturnValueOnce({ email: 'test123' })
+    verifyAndSendInvite.mockReturnValueOnce({ email: 'test124' })
+    const results = await inviteUserToMission(...mockParams)
+    expect(verifyAndSendInvite).toHaveBeenCalledTimes(2)
+    expect(results.successfulEmailAddresses.length).toEqual(2)
+    expect(results.failedEmailAddresses.length).toEqual(0)
+  })
+
+  it('it seperates failed email creations from succesful email creations', async () => {
+    getUserInvites.mockReturnValueOnce([
+      getMockUserInstance(),
+      [{ user: 'test' }],
+    ])
+    verifyAndSendInvite.mockReturnValueOnce({ email: 'test123' })
+    verifyAndSendInvite.mockReturnValueOnce({
+      email: 'test124',
+      error: 'user already exists',
+    })
+    const results = await inviteUserToMission(...mockParams)
+    expect(verifyAndSendInvite).toHaveBeenCalledTimes(2)
+    expect(results.successfulEmailAddresses.length).toEqual(1)
+    expect(results.failedEmailAddresses.length).toEqual(1)
+  })
+
+  it('it throws an error if user tries to create more than 50 invites in 24 hours', async () => {
+    getUserInvites.mockReturnValueOnce([
+      getMockUserInstance(),
+      [{ user: 'test' }],
+    ])
+    verifyAndSendInvite.mockReturnValueOnce({ email: 'test123' })
+    verifyAndSendInvite.mockReturnValueOnce({
+      email: 'test124',
+      error: 'user already exists',
+    })
+    const randomStringArray = []
+    function makeid(length) {
+      const result = []
+      const characters =
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+      const charactersLength = characters.length
+      for (let i = 0; i < length; i += 1) {
+        result.push(
+          characters.charAt(Math.floor(Math.random() * charactersLength))
+        )
+      }
+      return result.join('')
+    }
+    for (let i = 0; i < 61; i += 1) {
+      randomStringArray.push(makeid(9))
+    }
+    expect(
+      inviteUserToMission(userContext, userId, randomStringArray, 'alec')
+    ).rejects.toThrow('user is trying to invite too many people in 24 hours')
+  })
+})
