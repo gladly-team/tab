b776ef822a7aa75b6c16d53ce0ea7240b47c734f
Kevin Jennison <kevin.jennison1@gmail.com>
2021-10-25 19:28:06 -0400

    Update more copy

--- graphql/database/cause/causes/teamseas/causeData.js
+++ graphql/database/cause/causes/teamseas/causeData.js
@@ -57,22 +57,20 @@ const data = {
       },
     ],
   },
-
-  // TODO Extra, change facebookButtonTitle if revised
   sharing: {
     facebookButtonTitle:
-      'Changing the world for the better is a team effort, that is why I downloaded Tab for #TeamSeas. Every time I open a new tab, I raise money to help clean up trash from the water around the world. Check it out - it’s free!',
+      'I joined Tab for #TeamSeas, and now every tab I open helps clean up our rivers and oceans. Check it out - it’s free!',
     imgCategory: 'seas',
     redditButtonTitle:
-      'Surf the web and clean up our ocean, rivers, and lakes with Tab for #TeamSeas',
+      'Clean up our seas with every tab you open, for free, with Tab for #TeamSeas',
     sendgridEmailTemplateId: 'd-ff97cd972da342a6a208f09235671479',
     subtitle: socialSubtitle,
     title: socialTitle,
     tumblrCaption:
-      'Every time I open a new tab I am helping clean up our planet through Tab for #TeamSeas. Download it for free with my link and automatically help remove 5 plastic bottles worth of trash from oceans, rivers, and lakes around the world.',
+      "I joined Tab for #TeamSeas, and now every tab I open helps clean up our rivers and oceans. Join in - it's free!",
     tumblrTitle: 'Save the planet while surfing the web on Tab for #TeamSeas!',
     twitterButtonTitle:
-      'Making a positive change is a group effort. That is exactly why I am doing my part with Tab for #TeamSeas. Every time I open a new tab, I raise money to help clean up trash from the water around the world. Check it out - it’s free!',
+      'I joined Tab for #TeamSeas, and now every tab I open helps clean up our rivers and oceans. Check it out - it’s free:',
   },
   squads: {
     currentMissionAlert: 'TODO',
