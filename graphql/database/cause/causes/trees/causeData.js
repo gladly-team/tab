e01ad5a61afb1bf9549787b926461475d0e0e68a
Kevin Jennison <kevin.jennison1@gmail.com>
2022-02-22 09:36:54 -0500

    Copy edits

--- graphql/database/cause/causes/trees/causeData.js
+++ graphql/database/cause/causes/trees/causeData.js
@@ -6,7 +6,7 @@
 const data = {
   id: 'tdU_PsRIM',
   about:
-    '### About Tab for Trees\n\nTab for Trees is proud to support **Eden Reforestation Projects**. With each tab opened, you are making a positive impact on our planet that will affect generations to come.\n\n[Eden Reforestation Projects] (https://www.edenprojects.org/) works to combat deforestation and create job opportunities in local communities by planting trees. Deforestation can bring on flooding, erosion, and animal extinction, and Eden Reforestation Projects is able to enact change by planting trees in affected communities to help both the local ecosystem and economy.\n\n**Your tabs are making an impact**. The counter on your new tab page (with the tree icon) estimates how many trees you have helped plant. Eden Reforestation Projects plants trees and employs local communities to restore and protect their natural environment. Thank you for making a difference!',
+    '### About Tab for Trees\n\nTab for Trees is proud to support **Eden Reforestation Projects**. With each tab opened, you are making a positive impact on our planet that will affect generations to come.\n\n[Eden Reforestation Projects](https://www.edenprojects.org/) works to combat deforestation and create job opportunities in local communities by planting trees. Deforestation can bring on flooding, erosion, and animal extinction, and Eden Reforestation Projects is able to enact change by planting trees in affected communities to help both the local ecosystem and economy.\n\n**Your tabs are making an impact**. The counter on your new tab page (with the tree icon) estimates how many trees you have helped plant. Eden Reforestation Projects plants trees and employs local communities to restore and protect their natural environment. Thank you for making a difference!',
   name: 'Trees',
   isAvailableToSelect: false,
   backgroundImageCategory: 'trees',
@@ -17,7 +17,7 @@ const data = {
   slug: 'trees',
   impact: {
     impactCounterText:
-      '##### **Your tree tracker!**\n\nThis shows how many trees your tabs can plant. Every tab you open helps. Keep it up!\n\n',
+      '##### **Your tree tracker!**\n\nThis shows how many trees your tabs have planted. Every tab you open helps. Keep it up!\n\n',
     claimImpactSubtitle:
       '##### You did it! Your opened tabs have become a tree. Keep it up!',
     impactIcon: 'pine-tree',
