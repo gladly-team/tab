a9393a1191dbe04020e8d68f9ffb61c2aedbbff4
Kevin Jennison <kevin.jennison1@gmail.com>
2022-02-24 16:01:21 -0500

    Update cause copy

--- graphql/database/cause/causes/globalHealth/causeData.js
+++ graphql/database/cause/causes/globalHealth/causeData.js
@@ -5,56 +5,54 @@
 
 const data = {
   id: 'sPFpWRT7q',
-  // TODO
   about:
-    '### About Tab for Trees\n\nTab for Trees is proud to support **Eden Reforestation Projects**. With each tab opened, you are making a positive impact on our planet that will affect generations to come.\n\n[Eden Reforestation Projects](https://www.edenprojects.org/) works to combat deforestation and create job opportunities in local communities by planting trees. Deforestation can bring on flooding, erosion, and animal extinction, and Eden Reforestation Projects is able to enact change by planting trees in affected communities to help both the local ecosystem and economy.\n\n**Your tabs are making an impact**. The counter on your new tab page (with the tree icon) estimates how many trees you have helped plant. Eden Reforestation Projects plants trees and employs local communities to restore and protect their natural environment. Thank you for making a difference!',
+    '### About Tab for Global Health\n\n**Tab for Global Health** is proud to support [Partners In Health](https://www.pih.org/) and Dr. Paul Farmer’s legacy. With each tab opened, you are helping provide access to health care to those in need and strengthening public health systems all around the world. \n \nPartners In Health’s mission is to provide a preferential option for the poor in health care. By establishing long-term relationships with sister organizations based in settings of poverty, Partners In Health strives to achieve two overarching goals: to bring the benefits of modern medical science to those most in need of them and to serve as an antidote to despair.\n \nThey draw on the resources of the world’s leading medical and academic institutions and on the lived experience of the world’s most impoverished communities. At its root, their mission is both medical and moral. It is based on solidarity, rather than charity alone.\n \nWhen their patients are ill and have no access to care, their team of health professionals, scholars, and activists will do whatever it takes to make them well.\n \nPartners In Health has used a community-based model to provide health care and support for the last 30 years and now serves millions of patients across 12 countries.\n \n**Your tabs are making an impact**. The counter on your new tab page (with the medical bag icon) estimates how many in-home visits from a [community health worker](https://www.pih.org/programs/community-health-workers) your tabs have helped support.  Each counter increment represents raising ~1/50th of the money needed to support an in-home visit. Thank you for making a difference!',
   name: 'Global Health',
   isAvailableToSelect: false,
   backgroundImageCategory: 'legacy',
   charityId: 'cb7ab7e4-bda6-4fdf-825a-30db05911705',
   individualImpactEnabled: true,
-  impactVisits: 140,
-  landingPagePath: '/trees/',
-  slug: 'trees',
+  impactVisits: 131,
+  landingPagePath: '/global-health/',
+  slug: 'global-health',
   impact: {
     impactCounterText:
-      '##### **Your tree tracker!**\n\nThis shows how many trees your tabs have planted. Every tab you open helps. Keep it up!\n\n',
+      '##### **Your positive impact!**\n\nThis counter shows your progress toward helping provide an at-home medical visit. Every tab you open helps. Keep it up!\n\n',
     claimImpactSubtitle:
-      '##### You did it! Your opened tabs have become a tree. Keep it up!',
-    impactIcon: 'pine-tree',
+      '##### You did it! Your tabs are on their way to providing an at-home medical visit.',
+    impactIcon: 'medical-bag',
     // @feature/generalize-referral-reward
     // Note: this is not used until we generalize referral incentives:
     // https://github.com/gladly-team/tab/blob/master/graphql/database/users/rewardReferringUser.js#L91
     referralRewardNotification:
-      "##### Congrats! You recruited a friend to help fight climate change just by opening tabs. To celebrate, we'll have Eden Reforestation Projects plant ${pendingUserReferralImpact} extra tree${isPlural(pendingUserReferralImpact)} on your behalf.\n\n",
+      '##### Congrats! You recruited a friend to help provide access to health care around the world.\n\n',
     // Note: this is not used until we generalize referral incentives.
     // @feature/generalize-referral-reward
-    referralRewardTitle:
-      '#### You just planted ${claimedReferralImpact} tree${isPlural(claimedReferralImpact)}!\n\n',
+    referralRewardTitle: '#### You just moved global health forward!\n\n',
     referralRewardSubtitle:
-      "##### Congratulations! You're making a huge impact to fight climate change. Want to help plant even more trees? Invite a few more friends!",
+      "##### Congratulations! You're making a huge impact to support the wellbeing of people around the world. Want to do more? Invite a few more friends!",
     impactWalkthroughText:
-      "##### When you do, you'll plant a tree to fight climate change. We'll track how many trees you've planted on the top of the page:\n\n",
+      "##### When you do, we will chip in a little extra to help provide at-home healthcare visits to people in need of Partners In Health’s support. We'll track your progress at the top of the page:\n\n",
     // @feature/generalize-referral-reward
     // Note: this is not used until we generalize referral incentives.
     newlyReferredImpactWalkthroughText:
-      "##### Your friend has started you off with a tree! Open a new tab now to plant your second tree. We'll track how many trees you've planted on the top of the page.\n\n",
+      "##### Your friend has started you off with some impact! Open a new tab now to help provide at-home healthcare visits to people in need. We'll track your progress at the top of the page:\n\n",
     confirmImpactSubtitle:
-      "##### Each time you open a tab, you'll be helping plant trees to combat climate change. Ready to get started?",
+      "##### Each time you open a tab, you'll be helping provide at-home healthcare visits through Partners In Health. Ready to get started?\n\n",
     // walkMeGif: undefined,
   },
   onboarding: {
     steps: [
       {
-        title: '### Your tabs have a tree-mendous impact!',
+        title: '### Your tabs are doing great things',
         subtitle:
-          'Now, every tab you open works to fight climate change by replanting forests around the world.\n\nTabbers like you are supporting critical nonprofit work to ensure our planet is happy and healthy for years to come. Thank you!\n\n',
+          'Now, every tab you open helps train and support community healthcare workers to provide at-home visits.\n\nTabbers like you are supporting critical nonprofit work all around the world. Thank you!\n\n',
         imgName: 'globalHealth/onboarding1.svg',
       },
       {
         title: '### Do more with your squad',
         subtitle:
-          'Plant trees even faster with a squad!\n\nYou and your friends can team up to plant a forest in no time.',
+          'With your squad, help even more people access safe health care!\n\nYou and your friends can team up to do more good.\n\n',
         imgName: 'globalHealth/onboarding2.svg',
       },
       {
@@ -68,30 +66,30 @@ const data = {
   sharing: {
     title: '### **Get a friend on board**',
     subtitle:
-      '##### Everyone can and _should_ make a difference to protect our planet: invite a friend to join in.\n\n',
+      '##### Everyone can and _should_ make a difference: invite a friend to join in.\n\n',
     shareImage: 'globalHealth/sharing.svg',
     sentImage: 'globalHealth/sharing.svg',
-    imgCategory: 'trees',
+    imgCategory: 'global-health',
     email: {
       image:
         'https://prod-tab2017-media.gladly.io/img/cause/globalHealth/emailInvite.jpg',
       title:
-        'We all have a part to play in protecting our planet from the harmful effects of climate change. That’s why {{name}} thinks you should join them on Tab for Trees.',
+        'We all can <em>and should</em> make a difference where we can! With Tab for Global Health, supporting healthcare access has never been easier.',
       about:
-        '<div>Tab for Trees turns your web browser into a force for good. With each tab opened, ad revenue supports The Eden Reforestation Projects in fighting climate change through reforestation initiatives.</div>\n\n',
+        '<div>Tab for Global Health turns your web browser into a force for good. With each tab opened, ad revenue supports community healthcare training through Partners In Health. This training helps ensure the people in need of care will receive it, long term.</div>\n\n',
       faq:
-        'We’re partnered with Eden Reforestation Projects to ensure the money raised will directly support their goals of creating jobs, protecting ecosystems, and helping mitigate climate change.',
+        'We’re partnered with Partners In Health to ensure the money raised will directly support their mission of providing high-quality health care globally to those who need it.',
       sendgridEmailTemplateId: 'd-c925270aca954ff3b4c05f436cb52b54',
     },
     sendgridEmailTemplateId: 'd-c925270aca954ff3b4c05f436cb52b54',
     facebookButtonTitle:
-      'Looking to turn over a new leaf and start doing more for the environment? Well, then I have the browser extension for you! Tab for Trees helps plant new trees every time you open a new tab. Check it out - it’s free!',
+      'People around the world are struggling to access safe and reliable healthcare, but this is a problem we can all help solve. Now, with Tab for Global Health, every tab I open helps raise money for Partners In Health to train community healthcare workers. Check it out - it’s free!',
     twitterButtonTitle:
-      'I joined Tab for Trees and now every tab I open helps plant trees to combat climate change! Check it out - it’s free! 🌳🤍',
-    redditButtonTitle: 'Combat climate change with your browser tabs',
-    tumblrTitle: 'Combat climate change with your browser tabs',
+      'People around the world are struggling to access safe and reliable healthcare, but this is a problem we can all help solve. Now, with Tab for Global Health, every tab I open helps raise money for Partners In Health to train community healthcare workers. Check it out - it’s free!',
+    redditButtonTitle: 'Help provide quality healthcare around the world',
+    tumblrTitle: 'Help provide quality healthcare around the world ',
     tumblrCaption:
-      'Every time I open a new tab, I’m helping plant trees with The Eden Reforestation Projects. Join Tab for Trees for free and start making an impact today! 🌳🤍',
+      'Every time I open a new tab with Tab for Global Health I am raising money to train healthcare workers through Partners In Health. Join today for free! ',
   },
   // TODO
   squads: {
@@ -108,7 +106,7 @@ const data = {
     squadInviteTemplateId: 'TODO',
   },
   theme: {
-    primaryColor: '#0FAF7F',
+    primaryColor: '#FB5050',
     secondaryColor: '#29BEBA',
   },
 }
