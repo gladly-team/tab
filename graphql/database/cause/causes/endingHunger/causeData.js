f852df66faa7ae948837785c65da4c5d14935571
Kevin Jennison <kevin.jennison1@gmail.com>
2022-02-25 11:35:20 -0500

    Edit cause data

--- graphql/database/cause/causes/endingHunger/causeData.js
+++ graphql/database/cause/causes/endingHunger/causeData.js
@@ -6,40 +6,40 @@
 const data = {
   id: '1HCq9sFTp',
   about:
-    '### About Tab for Global Health\n\n**Tab for Global Health** is proud to support [Partners In Health](https://www.pih.org/) and Dr. Paul Farmer’s legacy. With each tab opened, you are helping provide access to health care to those in need and strengthening public health systems all around the world. \n \nPartners In Health’s mission is to provide a preferential option for the poor in health care. By establishing long-term relationships with sister organizations based in settings of poverty, Partners In Health strives to achieve two overarching goals: to bring the benefits of modern medical science to those most in need of them and to serve as an antidote to despair.\n \nThey draw on the resources of the world’s leading medical and academic institutions and on the lived experience of the world’s most impoverished communities. At its root, their mission is both medical and moral. It is based on solidarity, rather than charity alone.\n \nWhen their patients are ill and have no access to care, their team of health professionals, scholars, and activists will do whatever it takes to make them well.\n \nPartners In Health has used a community-based model to provide health care and support for the last 30 years and now serves millions of patients across 12 countries.\n \n**Your tabs are making an impact**. The counter on your new tab page (with the medical bag icon) estimates how many in-home visits from a [community health worker](https://www.pih.org/programs/community-health-workers) your tabs have helped support.  Each counter increment represents raising ~1/50th of the money needed to support an in-home visit. Thank you for making a difference!',
+    '### About Tab for Ending Hunger\n\n**Tab for Ending Hunger** is proud to support Action Against Hunger. With each tab opened, you are providing life-saving nutrition to people currently facing hunger. \n \n[Action Against Hunger](https://www.actionagainsthunger.org/) works to prevent and treat severe malnutrition around the world. It provides long-term results by gathering, analyzing, and sharing data, treating people in need, addressing both direct and underlying causes, and strengthening local capacity to combat this issue. \n \n**Your tabs are making an impact**. The counter on your new tab page (with the apple icon) estimates how many ready-to-use therapeutic food packs you’ve helped provide. Action Against Hunger helps provide this formula and other nutritional resources to ensure communities can thrive for years to come. Thank you for making a difference.',
   name: 'Ending Hunger',
   isAvailableToSelect: false,
-  icon: 'medical-bag',
+  icon: 'food-apple',
   backgroundImageCategory: 'trees', // TODO: update with new collection
-  charityId: '8d1ceba8-1aee-439f-a49b-65a318e92306',
+  charityId: 'ea019270-1cda-411f-b41e-90406fbe15ee',
   individualImpactEnabled: true,
-  impactVisits: 131,
-  landingPagePath: '/global-health/',
-  slug: 'global-health',
+  impactVisits: 366,
+  landingPagePath: '/ending-hunger/',
+  slug: 'ending-hunger',
   impact: {
     impactCounterText:
-      '##### **Your positive impact!**\n\nThis counter shows your progress toward helping provide an at-home medical visit. Every tab you open helps. Keep it up!\n\n',
+      '##### **Your positive impact!**\n\nThis shows how many ready-to-use therapeutic food packets your tabs can provide to help people experiencing severe hunger. Every tab you open helps. Keep it up!\n\n',
     claimImpactSubtitle:
-      '##### You did it! Your tabs are on their way to providing an at-home medical visit.',
-    impactIcon: 'medical-bag',
+      '##### You did it! You just turned your tabs into ready-to-use therapeutic food for someone facing hunger. Keep it up!',
+    impactIcon: 'food-apple',
     // @feature/generalize-referral-reward
     // Note: this is not used until we generalize referral incentives:
     // https://github.com/gladly-team/tab/blob/master/graphql/database/users/rewardReferringUser.js#L91
     referralRewardNotification:
-      '##### Congrats! You recruited a friend to help provide access to health care around the world.\n\n',
+      '##### Congrats! You recruited a friend to help fight world hunger just by opening tabs.\n\n',
     // Note: this is not used until we generalize referral incentives.
     // @feature/generalize-referral-reward
-    referralRewardTitle: '#### You just moved global health forward!\n\n',
+    referralRewardTitle: '#### You advanced the fight against hunger\n\n',
     referralRewardSubtitle:
-      "##### Congratulations! You're making a huge impact to support the wellbeing of people around the world. Want to do more? Invite a few more friends!",
+      "##### Congratulations! You're making a huge difference for people experiencing severe hunger. Want to help even more people? Invite a few more friends!\n\n",
     impactWalkthroughText:
-      "##### When you do, you’ll help provide at-home healthcare visits to people in need. We'll track your progress at the top of the page:\n\n",
+      "##### When you do, you'll provide a ready-to-use therapeutic food packet to someone experiencing severe hunger. We'll track how many packs you've provided at the top of the page:\n\n",
     // @feature/generalize-referral-reward
     // Note: this is not used until we generalize referral incentives.
     newlyReferredImpactWalkthroughText:
-      "##### Your friend has started you off with some impact! Open a new tab now to help provide at-home healthcare visits to people in need. We'll track your progress at the top of the page:\n\n",
+      "##### Open a new tab now to provide life-saving nutrition. We'll track how many ready-to-use therapeutic food packets you’ve provided at the top of the page:\n\n",
     confirmImpactSubtitle:
-      "##### Each time you open a tab, you'll be helping provide at-home healthcare visits through Partners In Health. Ready to get started?\n\n",
+      "##### Each time you open a tab, you'll be helping provide lifesaving nutrition to people experiencing severe hunger. Ready to get started?\n\n",
     // walkMeGif: undefined,
   },
   onboarding: {
@@ -47,50 +47,50 @@ const data = {
       {
         title: '### Your tabs are doing great things',
         subtitle:
-          'Now, every tab you open helps train and support community healthcare workers to provide at-home visits.\n\nTabbers like you are supporting critical nonprofit work all around the world. Thank you!\n\n',
-        imgName: 'globalHealth/onboarding1.svg',
+          'Now, every tab you open supports initiatives to help end world hunger.\n\nTabbers like you are supporting critical nonprofit work and saving lives. Thank you!\n\n',
+        imgName: 'endingHunger/onboarding1.svg',
       },
       {
         title: '### Do more with your squad',
         subtitle:
-          'With your squad, help even more people access safe health care!\n\nYou and your friends can team up to do more good.\n\n',
-        imgName: 'globalHealth/onboarding2.svg',
+          'Provide emergency nutrition even faster with a squad.\n\nYou and your friends can team up to do more good, together.\n\n',
+        imgName: 'endingHunger/onboarding2.svg',
       },
       {
         title: "### It doesn't cost you a thing",
         subtitle:
           "Ads on the new tab page raise money that we give to nonprofits. Most ads aren't good—but these ones are :)\n\n",
-        imgName: 'globalHealth/onboarding3.svg',
+        imgName: 'endingHunger/onboarding3.svg',
       },
     ],
   },
   sharing: {
-    title: '### **Get a friend on board**',
+    title: '#### **Get a friend on board to do more good**',
     subtitle:
       '##### Everyone can and _should_ make a difference: invite a friend to join in.\n\n',
-    shareImage: 'globalHealth/sharing.svg',
-    sentImage: 'globalHealth/sharing.svg',
-    imgCategory: 'global-health',
+    shareImage: 'endingHunger/sharing.svg',
+    sentImage: 'endingHunger/sharing.svg',
+    imgCategory: 'ending-hunger',
     email: {
       image:
-        'https://prod-tab2017-media.gladly.io/img/cause/globalHealth/emailInvite.jpg',
+        'https://prod-tab2017-media.gladly.io/img/cause/endingHunger/emailInvite.jpg',
       title:
-        'With Tab for Global Health, supporting healthcare access has never been easier.',
+        'With Tab for Ending Hunger, treating & preventing malnutrition can be part of our daily lives.',
       about:
-        '<div>Tab for Global Health turns your web browser into a force for good. With each tab opened, ad revenue supports community healthcare training through Partners In Health. This training helps ensure the people in need of care will receive it, long term.</div>\n\n',
+        '<div>Tab for Ending Hunger turns your web browser into a force for good. With each tab opened, ad revenue supports communities in need of life-saving nutrition. These resources help ensure people facing hunger can be treated and prevent health issues, long-term.</div>\n\n',
       faq:
-        'We’re partnered with Partners In Health to ensure the money raised will directly support their mission of providing high-quality health care globally to those who need it.',
+        'We’re partnered with Action Against Hunger to save lives by eliminating hunger through the prevention, detection and treatment of malnutrition.',
       sendgridEmailTemplateId: 'd-c925270aca954ff3b4c05f436cb52b54',
     },
     sendgridEmailTemplateId: 'd-c925270aca954ff3b4c05f436cb52b54',
     facebookButtonTitle:
-      'People around the world are struggling to access safe and reliable healthcare, but this is a problem we can all help solve. Now, with Tab for Global Health, every tab I open helps raise money for Partners In Health to train community healthcare workers. Check it out - it’s free!',
+      'Severe hunger is an issue being felt around the world, but it is a problem we can all help solve. Now, with Tab for Ending Hunger every tab I open helps raise money for Action Against Hunger to provide emergency nutrition to those in need. Check it out - it’s free!',
     twitterButtonTitle:
-      'People around the world are struggling to access quality health care, but this is a problem we can solve. Now, with Tab for Global Health, every tab I open raises money for Partners In Health to train community healthcare workers. Check it out - it’s free!',
-    redditButtonTitle: 'Help provide quality healthcare around the world',
-    tumblrTitle: 'Help provide quality healthcare around the world ',
+      'Severe hunger is an issue being felt around the world, but it is a problem we can solve. Now, with Tab for Ending Hunger, every tab I open helps raise money for Action Against Hunger to provide emergency nutrition to those in need. Check it out - it’s free!',
+    redditButtonTitle: 'Help end hunger around the world',
+    tumblrTitle: 'Help end hunger around the world',
     tumblrCaption:
-      'Every time I open a new tab with Tab for Global Health I am raising money to train healthcare workers through Partners In Health. Join today for free! ',
+      'Every time I open a new tab with Tab for Ending Hunger I am raising money to distribute life-saving nutrition to those in need through Action Against Hunger. Join today for free!',
   },
   // TODO
   squads: {
