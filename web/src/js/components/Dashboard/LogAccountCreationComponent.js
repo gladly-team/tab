51385bad1fbf38ef6c38c16eaf263dc0c06aeb12
Kevin Jennison <kevin.jennison1@gmail.com>
2022-03-09 11:09:05 -0500

    Set the user ID in a custom GA property

--- web/src/js/components/Dashboard/LogAccountCreationComponent.js
+++ web/src/js/components/Dashboard/LogAccountCreationComponent.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import gtag from 'js/analytics/google-analytics'
 
 import { accountCreated } from 'js/analytics/logEvent'
 
@@ -7,11 +8,25 @@ class LogAccountCreation extends React.Component {
   componentDidMount() {
     const { user } = this.props
 
+    // Set Google Analytics values that will be included on all events.
+    // https://developers.google.com/tag-platform/gtagjs/reference#set
+    // https://support.google.com/analytics/answer/11396839
+    // This is a quick & dirty implementation to match v4:
+    // https://github.com/gladly-team/tab-web/pull/302
+    // Really, we'd want to hoist this up
+    if (user) {
+      gtag('set', 'user_properties', {
+        ...(user.id && {
+          tfac_user_id: user.id,
+        }),
+      })
+    }
+
     // If this is the user's first tab, log the "accountCreated"
     // analytics event. It's a little inefficient to fetch the user's
     // lifetime tabs count just for this purpose, so we may eventually
     // want to move this analytics event to another trigger.
-    if (user.tabs === 0) {
+    if (user && user.tabs === 0) {
       accountCreated()
     }
   }
--- web/src/js/components/Dashboard/__tests__/LogAccountCreationComponent.test.js
+++ web/src/js/components/Dashboard/__tests__/LogAccountCreationComponent.test.js
@@ -2,10 +2,11 @@
 
 import React from 'react'
 import { shallow } from 'enzyme'
-
+import gtag from 'js/analytics/google-analytics'
 import { accountCreated } from 'js/analytics/logEvent'
 
 jest.mock('js/analytics/logEvent')
+jest.mock('js/analytics/google-analytics')
 
 afterEach(() => {
   jest.clearAllMocks()
@@ -30,5 +31,29 @@ describe('LogAccountCreation component', function() {
     }
     shallow(<LogAccountCreationComponent user={mockUserData} />)
     expect(accountCreated).not.toHaveBeenCalled()
+  })
+
+  it('sets the Google Analytics user ID property when one exists', function() {
+    const LogAccountCreationComponent = require('js/components/Dashboard/LogAccountCreationComponent')
+      .default
+    const mockUserData = {
+      id: 'abc-123',
+      tabs: 1,
+    }
+    shallow(<LogAccountCreationComponent user={mockUserData} />)
+    expect(gtag).toHaveBeenCalledWith('set', 'user_properties', {
+      tfac_user_id: 'abc-123',
+    })
+  })
+
+  it('does not set the Google Analytics user ID property when the user is not defined', function() {
+    const LogAccountCreationComponent = require('js/components/Dashboard/LogAccountCreationComponent')
+      .default
+
+    // Suppress expected error.
+    jest.spyOn(console, 'error').mockImplementationOnce(() => {})
+    const mockUserData = undefined
+    shallow(<LogAccountCreationComponent user={mockUserData} />)
+    expect(gtag).not.toHaveBeenCalled()
   })
 })
