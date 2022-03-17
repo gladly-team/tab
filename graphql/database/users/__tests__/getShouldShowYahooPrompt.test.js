0e43aa63e4010820f984e24b7aa31485b5105474
Jedi Master <jedi@alkimiya.io>
2022-03-17 17:12:17 -0400

    Fixing some more Logic Issues

--- graphql/database/users/__tests__/getShouldShowYahooPrompt.test.js
+++ graphql/database/users/__tests__/getShouldShowYahooPrompt.test.js
@@ -10,6 +10,25 @@ const userContext = getMockUserContext()
 const user = getMockUserInstance()
 
 describe('shouldShowYahooPrompt tests', () => {
+  it('returns false if user has seen yahoo prompt but is in experiment', async () => {
+    expect.assertions(1)
+    const getShouldShowYahooPrompt = require('../getShouldShowYahooPrompt')
+      .default
+    getUserFeature.mockResolvedValueOnce(
+      new Feature({
+        featureName: YAHOO_SEARCH_EXISTING_USERS,
+        variation: false,
+      })
+    )
+    const seenPromptUser = {
+      ...user,
+      yahooSwitchSearchPrompt: true,
+    }
+
+    const result = await getShouldShowYahooPrompt(userContext, seenPromptUser)
+    expect(result).toEqual(false)
+  })
+
   it('returns false if user has seen yahoo prompt', async () => {
     expect.assertions(1)
     const getShouldShowYahooPrompt = require('../getShouldShowYahooPrompt')
@@ -26,6 +45,36 @@ describe('shouldShowYahooPrompt tests', () => {
     }
 
     const result = await getShouldShowYahooPrompt(userContext, seenPromptUser)
+    expect(result).toEqual(false)
+  })
+
+  it('returns true if users feature is true for test', async () => {
+    expect.assertions(1)
+    const getShouldShowYahooPrompt = require('../getShouldShowYahooPrompt')
+      .default
+    getUserFeature.mockResolvedValueOnce(
+      new Feature({
+        featureName: YAHOO_SEARCH_EXISTING_USERS,
+        variation: true,
+      })
+    )
+
+    const result = await getShouldShowYahooPrompt(userContext, user)
+    expect(result).toEqual(true)
+  })
+
+  it('returns false if users feature is true for test', async () => {
+    expect.assertions(1)
+    const getShouldShowYahooPrompt = require('../getShouldShowYahooPrompt')
+      .default
+    getUserFeature.mockResolvedValueOnce(
+      new Feature({
+        featureName: YAHOO_SEARCH_EXISTING_USERS,
+        variation: false,
+      })
+    )
+
+    const result = await getShouldShowYahooPrompt(userContext, user)
     expect(result).toEqual(false)
   })
 })
