03dac650c7ea699fab3714bea404e4a06cc02a84
Kevin Jennison <kevin.jennison1@gmail.com>
2022-03-18 11:44:41 -0400

    Route to different search provider for /search/ vs /search/v2

--- lambda/src/search-app-lambda-edge/search-app-lambda-edge.js
+++ lambda/src/search-app-lambda-edge/search-app-lambda-edge.js
@@ -1,6 +1,7 @@
 /* eslint prefer-destructuring: 0 */
 
-const querystring = require('querystring')
+import { get } from 'lodash/object'
+import searchURLByRegion from './searchURLByRegion'
 
 // Rewrites URIs from /search* to another search provider.
 // Examples of Lambda@Edge functions:
@@ -10,28 +11,35 @@ const querystring = require('querystring')
 exports.handler = (event, context, callback) => {
   const request = event.Records[0].cf.request
   const SFAC_QUERY_QS_KEY = 'q'
+  const params = new URLSearchParams(request.querystring.toLowerCase())
+  const searchQueryVal = params.get(SFAC_QUERY_QS_KEY)
 
   // Map our search URL onto another search provider's URL.
-
-  // TODO: if request path starts with /search/v2, use Yahoo partner.
   // For v2, use Yahoo for search results.
-  // Yahoo localization needs access to headers:
-  // * Accept-Language
-  // * CloudFront-Viewer-Country
+  let searchBaseURL
+  let searchProviderQueryKey
+  if (get(event, 'Records[0].cf.request.uri', '').startsWith('/search/v2')) {
+    searchProviderQueryKey = 'p'
+    // Yahoo localization needs access to headers:
+    // * Accept-Language
+    // * CloudFront-Viewer-Country
+    const headers = get(request, 'Records[0].cf.request.headers', {})
+    const countryHeader = get(headers, 'cloudfront-viewer-country.value')
+    const acceptLanguageHeader = get(headers, 'accept-language.value')
+    searchBaseURL = searchURLByRegion(countryHeader, acceptLanguageHeader)
+  } else {
+    // For v1, use Google for search results.
+    searchProviderQueryKey = 'q'
+    searchBaseURL = 'https://www.google.com/search'
+  }
 
-  // const SEARCH_PROVIDER_URL = 'TODO'
-  // const SEARCH_PROVIDER_QS_KEY = 'p'
+  // Construct the final search URL with the search query.
+  const baseURL = new URL(searchBaseURL)
+  if (searchQueryVal) {
+    baseURL.searchParams.set(searchProviderQueryKey, searchQueryVal)
+  }
+  const redirectURL = baseURL.href
 
-  // For v1, use Google for search results.
-  const SEARCH_PROVIDER_URL = 'https://www.google.com/search'
-  const SEARCH_PROVIDER_QS_KEY = 'q'
-  const params = querystring.parse(request.querystring.toLowerCase())
-  const searchQueryVal = params[SFAC_QUERY_QS_KEY]
-  const redirectURL = searchQueryVal
-    ? `${SEARCH_PROVIDER_URL}?${querystring.stringify({
-        [SEARCH_PROVIDER_QS_KEY]: searchQueryVal,
-      })}`
-    : SEARCH_PROVIDER_URL
   const response = {
     status: '307',
     statusDescription: 'Found',
