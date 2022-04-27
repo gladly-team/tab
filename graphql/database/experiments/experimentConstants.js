2893f66c0b1ec53ee44ade04f62d300bec4e5b65
Kevin Jennison <kevin.jennison1@gmail.com>
2022-04-27 11:59:17 -0400

    Use experiment name for internal overrides key

--- graphql/database/experiments/experimentConstants.js
+++ graphql/database/experiments/experimentConstants.js
@@ -1,2 +1,4 @@
+export const MONEY_RAISED_EXCLAMATION_POINT_V2 =
+  'money-raised-exclamation-point-v2'
 export const YAHOO_SEARCH_NEW_USERS = 'yahoo-search-new-users'
 export const YAHOO_SEARCH_EXISTING_USERS = 'yahoo-search-existing-users'
--- graphql/database/experiments/features.js
+++ graphql/database/experiments/features.js
@@ -1,17 +1,18 @@
 import {
+  MONEY_RAISED_EXCLAMATION_POINT_V2,
   YAHOO_SEARCH_EXISTING_USERS,
   YAHOO_SEARCH_NEW_USERS,
 } from './experimentConstants'
 
 const features = {
-  'money-raised-exclamation-point-v2': {
+  [MONEY_RAISED_EXCLAMATION_POINT_V2]: {
     defaultValue: false,
     rules: [
       /* Begin internal overrides */
       {
         condition: {
           v4BetaEnabled: true,
-          'internalExperimentOverrides.money-raised-exclamation-point-v2': {
+          [`internalExperimentOverrides.${MONEY_RAISED_EXCLAMATION_POINT_V2}`]: {
             $eq: true,
             $exists: true,
           },
@@ -21,7 +22,7 @@ const features = {
       {
         condition: {
           v4BetaEnabled: true,
-          'internalExperimentOverrides.money-raised-exclamation-point-v2': {
+          [`internalExperimentOverrides.${MONEY_RAISED_EXCLAMATION_POINT_V2}`]: {
             $eq: false,
             $exists: true,
           },
@@ -39,72 +40,70 @@ const features = {
       },
     ],
   },
-}
-
-features[YAHOO_SEARCH_EXISTING_USERS] = {
-  defaultValue: false,
-  rules: [
-    /* Begin internal overrides */
-    {
-      condition: {
-        v4BetaEnabled: {
-          $eq: true,
+  [YAHOO_SEARCH_EXISTING_USERS]: {
+    defaultValue: false,
+    rules: [
+      /* Begin internal overrides */
+      {
+        condition: {
+          v4BetaEnabled: {
+            $eq: true,
+          },
+          [`internalExperimentOverrides.${YAHOO_SEARCH_EXISTING_USERS}`]: {
+            $eq: true,
+            $exists: true,
+          },
         },
-        'internalExperimentOverrides.search-existing-users': {
-          $eq: true,
-          $exists: true,
+        force: true,
+      },
+      {
+        condition: {
+          v4BetaEnabled: {
+            $eq: true,
+          },
+          [`internalExperimentOverrides.${YAHOO_SEARCH_EXISTING_USERS}`]: {
+            $eq: false,
+            $exists: true,
+          },
+        },
+        force: false,
+      },
+      /* End internal overrides */
+      {
+        variations: [false, true],
+        weights: [0.5, 0.5],
+        coverage: 0.3,
+        condition: {
+          joined: {
+            $lt: 1650726528502, // make a later date when we go to production.
+          },
+          v4BetaEnabled: {
+            $eq: true,
+          },
         },
       },
-      force: true,
-    },
-    {
-      condition: {
-        v4BetaEnabled: {
-          $eq: true,
-        },
-        'internalExperimentOverrides.search-existing-users': {
-          $eq: false,
-          $exists: true,
-        },
-      },
-      force: false,
-    },
-    /* End internal overrides */
-    {
-      variations: [false, true],
-      weights: [0.5, 0.5],
-      coverage: 0.3,
-      condition: {
-        joined: {
-          $lt: 1650726528502, // make a later date when we go to production.
-        },
-        v4BetaEnabled: {
-          $eq: true,
+    ],
+  },
+  [YAHOO_SEARCH_NEW_USERS]: {
+    defaultValue: 'Google',
+    rules: [
+      // Internal Overrides may not be useful here because we are assigning search engine on user creation.
+      /* End internal overrides */
+      {
+        variations: ['Google', 'SearchForACause'],
+        weights: [0.5, 0.5],
+        coverage: 1.0,
+        condition: {
+          joined: {
+            $gt: 1650726528502, // make a later date when we go to production.
+          },
+          v4BetaEnabled: {
+            $eq: true,
+          },
         },
       },
-    },
-  ],
-}
-
-features[YAHOO_SEARCH_NEW_USERS] = {
-  defaultValue: 'Google',
-  rules: [
-    // Internal Overrides may not be useful here because we are assigning search engine on user creation.
-    /* End internal overrides */
-    {
-      variations: ['Google', 'SearchForACause'],
-      weights: [0.5, 0.5],
-      coverage: 1.0,
-      condition: {
-        joined: {
-          $gt: 1650726528502, // make a later date when we go to production.
-        },
-        v4BetaEnabled: {
-          $eq: true,
-        },
-      },
-    },
-  ],
+    ],
+  },
 }
 
 export default features
