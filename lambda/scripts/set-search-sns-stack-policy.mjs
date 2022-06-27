53b0f85d19585ac2c4d8322922f65a202df80660
Kevin Jennison <kevin.jennison1@gmail.com>
2022-06-27 16:11:13 -0400

    Set restrictive stack policies for all stack instances

--- lambda/scripts/set-search-sns-stack-policy.mjs
+++ lambda/scripts/set-search-sns-stack-policy.mjs
@@ -1,14 +1,26 @@
 // Call with Node v13+:
 // AWS_PROFILE=some-admin-profile node scripts/set-search-sns-stack-policy.mjs
 
+/* eslint no-console: 0, import/no-extraneous-dependencies: 0 */
+
 import AWS from 'aws-sdk'
 import inquirer from 'inquirer'
+
+const stackPolicies = {
+  DENY_ALL:
+    '{"Statement":[{"Effect":"Deny","Principal":"*","Action":"Update:*","Resource":"*"}]}',
+  ALLOW_MODIFY:
+    '{"Statement":[{"Effect":"Allow","Principal":"*","Action":"Update:Modify","Resource":"*"}]}',
+}
+
+// Edit this to modify the policy you want to use.
+const STACK_POLICY = stackPolicies.DENY_ALL
 
 const setUpCreds = async () => {
   // Support MFA:
   // https://github.com/aws/aws-sdk-js/pull/2126#issue-199310808
   const tokenCodeFn = (serial, cb) => {
-    const resp = inquirer
+    inquirer
       .prompt({
         name: 'token',
         type: 'input',
@@ -29,22 +41,75 @@ const setUpCreds = async () => {
   try {
     await creds.getPromise()
     AWS.config.credentials = creds
+    AWS.config.update({ region: 'us-west-2' })
   } catch (e) {
     console.error('MFA credentials error:', e)
   }
 }
 
-await setUpCreds()
-
-// TODO
-const main = async () => {
-  const s3 = new AWS.S3()
+const setStackPolicy = async () => {
+  const cloudformation = new AWS.CloudFormation()
+  let stackInstanceData
   try {
-    const data = await s3.listBuckets({}).promise()
-    console.log(data)
+    stackInstanceData = await cloudformation
+      .listStackInstances({
+        StackSetName: 'search-edge-request',
+      })
+      .promise()
   } catch (e) {
-    console.error(e)
+    throw e
   }
+
+  // AWS doesn't seem to return a stack name when usng listStackInstances.
+  // Infer the stack names from stack IDs. Unsure if this is the best
+  // approach but seems to work for stack sets.
+  const getStackNameFromId = stackId => {
+    return stackId.split('/')[1]
+  }
+
+  const stackInstances = stackInstanceData.Summaries.map(stackInstance => ({
+    stackId: stackInstance.StackId,
+    stackName: getStackNameFromId(stackInstance.StackId),
+    region: stackInstance.Region,
+  }))
+  // console.log('=====')
+  // console.log('All stack instance data:', stackInstanceData)
+  // console.log('=====')
+  // console.log('Stack instances:', stackInstances)
+  // console.log('=====')
+
+  // Update stack policies for all stack instances.
+  await Promise.all(
+    stackInstances.map(instance => {
+      AWS.config.update({ region: instance.region })
+      const cloudformationRegion = new AWS.CloudFormation()
+      return cloudformationRegion
+        .setStackPolicy({
+          StackName: instance.stackName,
+          StackPolicyBody: STACK_POLICY,
+        })
+        .promise()
+        .then(() => {
+          console.log(
+            `Successfully updated stack policy for ${instance.region}.`
+          )
+        })
+        .catch(e => {
+          console.error(
+            `*** Failed to update stack policy for ${instance.region}.***`,
+            e
+          )
+        })
+    })
+  )
 }
 
-await main()
+const main = async () => {
+  await setUpCreds()
+  await setStackPolicy()
+  console.log('Done!')
+}
+
+;(async () => {
+  await main()
+})()
