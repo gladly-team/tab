d88805ea093efeccbc97098a75208ea8f02d95c8
Kevin Jennison <kevin.jennison1@gmail.com>
2022-06-28 14:16:19 -0400

    Clean up some comments

--- lambda/src/search-request-logger/search-request-logger.mjs
+++ lambda/src/search-request-logger/search-request-logger.mjs
@@ -2,9 +2,14 @@ import { get } from 'lodash/object'
 
 exports.handler = async event => {
   const message = get(event, 'Records[0].Sns.Message')
-  // TODO: remove when debugging
+
+  // TODO: Call the GraphQL service to log search. Use the appropriate
+  //   endpoint for dev vs prod deployments.
+
+  // TODO: remove when finished debugging
   // eslint-disable-next-line no-console
   console.log(message)
+
   return {
     some: 'stuff',
   }
