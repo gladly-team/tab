7ecd0ee2dbf291177b4ed2d6bbb0d4db0375dc81
Kevin Jennison <kevin.jennison1@gmail.com>
2022-06-28 11:00:15 -0400

    Log an SNS message

--- lambda/src/search-request-logger/__tests__/search-request-logger.test.mjs
+++ lambda/src/search-request-logger/__tests__/search-request-logger.test.mjs
@@ -1,19 +1,38 @@
 /* eslint-env jest */
 
+import { getMockSNSEventObject } from '../../utils/lambda-arg-utils'
+
 beforeEach(() => {
   jest.clearAllMocks()
 })
 
+const setSNSEventMessage = (event, message) => {
+  const newRecords = event.Records.map(rec => {
+    return {
+      ...rec,
+      Sns: {
+        ...rec.Sns,
+        Message: message,
+      },
+    }
+  })
+  return {
+    ...event,
+    Records: newRecords,
+  }
+}
+
 describe('Search request logger', () => {
-  it('calls console.log', async () => {
+  it('calls console.log with the SNS message', async () => {
     expect.assertions(1)
     const consoleLogSpy = jest
       .spyOn(console, 'log')
       .mockImplementationOnce(() => {})
+    const mockEvent = setSNSEventMessage(getMockSNSEventObject(), 'Hello world')
     const { handler } = require('../search-request-logger.mjs')
-    await handler()
+    await handler(mockEvent)
 
     // eslint-disable-next-line no-console
-    expect(consoleLogSpy).toHaveBeenCalled()
+    expect(consoleLogSpy).toHaveBeenLastCalledWith('Hello world')
   })
 })
--- lambda/src/search-request-logger/search-request-logger.mjs
+++ lambda/src/search-request-logger/search-request-logger.mjs
@@ -1,7 +1,10 @@
+import { get } from 'lodash/object'
+
 exports.handler = async event => {
+  const message = get(event, 'Records[0].Sns.Message')
   // TODO: remove when debugging
   // eslint-disable-next-line no-console
-  console.log('hi', event)
+  console.log(message)
   return {
     some: 'stuff',
   }
