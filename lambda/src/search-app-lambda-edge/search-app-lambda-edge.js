bb636cae5bb505134c2d48a35974cb73f8569d3e
Kevin Jennison <kevin.jennison1@gmail.com>
2022-06-28 16:37:47 -0400

    WIP - publish to SNS from edge function

--- lambda/src/search-app-lambda-edge/search-app-lambda-edge.js
+++ lambda/src/search-app-lambda-edge/search-app-lambda-edge.js
@@ -2,15 +2,37 @@
 // (but not `/search/api*`).
 /* eslint prefer-destructuring: 0 */
 
+import AWS from 'aws-sdk'
 import { get } from 'lodash/object'
 import searchURLByRegion from './searchURLByRegion'
+
+const PRODUCTION_STAGE = 'prod'
+
+const publishToSNS = async ({ stage, messageData }) => {
+  // Get the SNS topic ARN. Example:
+  //   "arn:aws:sns:eu-west-3:167811431063:dev-SearchRequest"
+  const awsRegion = process.env.AWS_REGION
+  const awsAccountId = '167811431063'
+  const snsTopicName = 'SearchRequest'
+  const snsTopicNamePrefix = stage === PRODUCTION_STAGE ? '' : 'dev-'
+  const snsTopicARN = `arn:aws:sns:${awsRegion}:${awsAccountId}:${snsTopicNamePrefix}${snsTopicName}`
+
+  // Publish.
+  const message = JSON.stringify(messageData)
+  const sns = new AWS.SNS()
+  const params = {
+    Message: message,
+    TopicArn: snsTopicARN,
+  }
+  await sns.publish(params).promise()
+}
 
 // Rewrites URIs from /search* to another search provider.
 // Examples of Lambda@Edge functions:
 // https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/lambda-examples.html#lambda-examples-general-examples
 // CloudFront event object:
 // https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/lambda-event-structure.html
-exports.handler = (event, context, callback) => {
+exports.handler = async event => {
   const request = get(event, 'Records[0].cf.request')
   const SFAC_QUERY_QS_KEY = 'q'
   const params = new URLSearchParams(request.querystring.toLowerCase())
@@ -28,7 +50,22 @@ exports.handler = (event, context, callback) => {
   // Get the redirect destination URL.
   let searchBaseURL
   let searchProviderQueryKey
-  if (get(event, 'Records[0].cf.request.uri', '').startsWith('/search/v2')) {
+  let searchSrc = null
+  let causeId = null
+
+  // Get the function "version", which we use in manual QA.
+  const uri = get(event, 'Records[0].cf.request.uri', '')
+  const defaultVersion = 1 // Bump this to "roll out" a new version
+  let version
+  if (uri.startsWith('/search/v2')) {
+    version = 2
+  } else if (uri.startsWith('/search/v3')) {
+    version = 3
+  } else {
+    version = defaultVersion
+  }
+
+  if (version >= 2) {
     // For v2, use Yahoo for search results.
     searchProviderQueryKey = 'p'
 
@@ -45,10 +82,12 @@ exports.handler = (event, context, callback) => {
     // Yahoo does not allow using hyphens.
     //   e.g.: type=src_tab.c_CA6A5C2uj.r_482
     try {
-      const searchSrc = params.get('src') || 'none'
-      const causeId = params.get('c') || 'none'
-      const referralId = params.get('r') || 'none'
-      const typeParamVal = `src_${searchSrc}.c_${causeId}.r_${referralId}`
+      searchSrc = params.get('src') || null
+      causeId = params.get('c') || null
+      const referralId = params.get('r') || null
+      const NONE = 'none'
+      const typeParamVal = `src_${searchSrc || NONE}.c_${causeId ||
+        NONE}.r_${referralId || NONE}`
       const url = new URL(yahooBaseURL)
       url.searchParams.set('type', typeParamVal)
       searchBaseURL = url.href
@@ -71,6 +110,28 @@ exports.handler = (event, context, callback) => {
   }
   const redirectURL = baseURL.href
 
+  // Publish the search request event to SNS.
+  if (version >= 3) {
+    try {
+      const searchEngine = 'SearchForACause' // TODO: get from URL param later
+      const messageData = {
+        user: {
+          idToken: null, // TODO: get from cookie
+        },
+        data: {
+          src: searchSrc,
+          engine: searchEngine,
+          causeId,
+        },
+      }
+      await publishToSNS({ stage, messageData })
+    } catch (e) {
+      // TODO: add Sentry error logging.
+      // eslint-disable-next-line no-console
+      console.error(e)
+    }
+  }
+
   const response = {
     status: '307',
     statusDescription: 'Found',
@@ -89,5 +150,5 @@ exports.handler = (event, context, callback) => {
       ],
     },
   }
-  callback(null, response)
+  return response
 }
