4a8f80860e9bd3d4d56d5a1c78ef8e0222033af3
Jed Tan <jed@tabforacause.org>
2022-08-12 17:33:54 -0400

    UserEventLog Model and Table Setup

---
+++ graphql/database/logs/UserEventLogModel.js
@@ -0,0 +1,52 @@
+import DynamoDBModel from '../base/DynamoDBModel'
+import types from '../fieldTypes'
+import tableNames from '../tables'
+import { permissionAuthorizers } from '../../utils/authorization-helpers'
+import { VALID_LOG_SCHEMAS, VALID_LOG_TYPES } from './logTypes'
+import { USER_EVENT_LOG } from '../constants'
+
+/*
+ * @extends DynamoDBModel
+ */
+class UserEventLogModel extends DynamoDBModel {
+  static get name() {
+    return USER_EVENT_LOG
+  }
+
+  static get hashKey() {
+    return 'userId'
+  }
+
+  static get rangeKey() {
+    return 'timestamp'
+  }
+
+  static get tableName() {
+    return tableNames.userEventLog
+  }
+
+  static get schema() {
+    return {
+      userId: types.string().required(),
+      type: types
+        .string()
+        .valid(VALID_LOG_TYPES)
+        .required(),
+      timestamp: types
+        .string()
+        .isoDate()
+        .required(),
+      eventData: types.object().valid(VALID_LOG_SCHEMAS),
+    }
+  }
+
+  static get permissions() {
+    return {
+      create: permissionAuthorizers.userIdMatchesHashKey,
+    }
+  }
+}
+
+UserEventLogModel.register()
+
+export default UserEventLogModel
