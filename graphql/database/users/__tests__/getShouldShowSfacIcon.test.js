032bf66e2ea851c8101aa9e3477448f1fe3fe7dd
Jed Tan <jed@tabforacause.org>
2022-09-11 23:46:40 -0400

    Opening Up Access for SFAC Activity UI Element

--- graphql/database/users/__tests__/getShouldShowSfacIcon.test.js
+++ graphql/database/users/__tests__/getShouldShowSfacIcon.test.js
@@ -14,50 +14,6 @@ beforeEach(() => {
 
 describe('getShouldShowSfacIcon tests', () => {
   it('returns false if the user is in the experiment but it is not dev', async () => {
-    jest.doMock('../../../config', () => ({
-      GROWTHBOOK_ENV: 'production',
-      DB_TABLE_NAME_APPENDIX: '',
-    }))
-    expect.assertions(1)
-
-    const getUserFeature = require('../../experiments/getUserFeature').default
-    getUserFeature.mockResolvedValueOnce(
-      new Feature({
-        featureName: SFAC_EXTENSION_PROMPT,
-        variation: 'Notification',
-      })
-    )
-
-    const getShouldShowSfacIcon = require('../getShouldShowSfacIcon').default
-    const result = await getShouldShowSfacIcon(userContext, user)
-    expect(result).toEqual(false)
-  })
-
-  it('returns false if the user is in not the experiment but it is dev', async () => {
-    jest.doMock('../../../config', () => ({
-      GROWTHBOOK_ENV: 'dev',
-      DB_TABLE_NAME_APPENDIX: '',
-    }))
-    expect.assertions(1)
-
-    const getUserFeature = require('../../experiments/getUserFeature').default
-    getUserFeature.mockResolvedValueOnce(
-      new Feature({
-        featureName: SFAC_EXTENSION_PROMPT,
-        variation: 'Control',
-      })
-    )
-
-    const getShouldShowSfacIcon = require('../getShouldShowSfacIcon').default
-    const result = await getShouldShowSfacIcon(userContext, user)
-    expect(result).toEqual(false)
-  })
-
-  it('returns true if the user is in the experiment and env is dev', async () => {
-    jest.doMock('../../../config', () => ({
-      GROWTHBOOK_ENV: 'dev',
-      DB_TABLE_NAME_APPENDIX: '',
-    }))
     expect.assertions(1)
 
     const getUserFeature = require('../../experiments/getUserFeature').default
@@ -71,5 +27,21 @@ describe('getShouldShowSfacIcon tests', () => {
     const getShouldShowSfacIcon = require('../getShouldShowSfacIcon').default
     const result = await getShouldShowSfacIcon(userContext, user)
     expect(result).toEqual(true)
+  })
+
+  it('returns false if the user is in not the experiment but it is dev', async () => {
+    expect.assertions(1)
+
+    const getUserFeature = require('../../experiments/getUserFeature').default
+    getUserFeature.mockResolvedValueOnce(
+      new Feature({
+        featureName: SFAC_EXTENSION_PROMPT,
+        variation: 'Control',
+      })
+    )
+
+    const getShouldShowSfacIcon = require('../getShouldShowSfacIcon').default
+    const result = await getShouldShowSfacIcon(userContext, user)
+    expect(result).toEqual(false)
   })
 })
