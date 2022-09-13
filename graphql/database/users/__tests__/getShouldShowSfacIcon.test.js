ed3f4362bbac838844a82b54d90391b961b5c6f6
Kevin Jennison <kevin.jennison1@gmail.com>
2022-09-13 17:01:25 -0400

    Add some tests

--- graphql/database/users/__tests__/getShouldShowSfacIcon.test.js
+++ graphql/database/users/__tests__/getShouldShowSfacIcon.test.js
@@ -116,4 +116,54 @@ describe('getShouldShowSfacIcon tests', () => {
     const result = await getShouldShowSfacIcon(userContext, user)
     expect(result).toEqual(true)
   })
+
+  it('returns true if the user is in the control group for the "new user SFAC notification" experiment but in the treatment group for the "existing user activity icon" experiment', async () => {
+    expect.assertions(1)
+
+    const getUserFeature = require('../../experiments/getUserFeature').default
+    getUserFeature.mockImplementation((_userContext, _user, featureName) => {
+      if (featureName === SFAC_EXTENSION_PROMPT) {
+        return new Feature({
+          featureName: SFAC_EXTENSION_PROMPT,
+          variation: 'Control',
+        })
+      }
+      if (featureName === SFAC_EXISTING_USER_ACTIVITY_ICON) {
+        return new Feature({
+          featureName: SFAC_EXISTING_USER_ACTIVITY_ICON,
+          variation: 'Icon',
+        })
+      }
+      return new Feature()
+    })
+
+    const getShouldShowSfacIcon = require('../getShouldShowSfacIcon').default
+    const result = await getShouldShowSfacIcon(userContext, user)
+    expect(result).toEqual(true)
+  })
+
+  it('returns false if the user is in the control group for both experiments', async () => {
+    expect.assertions(1)
+
+    const getUserFeature = require('../../experiments/getUserFeature').default
+    getUserFeature.mockImplementation((_userContext, _user, featureName) => {
+      if (featureName === SFAC_EXTENSION_PROMPT) {
+        return new Feature({
+          featureName: SFAC_EXTENSION_PROMPT,
+          variation: 'Control',
+        })
+      }
+      if (featureName === SFAC_EXISTING_USER_ACTIVITY_ICON) {
+        return new Feature({
+          featureName: SFAC_EXISTING_USER_ACTIVITY_ICON,
+          variation: 'Control',
+        })
+      }
+      return new Feature()
+    })
+
+    const getShouldShowSfacIcon = require('../getShouldShowSfacIcon').default
+    const result = await getShouldShowSfacIcon(userContext, user)
+    expect(result).toEqual(false)
+  })
 })
