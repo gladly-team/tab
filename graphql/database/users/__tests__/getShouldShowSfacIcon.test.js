76326a01ff6468746821ddbadd5fba2a46600e17
Kevin Jennison <kevin.jennison1@gmail.com>
2022-09-13 15:23:40 -0400

    Support multiple features in test mocks

--- graphql/database/users/__tests__/getShouldShowSfacIcon.test.js
+++ graphql/database/users/__tests__/getShouldShowSfacIcon.test.js
@@ -17,12 +17,15 @@ describe('getShouldShowSfacIcon tests', () => {
     expect.assertions(1)
 
     const getUserFeature = require('../../experiments/getUserFeature').default
-    getUserFeature.mockResolvedValueOnce(
-      new Feature({
-        featureName: SFAC_EXTENSION_PROMPT,
-        variation: 'Notification',
-      })
-    )
+    getUserFeature.mockImplementation((_userContext, _user, featureName) => {
+      if (featureName === SFAC_EXTENSION_PROMPT) {
+        return new Feature({
+          featureName: SFAC_EXTENSION_PROMPT,
+          variation: 'Notification',
+        })
+      }
+      return new Feature()
+    })
 
     const getShouldShowSfacIcon = require('../getShouldShowSfacIcon').default
     const result = await getShouldShowSfacIcon(userContext, user)
@@ -33,12 +36,15 @@ describe('getShouldShowSfacIcon tests', () => {
     expect.assertions(1)
 
     const getUserFeature = require('../../experiments/getUserFeature').default
-    getUserFeature.mockResolvedValueOnce(
-      new Feature({
-        featureName: SFAC_EXTENSION_PROMPT,
-        variation: 'Control',
-      })
-    )
+    getUserFeature.mockImplementation((_userContext, _user, featureName) => {
+      if (featureName === SFAC_EXTENSION_PROMPT) {
+        return new Feature({
+          featureName: SFAC_EXTENSION_PROMPT,
+          variation: 'Control',
+        })
+      }
+      return new Feature()
+    })
 
     const getShouldShowSfacIcon = require('../getShouldShowSfacIcon').default
     const result = await getShouldShowSfacIcon(userContext, user)
