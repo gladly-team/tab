fbce8c91ee7e312fbf20a4ebf72af732b472ed9a
Kevin Jennison <kevin.jennison1@gmail.com>
2022-12-20 18:59:11 -0500

    WIP - fixing updateSchema

--- graphql/scripts/updateSchema.js
+++ graphql/scripts/updateSchema.js
@@ -1,34 +1,29 @@
 /* eslint no-console: 0 */
-import fs from 'fs'
-import path from 'path'
-import { graphql } from 'graphql'
-import { introspectionQuery, printSchema } from 'graphql/utilities'
+// import fs from 'fs'
+// import path from 'path'
+import { validateSchema } from 'graphql'
+import { printSchema } from 'graphql/utilities'
 
 // Save JSON of full schema introspection for Babel Relay Plugin to use
 import { Schema } from '../data/schema'
 
-// eslint-disable-next-line
-async function updateSchema() {
+function updateSchema() {
   console.log('Updating schema...')
-  const result = await graphql(Schema, introspectionQuery)
-  if (result.errors) {
-    console.error(
-      'ERROR introspecting schema: ',
-      JSON.stringify(result.errors, null, 2)
-    )
-  } else {
-    fs.writeFileSync(
-      path.join(__dirname, '../data/schema.json'),
-      JSON.stringify(result, null, 2)
-    )
-    console.log('Schema updated.')
+  const errors = validateSchema(Schema)
+  if (errors.length) {
+    console.error('The GraphQL schema is invalid:')
+    console.error(errors)
+    return
   }
-}
+  console.log('The GraphQL schema is valid.')
 
-// Save user readable type system shorthand of schema
-fs.writeFileSync(
-  path.join(__dirname, '../data/schema.graphql'),
-  printSchema(Schema)
-)
+  // FIXME: fails with "TypeError: str.replace is not a function".
+  console.log(printSchema(Schema))
+
+  // fs.writeFileSync(
+  //   path.join(__dirname, '../data/schema.graphql'),
+  //   printSchema(Schema)
+  // )
+}
 
 updateSchema()
