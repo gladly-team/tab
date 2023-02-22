13626d6404996113dba94c9c544fe67c225c603f
Jed Tan <jed@tabforacause.org>
2023-02-21 22:26:48 -0800

    TFAC-1233 Adding additional Impact Metrics

--- graphql/database/groupImpact/impactMetricData.js
+++ graphql/database/groupImpact/impactMetricData.js
@@ -11,6 +11,42 @@ const data = [
       'Community health workers provide quality health care to those who might not otherwise have access.',
     active: false,
   },
+  {
+    id: 'mhwYA7KbK',
+    charityId: 'cb7ab7e4-bda6-4fdf-825a-30db05911705', // Partners in Health
+    dollarAmount: 60e6, // $60
+    impactTitle: 'Provide prenatal care for one woman',
+    metricTitle: 'prenatal care',
+    description:
+      'Provide prenatal care to one impoverished mother-to-be--and ensure her pregnancy stays safe.',
+    whyValuableDescription:
+      'This prenatal care helps ensure a safe pregnancy for an impoverished mother-to-be.',
+    active: false,
+  },
+  {
+    id: 'CwxR9zA5W',
+    charityId: 'cb7ab7e4-bda6-4fdf-825a-30db05911705', // Partners in Health
+    dollarAmount: 25e6, // $25
+    impactTitle: 'Provide an infant care package',
+    metricTitle: '1 newborn baby kit',
+    description:
+      'Not all mothers arrive at PIH facilities with newborn supplies. Help families return home in dignity by providing an infant care package, which includes: baby hat, socks, diapers, and bath essentials for the first six weeks of life.',
+    whyValuableDescription:
+      'This kit provides newborn essentials including hats, socks, diapers, and sanitation for the first 6 weeks, helping ensure a healthy start.',
+    active: false,
+  },
+  {
+    id: '3wHhDkuRR',
+    charityId: 'ea019270-1cda-411f-b41e-90406fbe15ee', // Action Against Hunger
+    dollarAmount: 50e6, // $50
+    impactTitle: 'Provide 1 child with an emergency nutrition course',
+    metricTitle: '1 malnutrition treatment',
+    description:
+      'This lifesaving course of nutrition saves children from debilitating and life-threatening malnutrition.',
+    whyValuableDescription:
+      'Hunger is predictable, preventable, and treatable. Yet, one in ten people suffer from hunger. This malnutrition treatment saves and improves lives of children around the world.',
+    active: false,
+  },
 ]
 
 export default data
