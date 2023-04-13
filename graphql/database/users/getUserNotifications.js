351f3c4f4799923c621506b280d3e53e31200bf5
Spicer Matthews <github@cloudmanic.com>
2023-04-13 10:45:52 -0700

    Added 3 different notifications for SHFAC

--- graphql/database/users/getUserNotifications.js
+++ graphql/database/users/getUserNotifications.js
@@ -9,13 +9,21 @@ import { SHFAC_NOTIFY_LAUNCH } from '../experiments/experimentConstants'
  * @return {Notifications[]} An array of notification data, or an empty
  *   array.
  */
-
 const getUserNotifications = async (userContext, user) => {
   let notifications = []
   try {
+    const notifFeature = await getUserFeature(
+      userContext,
+      user,
+      SHFAC_NOTIFY_LAUNCH
+    )
+    const enabled = notifFeature.variation !== 'None'
+    notifications = [
+      ...(enabled
+        ? [{ code: SHFAC_NOTIFY_LAUNCH, variation: notifFeature.variation }]
+        : []),
+    ]
     await getUserFeature(userContext, user, SHFAC_NOTIFY_LAUNCH)
-
-    notifications = [{ code: SHFAC_NOTIFY_LAUNCH, variation: 'Experiment' }]
   } catch (e) {
     logger.error(e)
   }
---
+++ web/src/js/components/Dashboard/ShfacExtensionSellNotification.js
@@ -0,0 +1,263 @@
+import React, { useCallback, useState, useEffect } from 'react'
+import Button from '@material-ui/core/Button'
+import PropTypes from 'prop-types'
+import Link from 'js/components/General/Link'
+import { Typography } from '@material-ui/core'
+// import CreateSfacExtensionPromptResponseMutation from 'js/mutations/CreateSfacExtensionPromptResponseMutation'
+// import awaitTimeLimit from 'js/utils/awaitTimeLimit'
+import {
+  getShopExtensionPage,
+  shopChromeExtensionPage,
+  replaceUrl,
+} from 'js/navigation/navigation'
+// import { AwaitedPromiseTimeout } from 'js/utils/errors'
+// import logger from 'js/utils/logger'
+import Notification from 'js/components/Dashboard/NotificationV2'
+import { withStyles } from '@material-ui/core/styles'
+import useDoesBrowserSupportSearchExtension from 'js/utils/hooks/useDoesBrowserSupportSearchExtension'
+import useBrowserName from 'js/utils/hooks/useBrowserName'
+
+const styles = theme => ({
+  noButton: {
+    height: '30px',
+    fontWeight: '500',
+    fontFamily: 'Poppins',
+  },
+  yesButton: {
+    background: '#29BEBA',
+    borderRadius: '15px',
+    height: '30px',
+    fontWeight: '900',
+    fontFamily: 'Poppins',
+    marginLeft: '6',
+    color: 'white',
+  },
+  title: {
+    fontWeight: '700',
+    fontSize: '24px',
+    fontFamily: 'Poppins',
+    paddingBottom: 8,
+  },
+  text: {
+    maxWidth: 350,
+  },
+  buttonsWrapper: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+  wrapper: {
+    maxWidth: '400px',
+    marginTop: '4px',
+  },
+  notification: {
+    padding: 12,
+  },
+  subtitle: {
+    paddingBottom: 16,
+  },
+})
+
+const ShfacExtensionSellNotification = ({
+  classes,
+  userId,
+  variation,
+  showShfacExtensionPrompt,
+}) => {
+  const [open, setOpen] = useState(false)
+  const [browser, setBrowser] = useState(null)
+  const browserName = useBrowserName()
+  const searchExtensionSupported = useDoesBrowserSupportSearchExtension()
+
+  useEffect(() => {
+    if (searchExtensionSupported) {
+      setOpen(showShfacExtensionPrompt)
+    }
+    setBrowser(browserName)
+  }, [searchExtensionSupported, showShfacExtensionPrompt, browserName])
+
+  const onYesClick = useCallback(async () => {
+    // // Log the search event but time-cap how long we wait to avoid a bad UX
+    // // if the request hangs.
+    // try {
+    //   const MS_TO_WAIT_FOR_LOG = 1500
+    //   await awaitTimeLimit(
+    //     CreateSfacExtensionPromptResponseMutation(userId, browser, true),
+    //     MS_TO_WAIT_FOR_LOG
+    //   )
+    // } catch (e) {
+    //   if (e.code !== AwaitedPromiseTimeout.code) {
+    //     logger.error(e)
+    //   }
+    // }
+    setOpen(false)
+
+    if (variation == 'Version1' || variation == 'Version3') {
+      replaceUrl(getShopExtensionPage)
+    } else {
+      replaceUrl(shopChromeExtensionPage)
+    }
+  }, [userId, browser])
+
+  const onNoClick = () => {
+    //CreateSfacExtensionPromptResponseMutation(userId, browser, false)
+    setOpen(false)
+  }
+
+  return (
+    <>
+      {variation == 'Version1' ? (
+        <div className={classes.wrapper}>
+          <Notification
+            className={classes.notification}
+            open={open}
+            text={
+              <span className={classes.text}>
+                <Typography className={classes.title}>
+                  Introducing: Shop for a Cause
+                </Typography>
+                <Typography className={classes.subtitle} variant="body1">
+                  We are excited to officially launch{' '}
+                  <Link
+                    to={getShopExtensionPage}
+                    target="_blank"
+                    style={{ color: '#9d4ba3' }}
+                  >
+                    Shop for a Cause
+                  </Link>
+                  ! Now, you can raise even more money for charity when you shop
+                  online. Like Tab for a Cause, it is simple, free, and
+                  impactful. It takes 10 seconds to get started, try it out
+                  today!
+                </Typography>
+              </span>
+            }
+            buttons={
+              <div className={classes.buttonsWrapper}>
+                <Button onClick={onNoClick} className={classes.noButton}>
+                  Maybe later
+                </Button>
+                <Button
+                  onClick={onYesClick}
+                  className={classes.yesButton}
+                  variant="contained"
+                >
+                  Add to Chrome
+                </Button>
+              </div>
+            }
+          />
+        </div>
+      ) : null}
+
+      {variation == 'Version2' ? (
+        <div className={classes.wrapper}>
+          <Notification
+            className={classes.notification}
+            open={open}
+            text={
+              <span className={classes.text}>
+                <Typography className={classes.title}>
+                  Introducing: Shop for a Cause
+                </Typography>
+                <Typography variant={'body1'}>
+                  We are excited to officially launch{' '}
+                  <Link
+                    to={getShopExtensionPage}
+                    target="_blank"
+                    style={{ color: '#9d4ba3' }}
+                  >
+                    Shop for a Cause
+                  </Link>
+                  ! Now, you can raise even more money for charity when you shop
+                  online. Like Tab for a Cause, it is simple, free, and
+                  impactful. It takes 10 seconds to get started, try it out
+                  today!
+                </Typography>
+              </span>
+            }
+            buttons={
+              <div className={classes.buttonsWrapper}>
+                <Button onClick={onNoClick} className={classes.noButton}>
+                  Maybe later
+                </Button>
+                <Button
+                  onClick={onYesClick}
+                  className={classes.yesButton}
+                  variant="contained"
+                >
+                  Add to Chrome
+                </Button>
+              </div>
+            }
+          />
+        </div>
+      ) : null}
+
+      {variation == 'Version3' ? (
+        <div className={classes.wrapper}>
+          <Notification
+            className={classes.notification}
+            open={open}
+            text={
+              <span className={classes.text}>
+                <Typography className={classes.title}>
+                  Shop for a Cause
+                </Typography>
+                <Typography variant={'body1'}>
+                  When Amazon{' '}
+                  <Link
+                    to={
+                      'https://www.npr.org/2023/01/19/1149993013/amazon-amazonsmile-charity-donation-program'
+                    }
+                    target="_blank"
+                    style={{ color: '#9d4ba3' }}
+                  >
+                    shutdown their Smile program
+                  </Link>{' '}
+                  to focus on more profit, it was a huge loss to charities. In
+                  response, we are proud to present{' '}
+                  <Link
+                    to={'https://shop.gladly.io/'}
+                    target="_blank"
+                    style={{ color: '#9d4ba3' }}
+                  >
+                    Shop for a Cause
+                  </Link>
+                  , our newest extension that raises money for charity as you
+                  shop online at over 10,000 partner stores. It is simple, free,
+                  and impactful :heart:
+                </Typography>
+              </span>
+            }
+            buttons={
+              <div className={classes.buttonsWrapper}>
+                <Button onClick={onNoClick} className={classes.noButton}>
+                  Maybe later
+                </Button>
+                <Button
+                  onClick={onYesClick}
+                  className={classes.yesButton}
+                  variant="contained"
+                >
+                  Add to Chrome
+                </Button>
+              </div>
+            }
+          />
+        </div>
+      ) : null}
+    </>
+  )
+}
+
+ShfacExtensionSellNotification.propTypes = {
+  userId: PropTypes.string,
+  variation: PropTypes.string.isRequired,
+  showShfacExtensionPrompt: PropTypes.bool.isRequired,
+}
+
+ShfacExtensionSellNotification.defaultProps = {
+  userId: PropTypes.string,
+}
+
+export default withStyles(styles)(ShfacExtensionSellNotification)
