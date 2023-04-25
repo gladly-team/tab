45aea88046c42b3b73e3fb83189b62c50d8dc10b
Jed Tan <jed@tabforacause.org>
2023-04-24 22:34:32 -0700

    Fix Read after Write Issue

--- graphql/__mocks__/@upstash/redis/with-fetch.js
+++ graphql/__mocks__/@upstash/redis/with-fetch.js
@@ -1,40 +1,73 @@
 /* eslint-env jest */
 
 const store = {}
+const methods = {
+  hgetall: (key) => {
+    // console.log(store)
+    if (!(key in store)) {
+      return null
+    }
+    return store[key]
+  },
+  hget: (key, field) => {
+    // console.log(store)
+    if (!(key in store)) {
+      return null
+    }
+    return store[key][field] === undefined ? null : store[key][field]
+  },
+  hset: (key, obj) => {
+    if (!(key in store)) {
+      store[key] = {}
+    }
+    const entries = Object.entries(obj)
+    for (let i = 0; i < entries.length; i += 1) {
+      const [objKey, objVal] = entries[i]
+      store[key][objKey] = objVal
+    }
+  },
+  hincrby: (key, field, increment) => {
+    store[key][field] += increment
+  },
+  del: (key) => {
+    // console.log(store)
+    if (key in store) {
+      delete store[key]
+      return 1
+    }
+    return 0
+  },
+}
+
+class Pipeline {
+  constructor() {
+    this.results = []
+  }
+
+  chain(result) {
+    this.results.push(result)
+    return this
+  }
+
+  hset(key, obj) {
+    return this.chain(methods.hset(key, obj))
+  }
+
+  hgetall(key) {
+    return this.chain(methods.hgetall(key))
+  }
+
+  exec() {
+    return this.results
+  }
+}
+
 module.exports = {
   Redis: () => {
     return {
-      hgetall: (key) => {
-        if (!(key in store)) {
-          return null
-        }
-        return store[key]
-      },
-      hget: (key, field) => {
-        if (!(key in store)) {
-          return null
-        }
-        return store[key][field] === undefined ? null : store[key][field]
-      },
-      hset: (key, obj) => {
-        if (!(key in store)) {
-          store[key] = {}
-        }
-        const entries = Object.entries(obj)
-        for (let i = 0; i < entries.length; i += 1) {
-          const [objKey, objVal] = entries[i]
-          store[key][objKey] = objVal
-        }
-      },
-      hincrby: (key, field, increment) => {
-        store[key][field] += increment
-      },
-      del: (key) => {
-        if (key in store) {
-          delete store[key]
-          return 1
-        }
-        return 0
+      ...methods,
+      multi: () => {
+        return new Pipeline()
       },
     }
   },
