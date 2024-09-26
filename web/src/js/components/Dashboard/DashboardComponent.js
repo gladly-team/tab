25d745098b361e0fdef01c7cf77f27770df8dde0
Spicer Matthews <github@cloudmanic.com>
2024-09-26 16:39:47 -0700

    Put some better error handling on logging tabs.

--- web/src/js/components/Dashboard/DashboardComponent.js
+++ web/src/js/components/Dashboard/DashboardComponent.js
@@ -176,24 +176,38 @@ class Dashboard extends React.Component {
   }
 
   logTabWithV5 = async () => {
-    const userToken = await getUserToken()
+    try {
+      const userToken = await getUserToken()
 
-    // This should not happen
-    if (!userToken) {
-      return
+      // This should not happen
+      if (!userToken) {
+        return
+      }
+
+      const headers = {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + userToken,
+      }
+
+      const response = await fetch(
+        `${process.env.REACT_APP_API_ENDPOINT}/api/v1/tab/log`,
+        {
+          method: 'POST',
+          headers: headers,
+          body: JSON.stringify({}),
+        }
+      )
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`)
+      }
+    } catch (error) {
+      console.error(
+        'logTabWithV5: There was a problem with the fetch operation:',
+        error
+      )
     }
-
-    const headers = {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-      Authorization: 'Bearer ' + userToken,
-    }
-
-    fetch(`${process.env.REACT_APP_API_ENDPOINT}/api/v1/tab/log`, {
-      method: 'POST',
-      headers: headers,
-      body: JSON.stringify({}),
-    })
   }
 
   componentDidMount() {
