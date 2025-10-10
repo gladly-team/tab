9d8a1fd6678e1f8a0e3de81152b714011815415e
Spicer Matthews <me@spicermatthews.com>
2025-10-10 08:52:44 -0700

    Updated money raised

--- graphql/database/globals/globals.js
+++ graphql/database/globals/globals.js
@@ -6,9 +6,9 @@ import { USER_REFERRAL_VC_REWARD } from '../constants'
 // When updating these numbers, also update them:
 // * in the tab-homepage repository
 // * in the search-homepage repository
-const MONEY_RAISED = 1900000.0
-const MONEY_RAISED_UPDATE_TIME = moment('2025-03-11T21:00:00.000Z')
-const MONEY_RAISED_PER_DAY = 380.0
+const MONEY_RAISED = 1980856.0
+const MONEY_RAISED_UPDATE_TIME = moment('2025-10-09T21:00:00.000Z')
+const MONEY_RAISED_PER_DAY = 310.0
 
 /**
  * Get an estimate of how much we've raised, using a combination of
